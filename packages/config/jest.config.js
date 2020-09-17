dcffb2ed4fc71f92892964a3b69d5babf2f0d4e7
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2020-09-17 14:50:48 +0200

    extract jest config + rename lib -> build

--- packages/config/jest.config.js
+++ packages/config/jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
+  ...require('../../jest/jest.config.js'),
   rootDir: __dirname,
-  testEnvironment: 'node',
-  testRegex: '/__tests__/.*(test|spec)\\.[jt]sx?$',
-  transform: {
-    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: require.resolve('./babel.config.js') }],
-  },
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
-  roots: ['__mocks__', 'src'],
+  roots: ['src'],
 };
