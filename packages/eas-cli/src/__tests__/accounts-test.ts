7cf50a2efa4b7b2a3946a2d6ccc2f57dc10546e9
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2020-09-22 10:51:15 +0200

    [eas-cli] use memfs in tests (#23)

--- packages/eas-cli/src/__tests__/accounts-test.ts
+++ packages/eas-cli/src/__tests__/accounts-test.ts
@@ -1,9 +1,10 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import tempy from 'tempy';
+import fs from 'fs-extra';
+import { vol } from 'memfs';
 
 import { getSessionSecret, loginAsync, logoutAsync } from '../accounts';
+import { getStateJsonPath } from '../utils/paths';
 
+jest.mock('fs');
 jest.mock('../utils/api', () => ({
   apiClient: {
     post: jest.fn(() => {
@@ -22,16 +23,15 @@ jest.mock('../utils/api', () => ({
   },
 }));
 
+beforeEach(() => {
+  vol.reset();
+});
+
 describe('loginAsync', () => {
   it('saves user data to ~/.expo/state.json', async () => {
-    const HOME = tempy.directory({ prefix: 'expo-home-' });
-    fs.mkdirSync(HOME, { recursive: true });
-    process.env.__UNSAFE_EXPO_HOME_DIRECTORY = HOME;
-    jest.resetModules();
-
     await loginAsync({ username: 'USERNAME', password: 'PASSWORD' });
 
-    expect(fs.readFileSync(path.join(HOME, 'state.json'), 'utf8')).toMatchInlineSnapshot(`
+    expect(await fs.readFile(getStateJsonPath(), 'utf8')).toMatchInlineSnapshot(`
       "{
         \\"auth\\": {
           \\"sessionSecret\\": \\"SESSION_SECRET\\",
@@ -47,11 +47,6 @@ describe('loginAsync', () => {
 
 describe('logoutAsync', () => {
   it('removes the session secret', async () => {
-    const HOME = tempy.directory({ prefix: 'expo-home-' });
-    fs.mkdirSync(HOME, { recursive: true });
-    process.env.__UNSAFE_EXPO_HOME_DIRECTORY = HOME;
-    jest.resetModules();
-
     await loginAsync({ username: 'USERNAME', password: 'PASSWORD' });
     expect(getSessionSecret()).toBe('SESSION_SECRET');
 
