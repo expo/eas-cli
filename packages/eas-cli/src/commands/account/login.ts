fa6e894bfbc2ccf248e5ea6b016d84401222f633
Ville Immonen <ville.immonen@iki.fi>
2020-11-11 11:26:43 +0200

    Align account command names with the style guide (#60)

--- packages/eas-cli/src/commands/account/login.ts
+++ packages/eas-cli/src/commands/account/login.ts
@@ -1,9 +1,11 @@
 import { Command } from '@oclif/command';
 
-import { showLoginPromptAsync } from '../user/actions';
+import { showLoginPromptAsync } from '../../user/actions';
 
-export default class Login extends Command {
+export default class AccountLogin extends Command {
   static description = 'log in with your EAS account';
+
+  static aliases = ['login'];
 
   async run() {
     this.log('Log in to EAS');
