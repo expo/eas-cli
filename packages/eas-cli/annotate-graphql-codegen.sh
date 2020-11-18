c5c6733fcf3aec52277fbf767cbb067291d30e2f
Quinlan Jung <quin@expo.io>
2020-11-18 11:28:31 -0800

    Update packages/eas-cli/annotate-graphql-codegen.sh
    Co-authored-by: Juwan Wheatley <datwheat@gmail.com>

--- packages/eas-cli/annotate-graphql-codegen.sh
+++ packages/eas-cli/annotate-graphql-codegen.sh
@@ -5,7 +5,7 @@ OUTFILE="src/graphql/generated.tsx"
 content="/**
  * This file was generated using GraphQL Codegen
  * Command: yarn generate-graphql-code
- * Run this during development for automatic code generation (schema & Apollo types + Apollo hooks & HOCs) when editing GraphQL documents
+ * Run this during development for automatic type generation when editing GraphQL documents
  * For more info and docs, visit https://graphql-code-generator.com/
  */
 "
