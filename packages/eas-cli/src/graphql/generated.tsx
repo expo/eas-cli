6822c9e7dc380abea88c40bb08b268821629e532
Quinlan Jung <quin@expo.io>
2020-11-18 13:17:23 -0800

    update annotation

--- packages/eas-cli/src/graphql/generated.tsx
+++ packages/eas-cli/src/graphql/generated.tsx
@@ -1,7 +1,7 @@
 /**
  * This file was generated using GraphQL Codegen
  * Command: yarn generate-graphql-code
- * Run this during development for automatic code generation (schema & Apollo types + Apollo hooks & HOCs) when editing GraphQL documents
+ * Run this during development for automatic type generation when editing GraphQL documents
  * For more info and docs, visit https://graphql-code-generator.com/
  */
 
