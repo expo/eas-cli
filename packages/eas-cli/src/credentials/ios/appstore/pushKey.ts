f3d19b7df30f5f241586a0dba5d1c60f5bd08af2
Evan Bacon <baconbrix@gmail.com>
2020-12-03 14:08:55 -0800

    Unify push spinners

--- packages/eas-cli/src/credentials/ios/appstore/pushKey.ts
+++ packages/eas-cli/src/credentials/ios/appstore/pushKey.ts
@@ -16,14 +16,14 @@ Please remember that Apple Keys are not application specific!
 `;
 
 export async function listPushKeysAsync(authCtx: AuthCtx): Promise<PushKeyStoreInfo[]> {
-  const spinner = ora(`Getting Push Keys from Apple...`).start();
+  const spinner = ora(`Fetching Push Keys from Apple`).start();
   try {
     const context = getRequestContext(authCtx);
     const keys = await Keys.getKeysAsync(context);
-    spinner.succeed();
+    spinner.succeed(`Fetched Push Keys from Apple`);
     return keys;
   } catch (error) {
-    spinner.fail();
+    spinner.fail(`Failed to fetch Push Keys from Apple`);
     throw error;
   }
 }
@@ -32,12 +32,12 @@ export async function createPushKeyAsync(
   authCtx: AuthCtx,
   name: string = `Expo Push Notifications Key ${dateformat('yyyymmddHHMMss')}`
 ): Promise<PushKey> {
-  const spinner = ora(`Creating Push Key on Apple Servers...`).start();
+  const spinner = ora(`Creating Push Key on Apple`).start();
   try {
     const context = getRequestContext(authCtx);
     const key = await Keys.createKeyAsync(context, { name, isApns: true });
     const apnsKeyP8 = await Keys.downloadKeyAsync(context, { id: key.id });
-    spinner.succeed();
+    spinner.succeed(`Created Push Key on Apple`);
     return {
       apnsKeyId: key.id,
       apnsKeyP8,
@@ -45,7 +45,7 @@ export async function createPushKeyAsync(
       teamName: authCtx.team.name,
     };
   } catch (err) {
-    spinner.fail('Failed to create Push Notifications Key');
+    spinner.fail('Failed to create Push Key on Apple');
     const resultString = err.rawDump?.resultString;
     if (
       err instanceof MaxKeysCreatedError ||
@@ -58,15 +58,15 @@ export async function createPushKeyAsync(
 }
 
 export async function revokePushKeyAsync(authCtx: AuthCtx, ids: string[]): Promise<void> {
-  const spinner = ora(`Revoking Push Key on Apple Servers...`).start();
+  const spinner = ora(`Revoking Push Key on Apple`).start();
   try {
     const context = getRequestContext(authCtx);
     await Promise.all(ids.map(id => Keys.revokeKeyAsync(context, { id })));
 
-    spinner.succeed();
+    spinner.succeed(`Revoked Push Key on Apple`);
   } catch (error) {
     log.error(error);
-    spinner.fail('Failed to revoke Push Key on Apple Servers');
+    spinner.fail('Failed to revoke Push Key on Apple');
     throw error;
   }
 }
