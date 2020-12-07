86257c1e2748b32c0f8134b07e393b740ef83889
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2020-12-07 20:38:49 +0100

    [eas submit] Add missing iOS error messages

--- packages/eas-cli/src/submissions/utils/errors.ts
+++ packages/eas-cli/src/submissions/utils/errors.ts
@@ -8,6 +8,8 @@ enum SubmissionErrorCode {
   ANDROID_FIRST_UPLOAD_ERROR = 'SUBMISSION_SERVICE_ANDROID_FIRST_UPLOAD_ERROR',
   ANDROID_OLD_VERSION_CODE_ERROR = 'SUBMISSION_SERVICE_ANDROID_OLD_VERSION_CODE_ERROR',
   ANDROID_MISSING_PRIVACY_POLICY = 'SUBMISSION_SERVICE_ANDROID_MISSING_PRIVACY_POLICY',
+  IOS_OLD_VERSION_CODE_ERROR = 'SUBMISSION_SERVICE_IOS_OLD_VERSION_CODE_ERROR',
+  IOS_UNKNOWN_ERROR = 'SUBMISSION_SERVICE_IOS_UNKNOWN_ERROR',
 }
 
 const SubmissionErrorMessages: Record<SubmissionErrorCode, string> = {
@@ -28,6 +30,12 @@ const SubmissionErrorMessages: Record<SubmissionErrorCode, string> = {
   [SubmissionErrorCode.ANDROID_MISSING_PRIVACY_POLICY]:
     'The app has permissions that require a privacy policy set for the app.\n' +
     `${learnMore('https://expo.fyi/missing-privacy-policy')}.`,
+  [SubmissionErrorCode.IOS_OLD_VERSION_CODE_ERROR]:
+    "You've already submitted this version of the app.\n" +
+    'Versions are identified by Build Numbers (expo.ios.buildNumber in app.json).\n' +
+    "If you're submitting an Expo project built with EAS Builds, increment the build number in app.json and build the project again.",
+  [SubmissionErrorCode.IOS_UNKNOWN_ERROR]:
+    "We couldn't figure out what went wrong. Please see logs to learn more.",
 };
 
 export function printSubmissionError(error: SubmissionError): boolean {
@@ -35,7 +43,9 @@ export function printSubmissionError(error: SubmissionError): boolean {
     const errorCode = error.errorCode as SubmissionErrorCode;
     log.addNewLineIfNone();
     log.error(SubmissionErrorMessages[errorCode]);
-    return errorCode === SubmissionErrorCode.ANDROID_UNKNOWN_ERROR;
+    return [SubmissionErrorCode.ANDROID_UNKNOWN_ERROR, SubmissionErrorCode.IOS_UNKNOWN_ERROR].some(
+      code => code === errorCode
+    );
   } else {
     log(error.message);
     return true;
