8f455237c783a69a4b3098a7d12ca16dea516f5b
Brent Vatne <brentvatne@gmail.com>
2020-12-08 09:58:58 -0800

    [eas-cli] Prevent importing extraneous dependencies

--- packages/eas-cli/.eslintrc.js
+++ packages/eas-cli/.eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   rules: {
     'import/no-cycle': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: [
+      '**/__tests__/**/*',
+      '**/__mocks__/**/*'
+    ] }],
   },
 };
