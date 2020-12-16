3733cc4cf8a9cf6ced51f5b474b46b3d8b50a4e7
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2020-12-16 11:56:43 +0100

    Fix no-newline typo

--- packages/eas-cli/src/submissions/utils/errors.ts
+++ packages/eas-cli/src/submissions/utils/errors.ts
@@ -35,7 +35,7 @@ const SubmissionErrorMessages: Record<SubmissionErrorCode, string> = {
   [SubmissionErrorCode.IOS_OLD_VERSION_CODE_ERROR]:
     "You've already submitted this version of the app.\n" +
     'Versions are identified by Build Numbers (expo.ios.buildNumber in app.json).\n' +
-    "If you're submitting an Expo project built with EAS Build, increment the build number in app.json and build the project again." +
+    "If you're submitting an Expo project built with EAS Build, increment the build number in app.json and build the project again.\n" +
     `${learnMore('https://expo.fyi/bumping-ios-build-number')}.`,
   [SubmissionErrorCode.IOS_UNKNOWN_ERROR]:
     "We couldn't figure out what went wrong. Please see logs to learn more.",
