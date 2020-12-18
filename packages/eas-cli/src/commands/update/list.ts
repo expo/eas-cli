a7d61a801788b76470e19e86e4fa7d76fb26ec65
jonsamp <sampjon@gmail.com>
2020-12-18 10:26:11 -0500

    SAVEPOINT

--- packages/eas-cli/src/commands/update/list.ts
+++ packages/eas-cli/src/commands/update/list.ts
@@ -1,63 +1,73 @@
 import { getConfig } from '@expo/config';
 import { Command, flags } from '@oclif/command';
+import chalk from 'chalk';
+import Table from 'cli-table3';
 import { cli } from 'cli-ux';
 import dateformat from 'dateformat';
 import gql from 'graphql-tag';
+import { groupBy } from 'lodash';
 
 import { graphqlClient, withErrorHandlingAsync } from '../../graphql/client';
+import { Update } from '../../graphql/generated';
 import log from '../../log';
+import { ensureProjectExistsAsync } from '../../project/ensureProjectExists';
 import { findProjectRootAsync, getProjectAccountNameAsync } from '../../project/projectUtils';
+import { promptAsync } from '../../prompts';
 
-type UpdateRelease = {
-  id: string;
-  createdAt: string;
-  platform: string;
-  updateMessage: string;
-  nativeRuntimeVersion: string;
-  updateGroup: string;
-  actor: {
-    firstName: string;
-  };
-};
+type TruncatedUpdate = Pick<
+  Update,
+  'updateGroup' | 'updateMessage' | 'createdAt' | 'actor' | 'platform' | 'nativeRuntimeVersion'
+>;
+const PAGE_LIMIT = 10_000;
 
-async function updateReleaseByReleaseNameAsync({
-  fullName,
+async function viewUpdateReleaseAsync({
+  appId,
   releaseName,
 }: {
-  fullName: string;
+  appId: string;
   releaseName: string;
-}): Promise<UpdateRelease[]> {
-  try {
-    const data = await withErrorHandlingAsync(
-      graphqlClient
-        .query<
-          {
-            app: {
-              byFullName: {
+}): Promise<{
+  id: string;
+  releaseName: string;
+  updates: TruncatedUpdate[];
+}> {
+  const data = await withErrorHandlingAsync(
+    graphqlClient
+      .mutation<
+        {
+          app: {
+            byId: {
+              updateReleaseByReleaseName: {
                 id: string;
-                updateReleaseByReleaseName: {
-                  updates: UpdateRelease[];
-                };
+                releaseName: string;
+                updates: TruncatedUpdate[];
               };
             };
-          },
-          { fullName: string; releaseName: string }
-        >(
-          gql`
-            query UpdateReleaseByReleaseName($fullName: String!, $releaseName: String!) {
-              app {
-                byFullName(fullName: $fullName) {
+          };
+        },
+        {
+          appId: string;
+          releaseName: string;
+        }
+      >(
+        gql`
+          query ViewRelease($appId: String!, $releaseName: String!) {
+            app {
+              byId(appId: $appId) {
+                updateReleaseByReleaseName(releaseName: $releaseName) {
                   id
-                  updateReleaseByReleaseName(releaseName: $releaseName) {
-                    id
-                    updates(offset: 0, limit: 1000) {
-                      id
-                      platform
-                      nativeRuntimeVersion
-                      createdAt
-                      updateMessage
-                      updateGroup
-                      actor {
+                  releaseName
+                  updates(offset: 0, limit: ${PAGE_LIMIT}) {
+                    updateGroup
+                    updateMessage
+                    createdAt
+                    platform
+                    nativeRuntimeVersion
+                    actor {
+                      ... on User {
+                        firstName
+                      }
+                      ... on Robot {
                         firstName
                       }
                     }
@@ -65,27 +75,30 @@ async function updateReleaseByReleaseNameAsync({
                 }
               }
             }
-          `,
-          {
-            fullName,
-            releaseName,
           }
-        )
-        .toPromise()
-    );
-    return data.app.byFullName.updateReleaseByReleaseName.updates;
-  } catch (error) {
-    throw new Error(`Unable to fetch updates. \n\n${error}`);
-  }
+        `,
+        {
+          appId,
+          releaseName,
+        }
+      )
+      .toPromise()
+  );
+  return data.app.byId.updateReleaseByReleaseName;
 }
 
 export default class UpdateList extends Command {
-  static description = 'View a list of updates by release name.';
+  static description = 'View a list of updates.';
+
+  static args = [
+    {
+      name: 'releaseName',
+      required: false,
+      description: 'Name of the release to view',
+    },
+  ];
 
   static flags = {
-    release: flags.string({
-      description: 'Name of the release.',
-    }),
     platform: flags.string({
       description: 'Update platforms to return: ios, android, web.',
     }),
@@ -94,19 +107,16 @@ export default class UpdateList extends Command {
       default: false,
     }),
     all: flags.boolean({
-      description: 'Return a list of all updates individually',
+      description: 'Return a list of all updates individually.',
       default: false,
     }),
   };
 
   async run() {
-    const {
-      flags: { json, release, platform },
+    let {
+      args: { releaseName },
+      flags: { json: jsonFlag, platform: platformFlag, all: allFlag },
     } = this.parse(UpdateList);
-
-    if (!release) {
-      throw new Error('A release name is required.');
-    }
 
     const projectDir = await findProjectRootAsync(process.cwd());
     if (!projectDir) {
@@ -116,17 +126,34 @@ export default class UpdateList extends Command {
     const {
       exp: { slug },
     } = getConfig(projectDir, { skipSDKVersionRequirement: true });
-
-    cli.action.start('Fetching updates');
-
-    const updates = await updateReleaseByReleaseNameAsync({
-      fullName: `@${accountName}/${slug}`,
-      releaseName: release,
+    const projectId = await ensureProjectExistsAsync({
+      accountName,
+      projectName: slug,
     });
 
-    cli.action.stop();
+    if (!releaseName) {
+      const validationMessage = 'Release name may not be empty.';
+      if (jsonFlag) {
+        throw new Error(validationMessage);
+      }
+      ({ releaseName } = await promptAsync({
+        type: 'text',
+        name: 'releaseName',
+        message: 'Please enter the name of the release to view:',
+        validate: value => (value ? true : validationMessage),
+      }));
+    }
 
-    const result = updates.reduce(
+    const UpdateRelease = await viewUpdateReleaseAsync({
+      appId: projectId,
+      releaseName,
+    });
+
+    // const updates = Object.values(groupBy(UpdateRelease.updates, u => u.updateGroup)).map(
+    //   updateGroup => updateGroup[0]
+    // );
+
+    const updates = UpdateRelease.updates.reduce(
       (acc, update) => {
         return {
           ...acc,
@@ -141,16 +168,31 @@ export default class UpdateList extends Command {
         };
       },
       {} as {
-        [i: string]: UpdateRelease;
+        [i: string]: TruncatedUpdate;
       }
     );
 
-    if (json) {
-      return log(Object.values(result));
+    if (jsonFlag) {
+      log({ ...UpdateRelease, updates });
+      return;
     }
 
+    // const updateGroupTable = new Table({
+    //   head: ['Created At', 'Update Group', 'Update message'],
+    //   wordWrap: true,
+    // });
+
+    // for (const update of updates) {
+    //   updateGroupTable.push([
+    //     new Date(update.createdAt).toLocaleString(),
+    //     update.updateGroup,
+    //     `[${update.actor?.firstName}] ${update.updateMessage}`,
+    //   ]);
+    // }
+
+    log('');
     cli.table(
-      Object.values(result),
+      Object.values(updates),
       {
         updateGroup: {
           header: 'ID',
@@ -159,20 +201,31 @@ export default class UpdateList extends Command {
         },
         createdAt: {
           header: 'Created At',
-          minWidth: 21,
+          minWidth: 24,
         },
         nativeRuntimeVersion: {
           header: 'RTV',
+          minWidth: 10,
         },
-        platforms: {},
+        platforms: {
+          minWidth: 15,
+        },
         updateMessage: {
           header: 'Message',
-          get: row => `[${row.actor.firstName}] ${row.updateMessage}`,
+          get: row => `[${row?.actor?.firstName}] ${row.updateMessage}`,
         },
       },
       {
-        filter: `platforms=${platform}`,
+        filter: platformFlag && `platforms=${platformFlag}`,
       }
     );
+
+    // log.withTick(
+    //   `️Release: ${chalk.bold(UpdateRelease.releaseName)} on project ${chalk.bold(
+    //     `@${accountName}/${slug}`
+    //   )}. Release ID: ${chalk.bold(UpdateRelease.id)}`
+    // );
+    // log(chalk.bold('Recent update groups published on this release:'));
+    // log(updateGroupTable.toString());
   }
 }
