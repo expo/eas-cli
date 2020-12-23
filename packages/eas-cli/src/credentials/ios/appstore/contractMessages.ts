68aacc3fa314e9aee7dd5bc85686d16f1b32d2a4
Evan Bacon <baconbrix@gmail.com>
2020-12-23 13:40:08 -0800

    prevent false positives

--- packages/eas-cli/src/credentials/ios/appstore/contractMessages.ts
+++ packages/eas-cli/src/credentials/ios/appstore/contractMessages.ts
@@ -26,24 +26,28 @@ async function getContractMessagesAsync(context: RequestContext) {
   }
 }
 
-async function getRequiredContractMessagesAsync(context: RequestContext) {
+async function getRequiredContractMessagesAsync(
+  context: RequestContext
+): Promise<{ messages: ITCAgreements.ITCContractMessage[]; isFatal: boolean }> {
   // This emulates the check that's performed on the ASC website's "apps"
   // page before presenting the (+) create app button.
   const status = await getContractStatusAsync(context);
 
-  if (['FREE_APP_AGREEMENT_ACTIVE', 'PAID_APP_AGREEMENT_ACTIVE'].includes(status)) {
-    // The user can freely create an app, no contracts need to be accepted.
-    // No need to check for messages because afaict no vital messages will be present.
-    return [];
-  } else if (
-    ['FREE_APP_AGREEMENT_OUTDATED', 'PAID_APP_AGREEMENT_OUTDATED', 'EXPIRED_MEMBERSHIP'].includes(
-      status
-    )
-  ) {
-    // The user cannot create an app until they've reviewed, and agreed to the updated agreements
-    // or renewed their membership on ASC.
-    // Get the exact messages from ASC to show the user a useful message.
-    return (await getContractMessagesAsync(context)) ?? [];
+  if (status) {
+    if (['FREE_APP_AGREEMENT_ACTIVE', 'PAID_APP_AGREEMENT_ACTIVE'].includes(status)) {
+      // The user can freely create an app, no contracts need to be accepted.
+      // No need to check for messages because afaict no vital messages will be present.
+      return { messages: [], isFatal: false };
+    } else if (
+      ['FREE_APP_AGREEMENT_OUTDATED', 'PAID_APP_AGREEMENT_OUTDATED', 'EXPIRED_MEMBERSHIP'].includes(
+        status
+      )
+    ) {
+      // The user cannot create an app until they've reviewed, and agreed to the updated agreements
+      // or renewed their membership on ASC.
+      // Get the exact messages from ASC to show the user a useful message.
+      return { messages: (await getContractMessagesAsync(context)) ?? [], isFatal: true };
+    }
   }
   // The contract messages aren't documented so if a new one is present we cannot be sure if it's fatal or not.
   // This will check for messages, if none exist, then the process will continue.
@@ -52,9 +56,10 @@ async function getRequiredContractMessagesAsync(context: RequestContext) {
   // prompt the user to open an issue so we can address the new contract state if it ever appears.
   // TODO: Maybe a silent analytic would be better
   log.error(
-    `Unexpected Apple developer contract status "${status}". Please open an issue on https://github.com/expo/eas-cli`
+    `\nUnexpected Apple developer contract status "${status}". Please open an issue on https://github.com/expo/eas-cli`
   );
-  return (await getContractMessagesAsync(context)) ?? [];
+  log.newLine();
+  return { messages: (await getContractMessagesAsync(context)) ?? [], isFatal: false };
 }
 
 const rootUrl = 'https://appstoreconnect.apple.com';
@@ -71,7 +76,7 @@ export function formatContractMessage(message: ITCAgreements.ITCContractMessage)
 }
 
 export async function assertContractMessagesAsync(context: RequestContext, spinner?: Ora) {
-  const messages = await getRequiredContractMessagesAsync(context);
+  const { messages, isFatal } = await getRequiredContractMessagesAsync(context);
 
   if (Array.isArray(messages) && messages.length) {
     if (spinner) {
@@ -89,6 +94,9 @@ export async function assertContractMessagesAsync(context: RequestContext, spinn
       log(formatContractMessage(message));
     }
     log.addNewLineIfNone();
-    throw new Error('App Store Connect has agreement updates that must be resolved');
+    // Only throw an error if we know that the status is fatal, otherwise attempt to finish the process.
+    if (isFatal) {
+      throw new Error('App Store Connect has agreement updates that must be resolved');
+    }
   }
 }
