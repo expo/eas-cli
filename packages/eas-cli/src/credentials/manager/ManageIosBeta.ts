263a950621185df202fb2d663b856418b3b43115
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-01-14 10:44:57 +0100

    rebase + fix ManageIosBeta

--- packages/eas-cli/src/credentials/manager/ManageIosBeta.ts
+++ packages/eas-cli/src/credentials/manager/ManageIosBeta.ts
@@ -1,3 +1,4 @@
+import log from '../../log';
 import { getProjectAccountName, getProjectConfigDescription } from '../../project/projectUtils';
 import { promptAsync } from '../../prompts';
 import { findAccountByName } from '../../user/Account';
@@ -25,49 +26,60 @@ enum ActionType {
 
 export class ManageIosBeta implements Action {
   async runAsync(manager: CredentialsManager, ctx: Context): Promise<void> {
-    // TODO uncomment this before committing!
-    //manager.pushNextAction(this);
-    await ctx.bestEffortAppStoreAuthenticateAsync();
+    while (true) {
+      try {
+        await ctx.bestEffortAppStoreAuthenticateAsync();
 
-    const accountName = ctx.hasProjectContext
-      ? getProjectAccountName(ctx.exp, ctx.user)
-      : ensureActorHasUsername(ctx.user);
+        const accountName = ctx.hasProjectContext
+          ? getProjectAccountName(ctx.exp, ctx.user)
+          : ensureActorHasUsername(ctx.user);
 
-    const account = findAccountByName(ctx.user.accounts, accountName);
-    if (!account) {
-      throw new Error(`You do not have access to account: ${accountName}`);
-    }
-    if (ctx.hasProjectContext) {
-      const appLookupParams = this.getAppLookupParamsFromContext(ctx);
-      const iosAppCredentials = await ctx.newIos.getIosAppCredentialsWithBuildCredentialsAsync(
-        appLookupParams
-      );
-      if (!iosAppCredentials) {
-        displayEmptyIosCredentials(appLookupParams);
-      } else {
-        displayIosAppCredentials(iosAppCredentials);
+        const account = findAccountByName(ctx.user.accounts, accountName);
+        if (!account) {
+          throw new Error(`You do not have access to account: ${accountName}`);
+        }
+        if (ctx.hasProjectContext) {
+          const appLookupParams = this.getAppLookupParamsFromContext(ctx);
+          const iosAppCredentials = await ctx.newIos.getIosAppCredentialsWithBuildCredentialsAsync(
+            appLookupParams
+          );
+          if (!iosAppCredentials) {
+            displayEmptyIosCredentials(appLookupParams);
+          } else {
+            displayIosAppCredentials(iosAppCredentials);
+          }
+        }
+
+        const projectSpecificActions: { value: ActionType; title: string }[] = ctx.hasProjectContext
+          ? []
+          : [];
+
+        const { action } = await promptAsync({
+          type: 'select',
+          name: 'action',
+          message: 'What do you want to do?',
+          choices: [
+            ...projectSpecificActions,
+            {
+              value: ActionType.RemoveDistributionCertificate,
+              title: 'Remove Distribution Certificate',
+            },
+          ],
+        });
+        try {
+          await manager.runActionAsync(this.getAction(ctx, accountName, action));
+        } catch (err) {
+          log.error(err);
+        }
+        await manager.runActionAsync(new PressAnyKeyToContinue());
+      } catch (err) {
+        log.error(err);
+        await manager.runActionAsync(new PressAnyKeyToContinue());
+      } finally {
+        // TODO remove finally block before committing!
+        return;
       }
     }
-
-    const projectSpecificActions: { value: ActionType; title: string }[] = ctx.hasProjectContext
-      ? []
-      : [];
-
-    const { action } = await promptAsync({
-      type: 'select',
-      name: 'action',
-      message: 'What do you want to do?',
-      choices: [
-        ...projectSpecificActions,
-        {
-          value: ActionType.RemoveDistributionCertificate,
-          title: 'Remove Distribution Certificate',
-        },
-      ],
-    });
-
-    manager.pushNextAction(new PressAnyKeyToContinue());
-    manager.pushNextAction(this.getAction(ctx, accountName, action));
   }
 
   private getAppLookupParamsFromContext(ctx: Context): AppLookupParams {
