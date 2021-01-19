92ad6a61ff9c70575f38b627cc95fa4805533c22
Quinlan Jung <quin@expo.io>
2021-01-18 19:03:19 -0800

    tests

---
+++ packages/eas-cli/src/credentials/ios/actions/new/__tests__/DistributionCertificateUtils-test.ts
@@ -0,0 +1,24 @@
+import mockdate from 'mockdate';
+
+import { AppleDistributionCertificateQuery } from '../../../api/graphql/queries/AppleDistributionCertificateQuery';
+import { formatDistributionCertificate } from '../DistributionCertificateUtils';
+jest.mock('../../../api/graphql/queries/AppleDistributionCertificateQuery');
+jest.mock('chalk', () => {
+  return {
+    __esModule: true, // this property makes it work
+    default: { gray: jest.fn(log => log) },
+  };
+});
+
+mockdate.set(new Date('4/20/2021'));
+describe('select credentials', () => {
+  it('select an AppleDistributionCertificate fragment', async () => {
+    const testDistCerts = (
+      await AppleDistributionCertificateQuery.getAllForAccount('quinAccount')
+    ).sort((a, b) => (a.serialNumber > b.serialNumber ? 1 : -1));
+    const loggedSoFar = testDistCerts
+      .map(cert => formatDistributionCertificate(cert))
+      .reduce((acc, certLog) => acc + certLog);
+    expect(loggedSoFar).toMatchSnapshot();
+  });
+});
