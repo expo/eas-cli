675f867c77dd00ee2213ece44200d26990276a70
Quinlan Jung <quin@expo.io>
2021-01-20 13:16:50 -0800

    [eas-cli] filter stale profiles

--- packages/eas-cli/src/credentials/ios/appstore/bundleId.ts
+++ packages/eas-cli/src/credentials/ios/appstore/bundleId.ts
@@ -1,6 +1,6 @@
 import { BundleId, Profile, RequestContext } from '@expo/apple-utils';
 
-export async function getProfilesForBundleIdAsync(
+async function getProfilesForBundleIdDangerousAsync(
   context: RequestContext,
   bundleIdentifier: string
 ): Promise<Profile[]> {
@@ -9,6 +9,34 @@ export async function getProfilesForBundleIdAsync(
     return bundleId.getProfilesAsync();
   }
   return [];
+}
+
+export async function getProfilesForBundleIdAsync(
+  context: RequestContext,
+  bundleIdentifier: string
+): Promise<Profile[]> {
+  const profiles = await getProfilesForBundleIdDangerousAsync(context, bundleIdentifier);
+  // users sometimes have a poisoned Apple cache and receive stale data from the API
+  // we call an arbitrary method, `getBundleIdAsync` on each profile
+  // if it errors, the profile was stale, so we remove it
+  const validProfileIds = new Set();
+  await Promise.all(
+    profiles.map(async profile => {
+      try {
+        await profile.getBundleIdAsync();
+        validProfileIds.add(profile.id);
+      } catch (e) {
+        if (
+          e.name === 'UnexpectedAppleResponse' &&
+          e.message.includes('The specified resource does not exist - There is no resource of type')
+        ) {
+          return;
+        }
+        throw e;
+      }
+    })
+  );
+  return profiles.filter(profile => validProfileIds.has(profile.id));
 }
 
 export async function getBundleIdForIdentifierAsync(
