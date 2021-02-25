6535d324b33c9a0f6f2fb4d9292e6d44eb266f81
Cedric van Putten <me@bycedric.com>
2021-02-25 14:52:35 +0100

    [eas-cli] add channel:view command

---
+++ packages/eas-cli/src/commands/channel/view.ts
@@ -0,0 +1,167 @@
+import { getConfig } from '@expo/config';
+import { Command, flags } from '@oclif/command';
+import chalk from 'chalk';
+import Table from 'cli-table3';
+import gql from 'graphql-tag';
+
+import { graphqlClient, withErrorHandlingAsync } from '../../graphql/client';
+import { UpdateChannel } from '../../graphql/generated';
+import Log from '../../log';
+import { ensureProjectExistsAsync } from '../../project/ensureProjectExists';
+import { findProjectRootAsync, getProjectAccountNameAsync } from '../../project/projectUtils';
+import { promptAsync } from '../../prompts';
+
+const PAGE_LIMIT = 25;
+
+async function getUpdateChannelOnAppAsync({
+  appId,
+  channelName,
+}: {
+  appId: string;
+  channelName: string;
+}): Promise<UpdateChannel> {
+  const data = await withErrorHandlingAsync(
+    graphqlClient
+      .query<
+        {
+          app: {
+            byId: {
+              updateChannelByName: {
+                id: string;
+                name: string;
+              };
+            };
+          };
+        },
+        {
+          appId: string;
+          name: string;
+          limit: number;
+        }
+      >(
+        gql`
+          query ViewChannelForApp($appId: String!, $name: String!, $limit: Int!) {
+            app {
+              byId(appId: $appId) {
+                name
+                updateChannelByName(name: $name) {
+                  id
+                  name
+                  createdAt
+                  updateBranches(offset: 0, limit: $limit) {
+                    id
+                    name
+                    updates(offset: 0, limit: $limit) {
+                      id
+                      group
+                      message
+                      createdAt
+                      actor {
+                        ... on User {
+                          firstName
+                        }
+                        ... on Robot {
+                          firstName
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+        {
+          appId,
+          name: channelName,
+          limit: PAGE_LIMIT,
+        }
+      )
+      .toPromise()
+  );
+  // todo: fix any typing
+  return data.app.byId.updateChannelByName as any;
+}
+
+export default class ChannelView extends Command {
+  static hidden = true;
+  static description = 'View a channel on the current project.';
+
+  static args = [
+    {
+      name: 'name',
+      required: false,
+      description: 'Name of the channel to view',
+    },
+  ];
+
+  static flags = {
+    json: flags.boolean({
+      description: 'print output as a JSON object with the channel ID, name and branch mapping.',
+      default: false,
+    }),
+  };
+
+  async run() {
+    let {
+      args: { name: channelName },
+      flags: { json: jsonFlag },
+    } = this.parse(ChannelView);
+
+    const projectDir = await findProjectRootAsync(process.cwd());
+    if (!projectDir) {
+      throw new Error('Please run this command inside a project directory.');
+    }
+    const accountName = await getProjectAccountNameAsync(projectDir);
+    const {
+      exp: { slug },
+    } = getConfig(projectDir, { skipSDKVersionRequirement: true });
+    const projectId = await ensureProjectExistsAsync({
+      accountName,
+      projectName: slug,
+    });
+
+    if (!channelName) {
+      const validationMessage = 'A channel name is required to view a specific channel.';
+      if (jsonFlag) {
+        throw new Error(validationMessage);
+      }
+      ({ name: channelName } = await promptAsync({
+        type: 'text',
+        name: 'name',
+        message: 'Please name the channel:',
+        validate: value => (value ? true : validationMessage),
+      }));
+    }
+
+    const channel = await getUpdateChannelOnAppAsync({ channelName, appId: projectId });
+
+    if (jsonFlag) {
+      Log.log(JSON.stringify(channel));
+      return;
+    }
+
+    const table = new Table({
+      head: ['channel', 'branch', 'update', 'message', 'created-at', 'actor'],
+      wordWrap: true,
+    });
+
+    for (const branch of channel.updateBranches) {
+      const update = branch.updates[0] || {};
+      table.push([
+        channel.name,
+        branch.name,
+        update.group,
+        update.message,
+        update.createdAt && new Date(update.createdAt).toLocaleString(),
+        update.actor?.firstName,
+      ]);
+    }
+
+    Log.withTick(
+      chalk`Channel: {bold ${channel.name}} on project {bold ${accountName}/${slug}}. Channel ID: {bold ${channel.id}}`
+    );
+    Log.log(chalk`{bold Recent update groups published on this branch:}`);
+    Log.log(table.toString());
+  }
+}
