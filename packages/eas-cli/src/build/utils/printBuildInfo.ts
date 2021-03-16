aef9320169b6df95965ed0291366405d14aa2bd8
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-03-16 14:25:27 +0100

    [eas-cli] update external errors logic

--- packages/eas-cli/src/build/utils/printBuildInfo.ts
+++ packages/eas-cli/src/build/utils/printBuildInfo.ts
@@ -48,7 +48,7 @@ export function printBuildResults(accountName: string, builds: (Build | null)[])
 function printBuildResult(accountName: string, build: Build): void {
   Log.addNewLineIfNone();
   if (build.status === 'errored') {
-    const userError = build.error && errors.fromExternalError(build.error);
+    const userError = build.error;
     Log.error(
       `${platformEmojis[build.platform]} ${platformDisplayNames[build.platform]} build failed${
         userError ? ':' : ''
@@ -106,9 +106,9 @@ export function printDeprecationWarnings(deprecationInfo?: DeprecationInfo): voi
   }
 }
 
-export function printUserError(error: errors.UserError): void {
+export function printUserError(error: errors.ExternalUserError): void {
   Log.error(error.message);
-  if (error.fyiURL) {
-    Log.error(learnMore(error.fyiURL, { dim: false }));
+  if (error.docsURL) {
+    Log.error(learnMore(error.docsURL, { dim: false }));
   }
 }
