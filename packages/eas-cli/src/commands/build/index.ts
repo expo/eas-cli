dc0a8af18ceb08314b63a0c004987276d6c0074b
Dominik Sokal <dominik.sokal@swmansion.com>
2021-03-25 12:33:42 +0100

    remove requested_platform from tracking context

--- packages/eas-cli/src/commands/build/index.ts
+++ packages/eas-cli/src/commands/build/index.ts
@@ -93,7 +93,6 @@ export default class Build extends Command {
 
     const trackingCtx = {
       tracking_id: uuidv4(),
-      requested_platform: flags.platform,
     };
     Analytics.logEvent(AnalyticsEvent.BUILD_COMMAND, trackingCtx);
 
