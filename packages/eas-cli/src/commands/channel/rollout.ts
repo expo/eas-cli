75d93916f04dd3fbefad02de45602956c91c9c85
Jonathan 小林 Hales <jonathan@expo.io>
2021-03-26 06:20:03 -0700

    first round

--- packages/eas-cli/src/commands/channel/rollout.ts
+++ packages/eas-cli/src/commands/channel/rollout.ts
@@ -13,14 +13,14 @@ import { promptAsync, selectAsync } from '../../prompts';
 import { updateChannelBranchMappingAsync } from './edit';
 import { getBranchMapping, getUpdateChannelByNameForAppAsync } from './view';
 
-async function getPercentAsync({
+async function getRolloutPercentAsync({
   validationMessage,
   promptMessage,
 }: {
   validationMessage: string;
   promptMessage: string;
 }): Promise<number> {
-  const { name: percent } = await promptAsync({
+  const { name: rolloutPercent } = await promptAsync({
     type: 'text',
     name: 'name',
     format: value => {
@@ -28,7 +28,7 @@ async function getPercentAsync({
     },
     message: promptMessage,
     initial: 0,
-    validate: (percent: string): true | string => {
+    validate: (rolloutPercent: string): true | string => {
       /**
        * Don't allow 100 to emphasise to the developer that rollouts have an open right hand endpoint: [0,N)
        *
@@ -36,13 +36,13 @@ async function getPercentAsync({
        * comparison would result in (0,100]. Assuming the new branch is more dangerous, we choose [0,N) to allow
        * '0 percent' rollouts.
        */
-      const floatValue = parseFloat(percent);
+      const floatValue = parseFloat(rolloutPercent);
       return Number.isInteger(floatValue) && floatValue >= 0 && floatValue <= 99
         ? true
         : validationMessage;
     },
   });
-  return percent;
+  return rolloutPercent;
 }
 
 export default class ChannelRollout extends Command {
@@ -110,19 +110,25 @@ export default class ChannelRollout extends Command {
       throw new Error('The channel is not pointing at any branches.');
     }
     if (currentBranchMapping.data.length > 2) {
-      throw new Error('This CLI cannot handle branch mappings with more than 2 branches.');
+      throw new Error('"channel:rollout" cannot handle branch mappings with more than 2 branches.');
     }
 
+    // This combination doesn't make sense. Throw an error explaining the options.
     if (isRollout && branchName && !endFlag) {
       throw new Error(
         `There is a rollout in progress. You can only either edit the rollout percent or 'end' it.`
       );
     }
 
+    /**
+     * This if/else block has three branches:
+     *  1. The branch mapping is not a rollout, i.e. it is pointing to a single branch.
+     *  2. The branch mapping is a rollout.
+     *    a. increase/decrease the rollout percentage.
+     *    b. end the rollout.
+     */
     let newChannelInfo, logMessage;
     if (!isRollout) {
-      // start a rollout
-      // this will likely be removed as more commands fill out
       if (!branchName) {
         const validationMessage = 'A branch must be specified.';
         if (jsonFlag) {
@@ -144,7 +150,7 @@ export default class ChannelRollout extends Command {
         if (jsonFlag) {
           throw new Error(validationMessage);
         }
-        percent = await getPercentAsync({ promptMessage, validationMessage });
+        percent = await getRolloutPercentAsync({ promptMessage, validationMessage });
       }
 
       const newBranchMapping = {
@@ -183,106 +189,109 @@ export default class ChannelRollout extends Command {
       )}% of users will be directed to ${chalk.bold(branchName)}, ${chalk.bold(
         100 - percent!
       )}% to ${oldBranch.name}.`;
-    } else if (!endFlag) {
-      if (jsonFlag && !percent) {
-        throw new Error(
-          'A rollout is already in progress. You can either end it with the --end flag or modify it with the --percent flag.'
-        );
-      }
-      const currentPercent = 100 * currentBranchMapping.data[0].branchMappingLogic.operand;
-      const [newBranchId, oldBranchId] = currentBranchMapping.data.map(d => d.branchId);
-      const newBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
-        branch => branch.id === newBranchId
-      )[0];
-      const oldBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
-        branch => branch.id === oldBranchId
-      )[0];
-      if (!newBranch || !oldBranch) {
-        throw new Error(
-          `Branch mapping rollout is missing a branch for channel "${channelName}" on app "@${accountName}/${slug}"`
-        );
-      }
-
-      if (percent === undefined) {
-        const promptMessage = `Currently ${currentPercent}% of all users are routed to branch ${
-          newBranch.name
-        } and ${100 - currentPercent}% of all users are routed to branch ${
-          oldBranch.name
-        }. What percent of users should be directed to the branch ${newBranch.name}?`;
-        const validationMessage =
-          'The rollout percentage must be an integer between 0 and 99 inclusive.';
-        if (jsonFlag) {
-          throw new Error(validationMessage);
-        }
-        percent = await getPercentAsync({ promptMessage, validationMessage });
-      }
-
-      const newBranchMapping = { ...currentBranchMapping };
-      newBranchMapping.data[0].branchMappingLogic.operand = percent / 100;
-
-      newChannelInfo = await updateChannelBranchMappingAsync({
-        channelId: getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.id!,
-        branchMapping: JSON.stringify(newBranchMapping),
-      });
-
-      logMessage = `️Rollout of branch ${chalk.bold(newBranch.name)} onto channel ${chalk.bold(
-        channelName!
-      )} updated from ${chalk.bold(currentPercent)}% to ${chalk.bold(percent)}%. ${chalk.bold(
-        percent
-      )}% of users will be directed to ${chalk.bold(newBranch.name)}, ${chalk.bold(
-        100 - percent!
-      )}% to ${oldBranch.name}.`;
-    } else if (endFlag) {
-      // end the rollout to the branch that is specified. check it is at 100% and throw if not interactive
-      const currentPercent = 100 * currentBranchMapping.data[0].branchMappingLogic.operand;
-      const [newBranchId, oldBranchId] = currentBranchMapping.data.map(d => d.branchId);
-      const newBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
-        branch => branch.id === newBranchId
-      )[0];
-      const oldBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
-        branch => branch.id === oldBranchId
-      )[0];
-      if (!newBranch || !oldBranch) {
-        throw new Error(
-          `Branch mapping rollout is missing a branch for channel "${channelName}" on app "@${accountName}/${slug}"`
-        );
-      }
-
-      const endOnNewBranch = await selectAsync<boolean>(
-        'Ending the rollout will send all traffic to a single branch. Which one should that be?',
-        [
-          {
-            title: `${newBranch.name} ${chalk.grey(`- current percent: ${100 - currentPercent}%`)}`,
-            value: true,
-          },
-          {
-            title: `${oldBranch.name} ${chalk.grey(`- current percent: ${currentPercent}%`)}`,
-            value: false,
-          },
-        ]
-      );
-
-      const newBranchMapping = {
-        version: 0,
-        data: [
-          {
-            branchId: endOnNewBranch ? newBranchId : oldBranchId,
-            branchMappingLogic: 'true',
-          },
-        ],
-      };
-
-      newChannelInfo = await updateChannelBranchMappingAsync({
-        channelId: getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.id!,
-        branchMapping: JSON.stringify(newBranchMapping),
-      });
-      logMessage = `️Rollout on channel ${chalk.bold(
-        channelName
-      )} ended. All traffic is now sent to ${chalk.bold(
-        endOnNewBranch ? newBranch.name : oldBranch.name
-      )}`;
     } else {
-      throw new Error('');
+      // not a rollout
+      if (!endFlag) {
+        const currentPercent = 100 * currentBranchMapping.data[0].branchMappingLogic.operand;
+        const [newBranchId, oldBranchId] = currentBranchMapping.data.map(d => d.branchId);
+        const newBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
+          branch => branch.id === newBranchId
+        )[0];
+        const oldBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
+          branch => branch.id === oldBranchId
+        )[0];
+        if (!newBranch || !oldBranch) {
+          throw new Error(
+            `Branch mapping rollout is missing a branch for channel "${channelName}" on app "@${accountName}/${slug}"`
+          );
+        }
+
+        if (percent === undefined) {
+          if (jsonFlag) {
+            throw new Error(
+              'A rollout is already in progress. If you wish to modify it you must use specify the new rollout percentage with the --percent flag.'
+            );
+          }
+          const promptMessage = `Currently ${currentPercent}% of all users are routed to branch ${
+            newBranch.name
+          } and ${100 - currentPercent}% of all users are routed to branch ${
+            oldBranch.name
+          }. What percent of users should be directed to the branch ${newBranch.name}?`;
+          const validationMessage =
+            'The rollout percentage must be an integer between 0 and 99 inclusive.';
+          if (jsonFlag) {
+            throw new Error(validationMessage);
+          }
+          percent = await getRolloutPercentAsync({ promptMessage, validationMessage });
+        }
+
+        const newBranchMapping = { ...currentBranchMapping };
+        newBranchMapping.data[0].branchMappingLogic.operand = percent / 100;
+
+        newChannelInfo = await updateChannelBranchMappingAsync({
+          channelId: getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.id!,
+          branchMapping: JSON.stringify(newBranchMapping),
+        });
+
+        logMessage = `️Rollout of branch ${chalk.bold(newBranch.name)} onto channel ${chalk.bold(
+          channelName!
+        )} updated from ${chalk.bold(currentPercent)}% to ${chalk.bold(percent)}%. ${chalk.bold(
+          percent
+        )}% of users will be directed to ${chalk.bold(newBranch.name)}, ${chalk.bold(
+          100 - percent!
+        )}% to ${oldBranch.name}.`;
+      } else {
+        // end flag is true
+        const currentPercent = 100 * currentBranchMapping.data[0].branchMappingLogic.operand;
+        const [newBranchId, oldBranchId] = currentBranchMapping.data.map(d => d.branchId);
+        const newBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
+          branch => branch.id === newBranchId
+        )[0];
+        const oldBranch = getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.updateBranches.filter(
+          branch => branch.id === oldBranchId
+        )[0];
+        if (!newBranch || !oldBranch) {
+          throw new Error(
+            `Branch mapping rollout is missing a branch for channel "${channelName}" on app "@${accountName}/${slug}"`
+          );
+        }
+
+        const endOnNewBranch = await selectAsync<boolean>(
+          'Ending the rollout will send all traffic to a single branch. Which one should that be?',
+          [
+            {
+              title: `${newBranch.name} ${chalk.grey(
+                `- current percent: ${100 - currentPercent}%`
+              )}`,
+              value: true,
+            },
+            {
+              title: `${oldBranch.name} ${chalk.grey(`- current percent: ${currentPercent}%`)}`,
+              value: false,
+            },
+          ]
+        );
+
+        const newBranchMapping = {
+          version: 0,
+          data: [
+            {
+              branchId: endOnNewBranch ? newBranchId : oldBranchId,
+              branchMappingLogic: 'true',
+            },
+          ],
+        };
+
+        newChannelInfo = await updateChannelBranchMappingAsync({
+          channelId: getUpdateChannelByNameForAppResult.app?.byId.updateChannelByName.id!,
+          branchMapping: JSON.stringify(newBranchMapping),
+        });
+        logMessage = `️Rollout on channel ${chalk.bold(
+          channelName
+        )} ended. All traffic is now sent to ${chalk.bold(
+          endOnNewBranch ? newBranch.name : oldBranch.name
+        )}`;
+      }
     }
 
     if (jsonFlag) {
