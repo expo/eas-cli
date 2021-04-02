9d88fba40e57328c3c5c8430f247ec5c89ffc262
Quinlan Jung <quin@expo.io>
2021-04-01 23:16:34 -0700

    [eas-cli] configure provisioning profile using new api (#299)
    * [eas-cli] configure provisioning profile using new api
    
    change positional args
    
    * configure prov prof: pr feedback
    
    * fix lint

---
+++ packages/eas-cli/src/credentials/ios/actions/new/__tests__/ConfigureProvisioningProfile-test.ts
@@ -0,0 +1,64 @@
+import { confirmAsync } from '../../../../../prompts';
+import { getAppstoreMock, testAuthCtx } from '../../../../__tests__/fixtures-appstore';
+import { createCtxMock } from '../../../../__tests__/fixtures-context';
+import {
+  testDistCertFragmentNoDependencies,
+  testProvisioningProfile,
+  testProvisioningProfileFragment,
+} from '../../../../__tests__/fixtures-ios';
+import { ManageIosBeta } from '../../../../manager/ManageIosBeta';
+import { MissingCredentialsNonInteractiveError } from '../../../errors';
+import { ConfigureProvisioningProfile } from '../ConfigureProvisioningProfile';
+jest.mock('../../../../../prompts');
+(confirmAsync as jest.Mock).mockImplementation(() => true);
+
+describe('ConfigureProvisioningProfile', () => {
+  it('configures a Provisioning Profile in Interactive Mode', async () => {
+    const mockProvisioningProfileFromApple = {
+      provisioningProfileId: testProvisioningProfileFragment.developerPortalIdentifier,
+      provisioningProfile: testProvisioningProfileFragment.provisioningProfile,
+    };
+    const ctx = createCtxMock({
+      nonInteractive: false,
+      appStore: {
+        ...getAppstoreMock(),
+        ensureAuthenticatedAsync: jest.fn(() => testAuthCtx),
+        authCtx: testAuthCtx,
+        createProvisioningProfileAsync: jest.fn(() => testProvisioningProfile),
+        listProvisioningProfilesAsync: jest.fn(() => [mockProvisioningProfileFromApple]),
+        useExistingProvisioningProfileAsync: jest.fn(() => mockProvisioningProfileFromApple),
+      },
+    });
+    const appLookupParams = ManageIosBeta.getAppLookupParamsFromContext(ctx);
+    const provProfConfigurator = new ConfigureProvisioningProfile(
+      appLookupParams,
+      testDistCertFragmentNoDependencies,
+      testProvisioningProfileFragment
+    );
+    await provProfConfigurator.runAsync(ctx);
+
+    // expect provisioning profile not to be updated on expo servers
+    expect((ctx.newIos.updateProvisioningProfileAsync as any).mock.calls.length).toBe(1);
+    // expect provisioning profile not to be updated on apple portal
+    expect((ctx.appStore.useExistingProvisioningProfileAsync as any).mock.calls.length).toBe(1);
+  });
+  it('errors in Non Interactive Mode', async () => {
+    const ctx = createCtxMock({
+      nonInteractive: true,
+    });
+    const appLookupParams = ManageIosBeta.getAppLookupParamsFromContext(ctx);
+    const provProfConfigurator = new ConfigureProvisioningProfile(
+      appLookupParams,
+      testDistCertFragmentNoDependencies,
+      testProvisioningProfileFragment
+    );
+    await expect(provProfConfigurator.runAsync(ctx)).rejects.toThrowError(
+      MissingCredentialsNonInteractiveError
+    );
+
+    // expect provisioning profile not to be updated on expo servers
+    expect((ctx.newIos.updateProvisioningProfileAsync as any).mock.calls.length).toBe(0);
+    // expect provisioning profile not to be updated on apple portal
+    expect((ctx.appStore.useExistingProvisioningProfileAsync as any).mock.calls.length).toBe(0);
+  });
+});
