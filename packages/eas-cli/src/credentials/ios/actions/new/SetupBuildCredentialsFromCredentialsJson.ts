09f33263cb11397b2b26a16cdd6361d0ba78219e
Quinlan Jung <quin@expo.io>
2021-04-14 14:28:06 -0700

    [eas-cli] use new graphql api in SetupBuildCredentialsFromCredentials… (#328)
    * [eas-cli] use new graphql api in SetupBuildCredentialsFromCredentialsJson
    
    * pr feedback
    
    * tests
    
    * Update packages/eas-cli/src/credentials/ios/actions/new/BuildCredentialsUtils.ts
    
    Co-authored-by: Dominik Sokal <dominik.sokal@swmansion.com>
    
    * pr feedback
    
    Co-authored-by: Dominik Sokal <dominik.sokal@swmansion.com>

---
+++ packages/eas-cli/src/credentials/ios/actions/new/SetupBuildCredentialsFromCredentialsJson.ts
@@ -0,0 +1,189 @@
+import chalk from 'chalk';
+import nullthrows from 'nullthrows';
+
+import {
+  AppleDistributionCertificateFragment,
+  AppleProvisioningProfileFragment,
+  AppleTeamFragment,
+  IosAppBuildCredentialsFragment,
+  IosDistributionType,
+} from '../../../../graphql/generated';
+import Log from '../../../../log';
+import { confirmAsync } from '../../../../prompts';
+import { Context } from '../../../context';
+import {
+  IosTargetCredentials,
+  isCredentialsMap,
+  readIosCredentialsAsync,
+} from '../../../credentialsJson/read';
+import { AppLookupParams } from '../../api/GraphqlClient';
+import { displayProjectCredentials } from '../../utils/printCredentialsBeta';
+import { readAppleTeam } from '../../utils/provisioningProfile';
+import {
+  assignBuildCredentialsAsync,
+  getBuildCredentialsAsync,
+  getDistributionCertificateAsync,
+  getProvisioningProfileAsync,
+} from './BuildCredentialsUtils';
+
+export class SetupBuildCredentialsFromCredentialsJson {
+  constructor(private app: AppLookupParams, private distributionType: IosDistributionType) {}
+
+  async getDistributionCertificateToAssignAsync(
+    ctx: Context,
+    targetCredentials: IosTargetCredentials,
+    appleTeam: AppleTeamFragment,
+    currentDistributionCertificate: AppleDistributionCertificateFragment | null
+  ): Promise<AppleDistributionCertificateFragment> {
+    const { distributionCertificate } = targetCredentials;
+    const { certP12, certPassword } = distributionCertificate;
+
+    if (!currentDistributionCertificate) {
+      return await ctx.newIos.createDistributionCertificateAsync(this.app, {
+        certP12,
+        certPassword,
+        teamId: appleTeam.appleTeamIdentifier,
+        teamName: appleTeam.appleTeamName ?? undefined,
+      });
+    }
+
+    const isSameCertificate = currentDistributionCertificate.certificateP12 === certP12;
+    if (!isSameCertificate) {
+      return await ctx.newIos.createDistributionCertificateAsync(this.app, {
+        certP12,
+        certPassword,
+        teamId: appleTeam.appleTeamIdentifier,
+        teamName: appleTeam.appleTeamName ?? undefined,
+      });
+    }
+
+    // If the configured Distribution Certificate is the same as the new one, just use the currently configured certificate
+    return currentDistributionCertificate;
+  }
+
+  async createNewProvisioningProfileAsync(
+    ctx: Context,
+    targetCredentials: IosTargetCredentials,
+    appleTeam: AppleTeamFragment
+  ): Promise<AppleProvisioningProfileFragment> {
+    const { provisioningProfile } = targetCredentials;
+
+    const appleAppIdentifier = await ctx.newIos.createOrGetExistingAppleAppIdentifierAsync(
+      this.app,
+      appleTeam
+    );
+    return await ctx.newIos.createProvisioningProfileAsync(this.app, appleAppIdentifier, {
+      appleProvisioningProfile: provisioningProfile,
+    });
+  }
+
+  async getProvisioningProfileToAssignAsync(
+    ctx: Context,
+    targetCredentials: IosTargetCredentials,
+    appleTeam: AppleTeamFragment,
+    currentProvisioningProfile: AppleProvisioningProfileFragment | null
+  ): Promise<AppleProvisioningProfileFragment> {
+    const { provisioningProfile } = targetCredentials;
+
+    if (!currentProvisioningProfile) {
+      return await this.createNewProvisioningProfileAsync(ctx, targetCredentials, appleTeam);
+    }
+
+    const isSameProfile = currentProvisioningProfile.provisioningProfile === provisioningProfile;
+    if (!isSameProfile) {
+      return await this.createNewProvisioningProfileAsync(ctx, targetCredentials, appleTeam);
+    }
+
+    // If the configured Provisioning Profile is the same as the new one, just use the currently configured profile
+    return currentProvisioningProfile;
+  }
+
+  async runAsync(ctx: Context): Promise<IosAppBuildCredentialsFragment> {
+    let localCredentials;
+    try {
+      localCredentials = await readIosCredentialsAsync(ctx.projectDir);
+    } catch (error) {
+      Log.error(
+        'Reading credentials from credentials.json failed. Make sure this file is correct and all credentials are present there.'
+      );
+      throw error;
+    }
+
+    // TODO: implement storing multi-target credentials on EAS servers
+    if (isCredentialsMap(localCredentials)) {
+      throw new Error(
+        'Storing multi-target iOS credentials from credentials.json on EAS servers is not yet supported.'
+      );
+    }
+
+    // currently configured credentials
+    const buildCredentials = await getBuildCredentialsAsync(ctx, this.app, this.distributionType);
+    const currentDistributionCertificate = await getDistributionCertificateAsync(
+      ctx,
+      this.app,
+      this.distributionType
+    );
+    const currentProfile = await getProvisioningProfileAsync(ctx, this.app, this.distributionType);
+    const appInfo = `@${this.app.account.name}/${this.app.projectName} (${this.app.bundleIdentifier})`;
+
+    // new credentials from local json
+    const appleTeamFromProvisioningProfile = readAppleTeam(localCredentials.provisioningProfile);
+    const appleTeam = await ctx.newIos.createOrGetExistingAppleTeamAsync(this.app, {
+      appleTeamIdentifier: appleTeamFromProvisioningProfile.teamId,
+      appleTeamName: appleTeamFromProvisioningProfile.teamName,
+    });
+    const distributionCertificateToAssign = await this.getDistributionCertificateToAssignAsync(
+      ctx,
+      localCredentials,
+      appleTeam,
+      currentDistributionCertificate
+    );
+    const provisioningProfileToAssign = await this.getProvisioningProfileToAssignAsync(
+      ctx,
+      localCredentials,
+      appleTeam,
+      currentProfile
+    );
+
+    const didCredentialsChange =
+      !currentDistributionCertificate ||
+      !currentProfile ||
+      currentDistributionCertificate.id !== distributionCertificateToAssign.id ||
+      currentProfile.id !== provisioningProfileToAssign.id;
+    if (!didCredentialsChange) {
+      Log.log('Credentials have already been configured to your credentials json.');
+      return nullthrows(buildCredentials, 'buildCredentials should have been defined.');
+    }
+
+    if (buildCredentials) {
+      Log.log('Currently configured credentials:');
+      displayProjectCredentials(this.app, buildCredentials);
+      if (!ctx.nonInteractive) {
+        const confirm = await confirmAsync({
+          message: `Would you like to replace this configuration with credentials from credentials json?`,
+        });
+        if (!confirm) {
+          throw new Error('Aborting setup of build credentials from credentials json');
+        }
+      }
+    }
+
+    Log.log(`Assigning credentials from credentials json to ${appInfo}...`);
+    const newBuildCredentials = await assignBuildCredentialsAsync(
+      ctx,
+      this.app,
+      this.distributionType,
+      distributionCertificateToAssign,
+      provisioningProfileToAssign,
+      appleTeam
+    );
+
+    Log.log('New credentials configuration:');
+    displayProjectCredentials(this.app, newBuildCredentials);
+
+    Log.newLine();
+    Log.log(chalk.green(`All credentials are ready to build ${appInfo}`));
+    Log.newLine();
+    return newBuildCredentials;
+  }
+}
---
+++ packages/eas-cli/src/credentials/ios/actions/new/__tests__/SetupBuildCredentialsFromCredentialsJson-test.ts
@@ -0,0 +1,171 @@
+import { asMock } from '../../../../../__tests__/utils';
+import { IosDistributionType } from '../../../../../graphql/generated';
+import { confirmAsync } from '../../../../../prompts';
+import { createCtxMock } from '../../../../__tests__/fixtures-context';
+import {
+  testAppleAppIdentifierFragment,
+  testDistCert,
+  testDistCertFragmentNoDependencies,
+  testIosAppCredentialsWithBuildCredentialsQueryResult,
+  testProvisioningProfile,
+  testProvisioningProfileFragment,
+} from '../../../../__tests__/fixtures-ios';
+import { getNewIosApiMockWithoutCredentials } from '../../../../__tests__/fixtures-new-ios';
+import { readIosCredentialsAsync } from '../../../../credentialsJson/read';
+import { ManageIosBeta } from '../../../../manager/ManageIosBeta';
+import { SetupBuildCredentialsFromCredentialsJson } from '../SetupBuildCredentialsFromCredentialsJson';
+jest.mock('../../../../../prompts');
+jest.mock('../../../../credentialsJson/read');
+
+const originalConsoleLog = console.log;
+const originalConsoleWarn = console.warn;
+beforeAll(() => {
+  console.log = jest.fn();
+  console.warn = jest.fn();
+});
+afterAll(() => {
+  console.log = originalConsoleLog;
+  console.warn = originalConsoleWarn;
+});
+
+beforeEach(() => {
+  asMock(confirmAsync).mockReset();
+  asMock(readIosCredentialsAsync).mockReset();
+  (confirmAsync as jest.Mock).mockImplementation(() => true);
+  (readIosCredentialsAsync as jest.Mock).mockImplementation(() => ({
+    provisioningProfile: testProvisioningProfile.provisioningProfile,
+    distributionCertificate: {
+      certP12: testDistCert.certP12,
+      certPassword: testDistCert.certPassword,
+    },
+  }));
+});
+
+describe('SetupBuildCredentialsFromCredentialsJson', () => {
+  it('sets up build credentials with same prior configuration in Interactive Mode', async () => {
+    // configure to be the same creds that are returned by readIosCredentialsAsync in the mock above
+    // create deep clone the quick and dirty way
+    const testBuildCreds = JSON.parse(
+      JSON.stringify(testIosAppCredentialsWithBuildCredentialsQueryResult)
+    );
+    testBuildCreds.iosAppBuildCredentialsArray[0].distributionCertificate.certificateP12 =
+      testDistCert.certP12;
+    testBuildCreds.iosAppBuildCredentialsArray[0].provisioningProfile.provisioningProfile =
+      testProvisioningProfile.provisioningProfile;
+    const ctx = createCtxMock({
+      newIos: {
+        ...getNewIosApiMockWithoutCredentials(),
+        getIosAppCredentialsWithBuildCredentialsAsync: jest.fn(() => testBuildCreds),
+        createOrGetExistingAppleAppIdentifierAsync: jest.fn(() => testAppleAppIdentifierFragment),
+        createDistributionCertificateAsync: jest.fn(() => ({ testDistCertFragmentNoDependencies })),
+        createProvisioningProfileAsync: jest.fn(() => testProvisioningProfileFragment),
+        createOrUpdateIosAppBuildCredentialsAsync: jest.fn(() => testBuildCreds),
+      },
+    });
+    const appLookupParams = ManageIosBeta.getAppLookupParamsFromContext(ctx);
+    const setupBuildCredentialsFromCredentialsJsonAction = new SetupBuildCredentialsFromCredentialsJson(
+      appLookupParams,
+      IosDistributionType.AppStore
+    );
+    await setupBuildCredentialsFromCredentialsJsonAction.runAsync(ctx);
+
+    // expect build credentials not to be created or updated on expo servers
+    expect((ctx.newIos.createOrUpdateIosAppBuildCredentialsAsync as any).mock.calls.length).toBe(0);
+    // expect distribution certificate not to be uploaded on expo servers
+    expect((ctx.newIos.createDistributionCertificateAsync as any).mock.calls.length).toBe(0);
+    // expect provisioning profile not to be uploaded on expo servers
+    expect((ctx.newIos.createProvisioningProfileAsync as any).mock.calls.length).toBe(0);
+  });
+  it('sets up build credentials with no prior configuration in Interactive Mode', async () => {
+    const ctx = createCtxMock({
+      newIos: {
+        ...getNewIosApiMockWithoutCredentials(),
+        getIosAppCredentialsWithBuildCredentialsAsync: jest.fn(() => null),
+        createOrGetExistingAppleAppIdentifierAsync: jest.fn(() => testAppleAppIdentifierFragment),
+        createDistributionCertificateAsync: jest.fn(() => ({ testDistCertFragmentNoDependencies })),
+        createProvisioningProfileAsync: jest.fn(() => testProvisioningProfileFragment),
+        createOrUpdateIosAppBuildCredentialsAsync: jest.fn(
+          () => testIosAppCredentialsWithBuildCredentialsQueryResult
+        ),
+      },
+    });
+    const appLookupParams = ManageIosBeta.getAppLookupParamsFromContext(ctx);
+    const setupBuildCredentialsFromCredentialsJsonAction = new SetupBuildCredentialsFromCredentialsJson(
+      appLookupParams,
+      IosDistributionType.AppStore
+    );
+    await setupBuildCredentialsFromCredentialsJsonAction.runAsync(ctx);
+
+    // expect build credentials to be created or updated on expo servers
+    expect((ctx.newIos.createOrUpdateIosAppBuildCredentialsAsync as any).mock.calls.length).toBe(1);
+    // expect distribution certificate to be uploaded on expo servers
+    expect((ctx.newIos.createDistributionCertificateAsync as any).mock.calls.length).toBe(1);
+    // expect provisioning profile to be uploaded on expo servers
+    expect((ctx.newIos.createProvisioningProfileAsync as any).mock.calls.length).toBe(1);
+  });
+  it('sets up build credentials with different prior configuration in Interactive Mode', async () => {
+    // create deep clone the quick and dirty way
+    const testBuildCreds = JSON.parse(
+      JSON.stringify(testIosAppCredentialsWithBuildCredentialsQueryResult)
+    );
+    testBuildCreds.iosAppBuildCredentialsArray[0].distributionCertificate.certificateP12 =
+      'something different so SetupBuildCredentialsFromCredentialsJson doesnt think we want to exact the same cert and skip upload to expo servers';
+    const ctx = createCtxMock({
+      newIos: {
+        ...getNewIosApiMockWithoutCredentials(),
+        getIosAppCredentialsWithBuildCredentialsAsync: jest.fn(() => testBuildCreds),
+        createOrGetExistingAppleAppIdentifierAsync: jest.fn(() => testAppleAppIdentifierFragment),
+        createDistributionCertificateAsync: jest.fn(() => ({ testDistCertFragmentNoDependencies })),
+        createProvisioningProfileAsync: jest.fn(() => testProvisioningProfileFragment),
+        createOrUpdateIosAppBuildCredentialsAsync: jest.fn(() => testBuildCreds),
+      },
+    });
+    const appLookupParams = ManageIosBeta.getAppLookupParamsFromContext(ctx);
+    const setupBuildCredentialsFromCredentialsJsonAction = new SetupBuildCredentialsFromCredentialsJson(
+      appLookupParams,
+      IosDistributionType.AppStore
+    );
+    await setupBuildCredentialsFromCredentialsJsonAction.runAsync(ctx);
+
+    // expect build credentials to be created or updated on expo servers
+    expect((ctx.newIos.createOrUpdateIosAppBuildCredentialsAsync as any).mock.calls.length).toBe(1);
+    // expect distribution certificate to be uploaded on expo servers
+    expect((ctx.newIos.createDistributionCertificateAsync as any).mock.calls.length).toBe(1);
+    // expect provisioning profile to be uploaded on expo servers
+    expect((ctx.newIos.createProvisioningProfileAsync as any).mock.calls.length).toBe(1);
+  });
+  it('works in Non Interactive Mode - sets up build credentials with different prior configuration', async () => {
+    // create deep clone the quick and dirty way
+    const testBuildCreds = JSON.parse(
+      JSON.stringify(testIosAppCredentialsWithBuildCredentialsQueryResult)
+    );
+    testBuildCreds.iosAppBuildCredentialsArray[0].distributionCertificate.certificateP12 =
+      'something different so SetupBuildCredentialsFromCredentialsJson doesnt think we want to exact the same cert and skip upload to expo servers';
+    const ctx = createCtxMock({
+      nonInteractive: true,
+      newIos: {
+        ...getNewIosApiMockWithoutCredentials(),
+        getIosAppCredentialsWithBuildCredentialsAsync: jest.fn(() => testBuildCreds),
+        createOrGetExistingAppleAppIdentifierAsync: jest.fn(() => testAppleAppIdentifierFragment),
+        createDistributionCertificateAsync: jest.fn(() => ({ testDistCertFragmentNoDependencies })),
+        createProvisioningProfileAsync: jest.fn(() => testProvisioningProfileFragment),
+        createOrUpdateIosAppBuildCredentialsAsync: jest.fn(() => testBuildCreds),
+      },
+    });
+    const appLookupParams = ManageIosBeta.getAppLookupParamsFromContext(ctx);
+    const setupBuildCredentialsFromCredentialsJsonAction = new SetupBuildCredentialsFromCredentialsJson(
+      appLookupParams,
+      IosDistributionType.AppStore
+    );
+    await setupBuildCredentialsFromCredentialsJsonAction.runAsync(ctx);
+
+    // expect no prompts to be called
+    expect((confirmAsync as any).mock.calls.length).toBe(0);
+    // expect build credentials to be created or updated on expo servers
+    expect((ctx.newIos.createOrUpdateIosAppBuildCredentialsAsync as any).mock.calls.length).toBe(1);
+    // expect distribution certificate to be uploaded on expo servers
+    expect((ctx.newIos.createDistributionCertificateAsync as any).mock.calls.length).toBe(1);
+    // expect provisioning profile to be uploaded on expo servers
+    expect((ctx.newIos.createProvisioningProfileAsync as any).mock.calls.length).toBe(1);
+  });
+});
