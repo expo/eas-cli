9a1538fb4d4843543071dc95bed3e2ca664f25eb
jkh <jonathan@expo.io>
2021-04-14 14:04:58 -0400

    add eas update:view [ID] (#327)

---
+++ packages/eas-cli/src/commands/update/view.ts
@@ -0,0 +1,99 @@
+import { Command, flags } from '@oclif/command';
+import Table from 'cli-table3';
+import gql from 'graphql-tag';
+
+import { graphqlClient, withErrorHandlingAsync } from '../../graphql/client';
+import { UpdatesByGroupQuery, UpdatesByGroupQueryVariables } from '../../graphql/generated';
+import Log from '../../log';
+import { UPDATE_COLUMNS, formatUpdate } from '../branch/list';
+
+export async function viewUpdateAsync({
+  groupId,
+}: {
+  groupId: string;
+}): Promise<UpdatesByGroupQuery> {
+  const data = await withErrorHandlingAsync(
+    graphqlClient
+      .query<UpdatesByGroupQuery, UpdatesByGroupQueryVariables>(
+        gql`
+          query UpdatesByGroup($groupId: ID!) {
+            updatesByGroup(group: $groupId) {
+              id
+              group
+              runtimeVersion
+              platform
+              message
+              actor {
+                id
+                ... on User {
+                  username
+                }
+                ... on Robot {
+                  firstName
+                }
+              }
+              createdAt
+            }
+          }
+        `,
+        {
+          groupId,
+        }
+      )
+      .toPromise()
+  );
+  if (data.updatesByGroup.length === 0) {
+    throw new Error(`Could not find any updates with group ID: "${groupId}"`);
+  }
+  return data;
+}
+export default class UpdateView extends Command {
+  static hidden = true;
+  static description = 'Update group details.';
+
+  static args = [
+    {
+      name: 'groupId',
+      required: true,
+      description: 'The ID of an update group.',
+    },
+  ];
+
+  static flags = {
+    json: flags.boolean({
+      description: `Return a json with the updates belonging to the group.`,
+      default: false,
+    }),
+  };
+
+  async run() {
+    const {
+      args: { groupId },
+      flags: { json: jsonFlag },
+    } = this.parse(UpdateView);
+
+    const { updatesByGroup } = await viewUpdateAsync({ groupId });
+
+    if (jsonFlag) {
+      Log.log(JSON.stringify(updatesByGroup));
+      return;
+    }
+
+    const groupTable = new Table({
+      head: [...UPDATE_COLUMNS, 'platforms'],
+      wordWrap: true,
+    });
+
+    // The relevant info of the updates are same except for their platform
+    const platforms = updatesByGroup.map(update => update.platform);
+    const representativeUpdate = updatesByGroup[0];
+    groupTable.push([
+      formatUpdate(representativeUpdate),
+      representativeUpdate.runtimeVersion,
+      representativeUpdate.group,
+      platforms.join(','),
+    ]);
+
+    Log.log(groupTable.toString());
+  }
+}
