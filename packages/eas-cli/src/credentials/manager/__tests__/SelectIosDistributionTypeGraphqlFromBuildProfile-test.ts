0f286061e4c75763b37f2f8f3919aafca27d4fe8
Quinlan Jung <quin@expo.io>
2021-04-20 14:13:44 -0700

    [eas-cli] tests for SelectIosDistributionTypeGraphqlFromBuildProfile

---
+++ packages/eas-cli/src/credentials/manager/__tests__/SelectIosDistributionTypeGraphqlFromBuildProfile-test.ts
@@ -0,0 +1,127 @@
+import { EasConfig } from '@expo/eas-json';
+
+import {
+  CommonIosAppCredentialsFragment,
+  IosDistributionType as IosDistributionTypeGraphql,
+} from '../../../graphql/generated';
+import { createCtxMock } from '../../__tests__/fixtures-context';
+import { testIosAppCredentialsWithBuildCredentialsQueryResult } from '../../__tests__/fixtures-ios';
+import { SelectIosDistributionTypeGraphqlFromBuildProfile } from '../SelectIosDistributionTypeGraphqlFromBuildProfile';
+
+describe('SelectIosDistributionTypeGraphqlFromBuildProfile', () => {
+  it('errors with a simulator distribution', async () => {
+    const easConfig = {
+      builds: {
+        ios: {
+          distribution: 'simulator',
+        },
+      },
+    } as EasConfig;
+
+    const ctx = createCtxMock({
+      nonInteractive: false,
+    });
+    const selectIosDistributionTypeGraphqlFromBuildProfileAction = new SelectIosDistributionTypeGraphqlFromBuildProfile(
+      easConfig
+    );
+    await expect(
+      selectIosDistributionTypeGraphqlFromBuildProfileAction.runAsync(
+        ctx,
+        /* iosAppCredentials */ null
+      )
+    ).rejects.toThrowError();
+  });
+  it('returns APP_STORE type with a store distribution', async () => {
+    const easConfig = {
+      builds: {
+        ios: {
+          distribution: 'store',
+        },
+      },
+    } as EasConfig;
+
+    const ctx = createCtxMock({
+      nonInteractive: false,
+    });
+    const selectIosDistributionTypeGraphqlFromBuildProfileAction = new SelectIosDistributionTypeGraphqlFromBuildProfile(
+      easConfig
+    );
+    const iosDistributionTypeGraphql = await selectIosDistributionTypeGraphqlFromBuildProfileAction.runAsync(
+      ctx,
+      /* iosAppCredentials */ null
+    );
+    expect(iosDistributionTypeGraphql).toBe(IosDistributionTypeGraphql.AppStore);
+  });
+  it('returns ENTERPRISE type with an internal distribution with the universal provisioningEnterprise configuration', async () => {
+    const easConfig = {
+      builds: {
+        ios: {
+          distribution: 'internal',
+          enterpriseProvisioning: 'universal',
+        },
+      },
+    } as EasConfig;
+
+    const ctx = createCtxMock({
+      nonInteractive: false,
+    });
+    const selectIosDistributionTypeGraphqlFromBuildProfileAction = new SelectIosDistributionTypeGraphqlFromBuildProfile(
+      easConfig
+    );
+    const iosDistributionTypeGraphql = await selectIosDistributionTypeGraphqlFromBuildProfileAction.runAsync(
+      ctx,
+      /* iosAppCredentials */ null
+    );
+    expect(iosDistributionTypeGraphql).toBe(IosDistributionTypeGraphql.Enterprise);
+  });
+  it('returns ADHOC type with an internal distribution with the adhoc provisioningEnterprise configuration', async () => {
+    const easConfig = {
+      builds: {
+        ios: {
+          distribution: 'internal',
+          enterpriseProvisioning: 'adhoc',
+        },
+      },
+    } as EasConfig;
+
+    const ctx = createCtxMock({
+      nonInteractive: false,
+    });
+    const selectIosDistributionTypeGraphqlFromBuildProfileAction = new SelectIosDistributionTypeGraphqlFromBuildProfile(
+      easConfig
+    );
+    const iosDistributionTypeGraphql = await selectIosDistributionTypeGraphqlFromBuildProfileAction.runAsync(
+      ctx,
+      /* iosAppCredentials */ null
+    );
+    expect(iosDistributionTypeGraphql).toBe(IosDistributionTypeGraphql.AdHoc);
+  });
+  it('extrapolates type for an internal distribution with ambiguous configuration', async () => {
+    const easConfig = {
+      builds: {
+        ios: {
+          distribution: 'internal',
+        },
+      },
+    } as EasConfig;
+
+    const ctx = createCtxMock({
+      nonInteractive: false,
+    });
+    const selectIosDistributionTypeGraphqlFromBuildProfileAction = new SelectIosDistributionTypeGraphqlFromBuildProfile(
+      easConfig
+    );
+
+    // create deep clone the quick and dirty way
+    const testAppCredentials = JSON.parse(
+      JSON.stringify(testIosAppCredentialsWithBuildCredentialsQueryResult)
+    ) as CommonIosAppCredentialsFragment;
+    testAppCredentials.iosAppBuildCredentialsArray[0].iosDistributionType =
+      IosDistributionTypeGraphql.AdHoc;
+    const iosDistributionTypeGraphql = await selectIosDistributionTypeGraphqlFromBuildProfileAction.runAsync(
+      ctx,
+      testAppCredentials
+    );
+    expect(iosDistributionTypeGraphql).toBe(IosDistributionTypeGraphql.AdHoc);
+  });
+});
