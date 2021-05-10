3601b448fe7f8926c318adef3ba84837827fde38
Quinlan Jung <quin@expo.io>
2021-05-10 16:38:17 -0700

    [eas-cli] create keystore action (#387)
    * [eas-cli] create android keystore action
    
    * 1: some pr feedback

---
+++ packages/eas-cli/src/credentials/android/api/graphql/mutations/AndroidKeystoreMutation.ts
@@ -0,0 +1,59 @@
+import assert from 'assert';
+import { print } from 'graphql';
+import gql from 'graphql-tag';
+
+import { graphqlClient, withErrorHandlingAsync } from '../../../../../graphql/client';
+import {
+  AndroidKeystoreFragment,
+  AndroidKeystoreType,
+  CreateAndroidKeystoreMutation,
+} from '../../../../../graphql/generated';
+import { AndroidKeystoreFragmentNode } from '../../../../../graphql/types/credentials/AndroidKeystore';
+
+const AndroidKeystoreMutation = {
+  async createAndroidKeystore(
+    androidKeystoreInput: {
+      base64EncodedKeystore: string;
+      keystorePassword: string;
+      keyAlias: string;
+      keyPassword?: string;
+      type: AndroidKeystoreType;
+    },
+    accountId: string
+  ): Promise<AndroidKeystoreFragment> {
+    const data = await withErrorHandlingAsync(
+      graphqlClient
+        .mutation<CreateAndroidKeystoreMutation>(
+          gql`
+            mutation CreateAndroidKeystoreMutation(
+              $androidKeystoreInput: AndroidKeystoreInput!
+              $accountId: ID!
+            ) {
+              androidKeystore {
+                createAndroidKeystore(
+                  androidKeystoreInput: $androidKeystoreInput
+                  accountId: $accountId
+                ) {
+                  id
+                  ...AndroidKeystoreFragment
+                }
+              }
+            }
+            ${print(AndroidKeystoreFragmentNode)}
+          `,
+          {
+            androidKeystoreInput,
+            accountId,
+          }
+        )
+        .toPromise()
+    );
+    assert(
+      data.androidKeystore.createAndroidKeystore,
+      'GraphQL: `createAndroidKeystore` not defined in server response'
+    );
+    return data.androidKeystore.createAndroidKeystore;
+  },
+};
+
+export { AndroidKeystoreMutation };
