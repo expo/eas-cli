75bf6316e580f50dd2c8954a8c9511907a91e831
Kirby Cofino <kirbygc00@gmail.com>
2021-06-03 11:01:41 -0700

    renamed test files, added in test for calling logevent

--- packages/eas-cli/src/__tests__/commands/TestAuthenticatedCommand.ts
+++ packages/eas-cli/src/__tests__/commands/TestAuthenticatedCommand.ts
@@ -2,6 +2,6 @@ import AuthenticatedCommand from '../../commands/abstract/authenticatedCommand';
 
 // Kirby - cannot be in the commands folder or oclif will pick it up as a valid command, prepack + test will fail
 // seems there is some pattern to exclude https://github.com/oclif/config/blob/master/src/plugin.ts#L230 but i couldn't get it to work
-export default class TestAuthrizedCommand extends AuthenticatedCommand {
+export default class TestAuthenticatedCommand extends AuthenticatedCommand {
   async run() {}
 }
--- packages/eas-cli/src/__tests__/commands/authCommand-test.ts
+++ packages/eas-cli/src/__tests__/commands/authCommand-test.ts
@@ -1,8 +1,8 @@
-import { flushAsync, initAsync } from '../../analytics';
+import { flushAsync, initAsync, logEvent } from '../../analytics';
 import AuthenticatedCommand from '../../commands/abstract/authenticatedCommand';
 import { jester as mockJester } from '../../credentials/__tests__/fixtures-constants';
 import { ensureLoggedInAsync } from '../../user/actions';
-import TestAuthrizedCommand from './TestAuthrizedCommand';
+import TestAuthenticatedCommand from './TestAuthenticatedCommand';
 
 describe(AuthenticatedCommand.name, () => {
   beforeAll(() => {
@@ -22,32 +22,41 @@ describe(AuthenticatedCommand.name, () => {
     (ensureLoggedInAsync as jest.Mock).mockClear();
     (initAsync as jest.Mock).mockClear();
     (flushAsync as jest.Mock).mockClear();
+    (logEvent as jest.Mock).mockClear();
   });
 
   describe('without exceptions', () => {
     it('ensures the user is logged in', async () => {
-      await TestAuthrizedCommand.run();
+      await TestAuthenticatedCommand.run();
 
       expect(ensureLoggedInAsync).toHaveReturnedWith(mockJester);
     });
 
     it('initializes analytics', async () => {
-      await TestAuthrizedCommand.run();
+      await TestAuthenticatedCommand.run();
 
       expect(initAsync).toHaveBeenCalled();
     });
 
     it('flushes analytics', async () => {
-      await TestAuthrizedCommand.run();
+      await TestAuthenticatedCommand.run();
 
       expect(flushAsync).toHaveBeenCalled();
+    });
+
+    it('logs events', async () => {
+      await TestAuthenticatedCommand.run();
+
+      expect(logEvent).toHaveBeenCalledWith('action', {
+        action: `eas ${TestAuthenticatedCommand.id}`,
+      });
     });
   });
 
   describe('after exceptions', () => {
     it('flushes analytics', async () => {
       try {
-        await TestAuthrizedCommand.run().then(_ => {
+        await TestAuthenticatedCommand.run().then(_ => {
           throw new Error('foo');
         });
       } catch (error) {}
