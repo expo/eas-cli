dcd555fefeab2a8af317a11db9e61a683c9aa0e6
Quinlan Jung <quin@expo.io>
2021-06-25 16:46:51 -0700

    pr feedback

--- packages/eas-cli/src/build/ios/credentials.ts
+++ packages/eas-cli/src/build/ios/credentials.ts
@@ -1,15 +1,10 @@
-import { CredentialsSource } from '@expo/eas-json';
-
 import { createCredentialsContextAsync } from '../../credentials/context';
 import IosCredentialsProvider from '../../credentials/ios/IosCredentialsProvider';
 import { getAppFromContext } from '../../credentials/ios/actions/BuildCredentialsUtils';
-import { SetupPushKey } from '../../credentials/ios/actions/SetupPushKey';
+import { SetupPushKeyForMultitarget } from '../../credentials/ios/actions/SetupPushKeyForMultitarget';
 import { resolveEntitlementsJsonAsync } from '../../credentials/ios/appstore/entitlements';
 import { IosCredentials, Target } from '../../credentials/ios/types';
 import { CommonIosAppCredentialsFragment } from '../../graphql/generated';
-import Log from '../../log';
-import { findApplicationTarget } from '../../project/ios/target';
-import { confirmAsync } from '../../prompts';
 import { CredentialsResult } from '../build';
 import { BuildContext } from '../context';
 import { Platform } from '../types';
@@ -59,13 +54,9 @@ export async function setupPushKeyAsync(
    * Abort push key setup if:
    * - build doesn't require credentials (ie) simulator
    * - build is triggered programmatically: a human should be aware push notifications are being setup
-   * - user has specified credentials should only be retrieved from credentials.json: they can manually upload to www later
    */
-  const { credentialsSource } = buildCtx.buildProfile;
   const shouldAbortSetup =
-    !shouldProvideCredentials(buildCtx) ||
-    buildCtx.commandCtx.nonInteractive ||
-    credentialsSource === CredentialsSource.LOCAL;
+    !shouldProvideCredentials(buildCtx) || buildCtx.commandCtx.nonInteractive;
   if (shouldAbortSetup) {
     return null;
   }
@@ -75,31 +66,7 @@ export async function setupPushKeyAsync(
   const ctx = await createCredentialsContextAsync(projectDir, {
     nonInteractive: buildCtx.commandCtx.nonInteractive,
   });
-
-  const applicationTarget = findApplicationTarget(targets);
-  const app = getAppFromContext(ctx);
-  const appLookupParams = {
-    ...app,
-    bundleIdentifier: applicationTarget.bundleIdentifier,
-    parentBundleIdentifier: applicationTarget.parentBundleIdentifier,
-  };
-
-  const setupPushKeyAction = await new SetupPushKey(appLookupParams);
-  const isPushKeySetup = await setupPushKeyAction.isPushKeySetupAsync(ctx);
-  if (isPushKeySetup) {
-    Log.succeed(
-      `Push Notifications setup for ${app.projectName}:${applicationTarget.bundleIdentifier}`
-    );
-    return null;
-  }
-
-  const confirmSetup = await confirmAsync({
-    message: `Would you like to setup Push Notifications for your project?`,
-  });
-  if (!confirmSetup) {
-    return null;
-  }
-  return await new SetupPushKey(appLookupParams).runAsync(ctx);
+  return await new SetupPushKeyForMultitarget().runAsync(ctx, targets);
 }
 
 function shouldProvideCredentials(buildCtx: BuildContext<Platform.IOS>): boolean {
