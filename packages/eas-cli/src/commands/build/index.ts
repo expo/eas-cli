330aa5cb0131c3ed528890bf1eb92c21c6afd7d1
Brent Vatne <brentvatne@gmail.com>
2021-06-25 16:41:00 -0700

    [eas-cli] Ensure messaging is consistent with what we are configuring

--- packages/eas-cli/src/commands/build/index.ts
+++ packages/eas-cli/src/commands/build/index.ts
@@ -152,9 +152,15 @@ async function ensureProjectConfiguredAsync(
     return null;
   }
 
-  const confirm = await confirmAsync({
-    message: 'This app is not set up for building with EAS. Set it up now?',
-  });
+  // Ensure the prompt is consistent with the platforms we need to configure
+  let message = 'This app is not configured to build with EAS. Set it up now?';
+  if (platformsToConfigure === RequestedPlatform.Ios) {
+    message = 'Your iOS app is not configured to build with EAS. Set it up now?';
+  } else if (platformsToConfigure === RequestedPlatform.Android) {
+    message = 'Your Android app is not configured to build with EAS. Set it up now?';
+  }
+
+  const confirm = await confirmAsync({ message });
   if (confirm) {
     await configureAsync({
       projectDir,
