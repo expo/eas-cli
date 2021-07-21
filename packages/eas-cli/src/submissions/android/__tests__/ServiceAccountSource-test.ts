139dc94523310a6b788e4a3456239cc03c05936f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-07-21 08:53:24 +0200

    add tests

--- packages/eas-cli/src/submissions/android/__tests__/ServiceAccountSource-test.ts
+++ packages/eas-cli/src/submissions/android/__tests__/ServiceAccountSource-test.ts
@@ -1,6 +1,7 @@
 import { vol } from 'memfs';
 
 import { asMock } from '../../../__tests__/utils';
+import { findProjectRootAsync } from '../../../project/projectUtils';
 import { promptAsync } from '../../../prompts';
 import {
   ServiceAccountSource,
@@ -10,11 +11,14 @@ import {
 
 jest.mock('fs');
 jest.mock('../../../prompts');
+jest.mock('../../../project/projectUtils');
 
 describe(getServiceAccountAsync, () => {
   beforeAll(() => {
     vol.fromJSON({
       '/google-service-account.json': JSON.stringify({ service: 'account' }),
+      '/valid-service-account.json': JSON.stringify({ type: 'service_account' }),
+      '/other_dir/invalid_file.txt': 'this is not even a JSON',
     });
   });
   afterAll(() => {
@@ -87,6 +91,57 @@ describe(getServiceAccountAsync, () => {
       };
       const serviceAccountPath = await getServiceAccountAsync(source);
       expect(promptAsync).toHaveBeenCalledTimes(3);
+      expect(serviceAccountPath).toBe('/google-service-account.json');
+    });
+  });
+
+  describe('when source is ServiceAccountSourceType.detect', () => {
+    it('detects a valid google-services file', async () => {
+      asMock(findProjectRootAsync).mockResolvedValueOnce('/');
+      asMock(promptAsync).mockResolvedValueOnce({
+        confirmed: true,
+      });
+      const source: ServiceAccountSource = {
+        sourceType: ServiceAccountSourceType.detect,
+      };
+
+      const serviceAccountPath = await getServiceAccountAsync(source);
+
+      expect(promptAsync).toHaveBeenCalledTimes(1);
+      expect(serviceAccountPath).toBe('/valid-service-account.json');
+    });
+
+    it('falls back to prompt, when no valid files are found in the dir', async () => {
+      asMock(findProjectRootAsync).mockResolvedValueOnce('/other_dir');
+      asMock(promptAsync).mockResolvedValueOnce({
+        filePath: '/google-service-account.json',
+      });
+      const source: ServiceAccountSource = {
+        sourceType: ServiceAccountSourceType.detect,
+      };
+
+      const serviceAccountPath = await getServiceAccountAsync(source);
+
+      expect(promptAsync).toHaveBeenCalledTimes(1);
+      expect(serviceAccountPath).toBe('/google-service-account.json');
+    });
+
+    it('falls back to prompt, when user rejects to use detected file', async () => {
+      asMock(findProjectRootAsync).mockResolvedValueOnce('/');
+      asMock(promptAsync)
+        .mockResolvedValueOnce({
+          confirmed: false,
+        })
+        .mockResolvedValueOnce({
+          filePath: '/google-service-account.json',
+        });
+      const source: ServiceAccountSource = {
+        sourceType: ServiceAccountSourceType.detect,
+      };
+
+      const serviceAccountPath = await getServiceAccountAsync(source);
+
+      expect(promptAsync).toHaveBeenCalledTimes(2);
       expect(serviceAccountPath).toBe('/google-service-account.json');
     });
   });
