573af1db13116ca405d48564621b6628ecc509b9
Dominik Sokal <dominik.sokal@swmansion.com>
2021-07-28 10:31:44 +0200

    address PR feedback

--- packages/eas-cli/src/project/metroConfig.ts
+++ packages/eas-cli/src/project/metroConfig.ts
@@ -1,6 +1,7 @@
-import * as MetroConfig from '@expo/metro-config';
 import { exit } from '@oclif/errors';
 import chalk from 'chalk';
+import type MetroConfig from 'metro-config';
+import resolveFrom from 'resolve-from';
 
 import { BuildContext } from '../build/context';
 import { Platform } from '../build/types';
@@ -8,11 +9,11 @@ import Log, { learnMore } from '../log';
 import { confirmAsync } from '../prompts';
 
 export async function validateMetroConfigForManagedWorkflowAsync(ctx: BuildContext<Platform>) {
-  if (!(await MetroConfig.existsAsync(ctx.projectDir))) {
+  if (!(await configExistsAsync(ctx.projectDir))) {
     return;
   }
 
-  const metroConfig = await MetroConfig.loadAsync(ctx.projectDir, { withoutDefaults: true });
+  const metroConfig = await loadConfigAsync(ctx.projectDir);
   const hasHashAssetFilesPlugin = metroConfig.transformer?.assetPlugins?.find((plugin: string) =>
     plugin.match(/expo-asset\/tools\/hashAssetFiles/)
   );
@@ -41,3 +42,37 @@ export async function validateMetroConfigForManagedWorkflowAsync(ctx: BuildConte
     }
   }
 }
+
+function importMetroConfigFromProject(projectDir: string): typeof MetroConfig {
+  const resolvedPath = resolveFrom.silent(projectDir, 'metro-config');
+  if (!resolvedPath) {
+    throw new MetroConfigPackageMissingError(
+      'Missing package "metro-config" in the project. ' +
+        'This usually means `react-native` is not installed. ' +
+        'Please verify that dependencies in package.json include "react-native" ' +
+        'and run `yarn` or `npm install`.'
+    );
+  }
+  return require(resolvedPath);
+}
+
+async function configExistsAsync(projectRoot: string): Promise<boolean> {
+  try {
+    const MetroConfig = importMetroConfigFromProject(projectRoot);
+    const result = await MetroConfig.resolveConfig(undefined, projectRoot);
+    return !result.isEmpty;
+  } catch (err) {
+    if (err instanceof MetroConfigPackageMissingError) {
+      return false;
+    } else {
+      throw err;
+    }
+  }
+}
+
+async function loadConfigAsync(projectDir: string): Promise<MetroConfig.ConfigT> {
+  const MetroConfig = importMetroConfigFromProject(projectDir);
+  return await MetroConfig.loadConfig({ cwd: projectDir }, {});
+}
+
+class MetroConfigPackageMissingError extends Error {}
