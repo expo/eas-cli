094c04c7f07ba1f1ee799fafb1acbdeb4e22599a
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-08-19 14:58:19 +0200

    [eas-cli] support --json flag for build commands (#567)
    * [eas-cli] support --json flag for build commands
    
    * sanitize json object
    
    * update CHANGELOG
    
    * Apply suggestions from code review
    
    Co-authored-by: Dominik Sokal <dominik.sokal@swmansion.com>
    
    Co-authored-by: Dominik Sokal <dominik.sokal@swmansion.com>

---
+++ packages/eas-cli/src/utils/json.ts
@@ -0,0 +1,43 @@
+import assert from 'assert';
+
+import Log from '../log';
+
+let stdoutWrite: NodeJS.WriteStream['write'] | undefined;
+
+export function enableJsonOutput() {
+  if (stdoutWrite) {
+    return;
+  }
+  stdoutWrite = process.stdout.write;
+  process.stdout.write = (...args: any) => {
+    return process.stderr.write.call(null, args);
+  };
+}
+
+export function printJsonOnlyOutput(value: object): void {
+  assert(stdoutWrite, 'this should only be called with --json flag');
+  try {
+    process.stdout.write = stdoutWrite;
+    Log.log(JSON.stringify(sanitizeValue(value), null, 2));
+  } finally {
+    process.stdout.write = (...args: any) => {
+      return process.stderr.write.call(null, args);
+    };
+  }
+}
+
+function sanitizeValue(value: any): unknown {
+  if (Array.isArray(value)) {
+    return value.map(val => sanitizeValue(val));
+  } else if (value && typeof value === 'object') {
+    const result: Record<string, any> = {};
+    Object.keys(value).forEach(key => {
+      if (key !== '__typename' && value[key] !== null) {
+        result[key] = sanitizeValue(value[key]);
+      }
+    });
+    return result;
+  } else {
+    return value;
+  }
+}
