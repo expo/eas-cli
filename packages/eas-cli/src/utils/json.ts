f21bd79dcec0060bd492903a96ebda6f5079f8ce
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2021-08-19 10:54:52 +0200

    sanitize json object

--- packages/eas-cli/src/utils/json.ts
+++ packages/eas-cli/src/utils/json.ts
@@ -18,10 +18,26 @@ export function printJsonOnlyOutput(value: object): void {
   assert(stdoutWrite, 'this should only be called with --json flag');
   try {
     process.stdout.write = stdoutWrite;
-    Log.log(JSON.stringify(value, null, 2));
+    Log.log(JSON.stringify(sanitizeValue(value), null, 2));
   } finally {
     process.stdout.write = (...args: any) => {
       return process.stderr.write.call(null, args);
     };
+  }
+}
+
+function sanitizeValue(value: any): unknown {
+  if (Array.isArray(value)) {
+    return value.map(val => sanitizeValue(val));
+  } else if (value && typeof value === 'object') {
+    const result: Record<string, any> = {};
+    Object.keys(value).forEach(key => {
+      if (key !== '__typename' && value[key] !== null) {
+        result[key] = sanitizeValue(value[key]);
+      }
+    });
+    return result;
+  } else {
+    return value;
   }
 }
