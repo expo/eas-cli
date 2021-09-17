e9cbb41a50212006a9238b2f8be3382fa86bd1e0
Dominik Sokal <dominik.sokal@swmansion.com>
2021-09-17 12:40:10 +0200

    move expo-cli runner to utils

---
+++ packages/eas-cli/src/utils/expoCommand.ts
@@ -0,0 +1,29 @@
+import spawnAsync from '@expo/spawn-async';
+import chalk from 'chalk';
+import resolveFrom from 'resolve-from';
+
+import Log from '../log';
+
+export async function expoCommandAsync(projectDir: string, args: string[]): Promise<void> {
+  const expoCliPath = resolveFrom(projectDir, 'expo/bin/cli.js');
+  const spawnPromise = spawnAsync(expoCliPath, args, {
+    stdio: ['inherit', 'pipe', 'pipe'], // inherit stdin so user can install a missing expo-cli from inside this command
+  });
+  const {
+    child: { stdout, stderr },
+  } = spawnPromise;
+  if (!(stdout && stderr)) {
+    throw new Error('Failed to spawn expo-cli');
+  }
+  stdout.on('data', data => {
+    for (const line of data.toString().trim().split('\n')) {
+      Log.log(`${chalk.gray('[expo-cli]')} ${line}`);
+    }
+  });
+  stderr.on('data', data => {
+    for (const line of data.toString().trim().split('\n')) {
+      Log.warn(`${chalk.gray('[expo-cli]')} ${line}`);
+    }
+  });
+  await spawnPromise;
+}
