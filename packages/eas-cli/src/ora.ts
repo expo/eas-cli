4a70edb9d80088930880e43c3cb1b0b395c9be0f
Dominik Sokal <dominik.sokal@swmansion.com>
2021-09-20 15:40:14 +0200

    address PR feedback

--- packages/eas-cli/src/ora.ts
+++ packages/eas-cli/src/ora.ts
@@ -2,6 +2,17 @@ import oraReal, { Options, Ora } from 'ora';
 
 import Log from './log';
 
+export { Ora, Options };
+
+// eslint-disable-next-line no-console
+const logReal = console.log;
+// eslint-disable-next-line no-console
+const infoReal = console.info;
+// eslint-disable-next-line no-console
+const warnReal = console.warn;
+// eslint-disable-next-line no-console
+const errorReal = console.error;
+
 /**
  * A custom ora spinner that sends the stream to stdout in CI, or non-TTY, instead of stderr (the default).
  *
@@ -19,35 +30,28 @@ export function ora(options?: Options | string): Ora {
     ...inputOptions,
   });
 
-  // eslint-disable-next-line no-console
-  const logReal = console.log;
-  // eslint-disable-next-line no-console
-  const infoReal = console.info;
-  // eslint-disable-next-line no-console
-  const warnReal = console.warn;
-  // eslint-disable-next-line no-console
-  const errorReal = console.error;
-
+  const oraStart = spinner.start.bind(spinner);
   const oraStop = spinner.stop.bind(spinner);
-
   const oraStopAndPersist = spinner.stopAndPersist.bind(spinner);
 
-  const logWrap = (method: any, args: any[]) => {
+  const logWrap = (method: any, args: any[]): void => {
     oraStop();
     method(...args);
     spinner.start();
   };
 
-  // eslint-disable-next-line no-console
-  console.log = (...args: any) => logWrap(logReal, args);
-  // eslint-disable-next-line no-console
-  console.info = (...args: any) => logWrap(infoReal, args);
-  // eslint-disable-next-line no-console
-  console.warn = (...args: any) => logWrap(warnReal, args);
-  // eslint-disable-next-line no-console
-  console.error = (...args: any) => logWrap(errorReal, args);
+  const wrapNativeLogs = (): void => {
+    // eslint-disable-next-line no-console
+    console.log = (...args: any) => logWrap(logReal, args);
+    // eslint-disable-next-line no-console
+    console.info = (...args: any) => logWrap(infoReal, args);
+    // eslint-disable-next-line no-console
+    console.warn = (...args: any) => logWrap(warnReal, args);
+    // eslint-disable-next-line no-console
+    console.error = (...args: any) => logWrap(errorReal, args);
+  };
 
-  const resetNativeLogs = () => {
+  const resetNativeLogs = (): void => {
     // eslint-disable-next-line no-console
     console.log = logReal;
     // eslint-disable-next-line no-console
@@ -58,19 +62,22 @@ export function ora(options?: Options | string): Ora {
     console.error = errorReal;
   };
 
+  spinner.start = (): Ora => {
+    wrapNativeLogs();
+    return oraStart();
+  };
+
   spinner.stopAndPersist = (): Ora => {
-    oraStopAndPersist();
+    const result = oraStopAndPersist();
     resetNativeLogs();
-    return spinner;
+    return result;
   };
 
   spinner.stop = (): Ora => {
-    oraStop();
+    const result = oraStop();
     resetNativeLogs();
-    return spinner;
+    return result;
   };
 
   return spinner;
 }
-
-export { Ora, Options };
