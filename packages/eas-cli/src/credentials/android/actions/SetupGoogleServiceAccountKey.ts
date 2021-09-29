0d555f83b36107137a71e0d318e33d566602446c
Quinlan Jung <quin@expo.io>
2021-09-28 22:25:59 -0700

    lint

--- packages/eas-cli/src/credentials/android/actions/SetupGoogleServiceAccountKey.ts
+++ packages/eas-cli/src/credentials/android/actions/SetupGoogleServiceAccountKey.ts
@@ -1,44 +1,61 @@
 import nullthrows from 'nullthrows';
-import {  CommonAndroidAppCredentialsFragment, GoogleServiceAccountKeyFragment } from '../../../graphql/generated';
+
+import {
+  CommonAndroidAppCredentialsFragment,
+  GoogleServiceAccountKeyFragment,
+} from '../../../graphql/generated';
 import Log from '../../../log';
-import {  promptAsync } from '../../../prompts';
+import { promptAsync } from '../../../prompts';
 import { Context } from '../../context';
+import { MissingCredentialsNonInteractiveError } from '../../errors';
 import { AppLookupParams } from '../api/GraphqlClient';
 import { AssignGoogleServiceAccountKey } from './AssignGoogleServiceAccountKey';
 import { CreateGoogleServiceAccountKey } from './CreateGoogleServiceAccountKey';
 import { UseExistingGoogleServiceAccountKey } from './UseExistingGoogleServiceAccountKey';
 
-
 export class SetupGoogleServiceAccountKey {
   constructor(private app: AppLookupParams) {}
 
   private async isGoogleServiceAccountKeySetupAsync(ctx: Context): Promise<boolean> {
-    const appCredentials = await ctx.android.getAndroidAppCredentialsWithCommonFieldsAsync(this.app);
-    return !!(appCredentials?.googleServiceAccountKeyForSubmissions);
+    const appCredentials = await ctx.android.getAndroidAppCredentialsWithCommonFieldsAsync(
+      this.app
+    );
+    return !!appCredentials?.googleServiceAccountKeyForSubmissions;
   }
 
   public async runAsync(ctx: Context): Promise<CommonAndroidAppCredentialsFragment> {
     if (ctx.nonInteractive) {
-      throw new Error(`Google Service Account Keys cannot be setup in non-interactive mode.`);
+      throw new MissingCredentialsNonInteractiveError(
+        'Google Service Account Keys cannot be setup in --non-interactive mode.'
+      );
     }
 
     const isKeySetup = await this.isGoogleServiceAccountKeySetupAsync(ctx);
     if (isKeySetup) {
-      Log.succeed('Google Service Account Key already setup.')
-      return nullthrows(await ctx.android.getAndroidAppCredentialsWithCommonFieldsAsync(this.app), 'androidAppCredentials cannot be null if google service account key is already setup');
+      Log.succeed('Google Service Account Key already setup.');
+      return nullthrows(
+        await ctx.android.getAndroidAppCredentialsWithCommonFieldsAsync(this.app),
+        'androidAppCredentials cannot be null if google service account key is already setup'
+      );
     }
 
-    const keysForAccount = await ctx.android.getGoogleServiceAccountKeysForAccountAsync(this.app.account);
+    const keysForAccount = await ctx.android.getGoogleServiceAccountKeysForAccountAsync(
+      this.app.account
+    );
     let googleServiceAccountKey = null;
     if (keysForAccount.length === 0) {
-      googleServiceAccountKey = await new CreateGoogleServiceAccountKey(this.app.account).runAsync(ctx);
+      googleServiceAccountKey = await new CreateGoogleServiceAccountKey(this.app.account).runAsync(
+        ctx
+      );
     } else {
       googleServiceAccountKey = await this.createOrUseExistingKeyAsync(ctx);
     }
     return await new AssignGoogleServiceAccountKey(this.app).runAsync(ctx, googleServiceAccountKey);
   }
 
-  private async createOrUseExistingKeyAsync(ctx: Context): Promise<GoogleServiceAccountKeyFragment> {
+  private async createOrUseExistingKeyAsync(
+    ctx: Context
+  ): Promise<GoogleServiceAccountKeyFragment> {
     const { action } = await promptAsync({
       type: 'select',
       name: 'action',
@@ -54,7 +71,10 @@ export class SetupGoogleServiceAccountKey {
 
     if (action === 'GENERATE') {
       return await new CreateGoogleServiceAccountKey(this.app.account).runAsync(ctx);
-    } 
-    return await new UseExistingGoogleServiceAccountKey(this.app.account).runAsync(ctx)?? await this.createOrUseExistingKeyAsync(ctx);
+    }
+    return (
+      (await new UseExistingGoogleServiceAccountKey(this.app.account).runAsync(ctx)) ??
+      (await this.createOrUseExistingKeyAsync(ctx))
+    );
   }
 }
