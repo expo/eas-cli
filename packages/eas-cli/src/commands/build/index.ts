4da41f95b845e9c487703f5950dbbd97fdcc2fa0
jonsamp <sampjon@gmail.com>
2021-10-08 11:08:16 -0400

    lint

--- packages/eas-cli/src/commands/build/index.ts
+++ packages/eas-cli/src/commands/build/index.ts
@@ -286,7 +286,7 @@ export default class Build extends EasCommand {
     };
   }
 
-  private async prepareAndStartBuildAsync<T extends Platform>({
+  private async prepareAndStartBuildAsync({
     projectDir,
     flags,
     moreBuilds,
