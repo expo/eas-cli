3f81bf7110e4ec709670067c8c808ca1949f51dc
Quinlan Jung <quin@expo.io>
2021-10-08 23:13:03 -0700

    fyi

--- packages/eas-cli/src/credentials/ios/actions/AscApiKeyUtils.ts
+++ packages/eas-cli/src/credentials/ios/actions/AscApiKeyUtils.ts
@@ -1,11 +1,22 @@
+import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
 
+import Log, { learnMore } from '../../../log';
 import { promptAsync } from '../../../prompts';
 import { getCredentialsFromUserAsync } from '../../utils/promptForCredentials';
 import { AscApiKeyPath, ascApiKeyMetadataSchema } from '../credentials';
 
 export async function promptForAscApiKeyAsync(): Promise<AscApiKeyPath> {
+  Log.log(
+    `${chalk.bold(
+      'An App Store Connect Api key is required to upload your app to the Apple App Store'
+    )}.\n` +
+      `If you're not sure what this is or how to create one, ${learnMore(
+        'https://expo.fyi/creating-asc-api-key'
+      )}`
+  );
+
   const { keyP8Path } = await promptAsync({
     type: 'text',
     name: 'keyP8Path',
