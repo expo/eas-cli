5ba3cc66237d8ca8984d1fbd1efff65097f5feaf
Dominik Sokal <dominik.sokal@swmansion.com>
2021-11-08 14:53:34 +0100

    fix working with local api server (#747)

--- packages/eas-cli/src/__tests__/api-test.ts
+++ packages/eas-cli/src/__tests__/api-test.ts
@@ -3,12 +3,12 @@ import { RequestError } from 'got/dist/source';
 import nock from 'nock';
 
 import ApiV2Error from '../ApiV2Error';
-import { apiClient, getExpoApiBaseUrl } from '../api';
+import { apiClient, getExpoApiV2Url } from '../api';
 
 describe('apiClient', () => {
   it('converts Expo APIv2 error to ApiV2Error', async () => {
-    nock(getExpoApiBaseUrl())
-      .post('/v2/test')
+    nock(getExpoApiV2Url())
+      .post('/test')
       .reply(400, {
         errors: [
           {
@@ -39,7 +39,7 @@ describe('apiClient', () => {
   });
 
   it('does not convert non-APIv2 error to ApiV2Error', async () => {
-    nock(getExpoApiBaseUrl()).post('/v2/test').reply(500, 'Something went wrong');
+    nock(getExpoApiV2Url()).post('/test').reply(500, 'Something went wrong');
 
     let error: Error | null = null;
     try {
--- packages/eas-cli/src/api.ts
+++ packages/eas-cli/src/api.ts
@@ -4,7 +4,7 @@ import ApiV2Error from './ApiV2Error';
 import { getAccessToken, getSessionSecret } from './user/sessionStorage';
 
 export const apiClient = got.extend({
-  prefixUrl: getExpoApiBaseUrl() + '/v2/',
+  prefixUrl: getExpoApiV2Url(),
   hooks: {
     beforeRequest: [
       (options: NormalizedOptions) => {
@@ -38,13 +38,23 @@ export const apiClient = got.extend({
   },
 });
 
-export function getExpoApiBaseUrl(): string {
+export function getExpoApiV2Url(): string {
   if (process.env.EXPO_STAGING) {
-    return `https://staging-api.expo.dev`;
+    return `https://staging-api.expo.dev/v2`;
   } else if (process.env.EXPO_LOCAL) {
-    return `http://127.0.0.1:3000`;
+    return `http://127.0.0.1:3000/--/api/v2`;
   } else {
-    return `https://api.expo.dev`;
+    return `https://api.expo.dev/v2`;
+  }
+}
+
+export function getExpoGraphqlUrl(): string {
+  if (process.env.EXPO_STAGING) {
+    return `https://staging-api.expo.dev/graphql`;
+  } else if (process.env.EXPO_LOCAL) {
+    return `http://127.0.0.1:3000/--/graphql`;
+  } else {
+    return `https://api.expo.dev/graphql`;
   }
 }
 
