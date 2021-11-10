bc8abe854301a0f3a732201f703ab51c848f0aff
Dominik Sokal <dominik.sokal@swmansion.com>
2021-11-10 14:11:38 +0100

    automate eas-cli-local-build-plugin upgrades

--- packages/eas-cli/src/build/local.ts
+++ packages/eas-cli/src/build/local.ts
@@ -1,45 +1,27 @@
 import { Job } from '@expo/eas-build-job';
 import spawnAsync from '@expo/spawn-async';
-import chalk from 'chalk';
-import fs from 'fs-extra';
-import path from 'path';
-
-import Log from '../log';
 
 const PLUGIN_PACKAGE_NAME = 'eas-cli-local-build-plugin';
+const PLUGIN_PACKAGE_VERSION = '0.0.45';
 
 export async function runLocalBuildAsync(job: Job): Promise<void> {
-  const execNameOrPath =
-    process.env.EAS_LOCAL_BUILD_PLUGIN_PATH ??
-    (await findWithNodeResolutionAsync()) ??
-    PLUGIN_PACKAGE_NAME;
-  try {
-    const arg = Buffer.from(JSON.stringify({ job })).toString('base64');
-    await spawnAsync(execNameOrPath, [arg], {
-      stdio: 'inherit',
-    });
-  } catch (err: any) {
-    if (err.code === 'ENOENT') {
-      Log.warn(`Could not resolve executable ${execNameOrPath}.`);
-      Log.warn(
-        `Install ${PLUGIN_PACKAGE_NAME} package from npm e.g. ${chalk.bold(
-          `npm install -g ${PLUGIN_PACKAGE_NAME}`
-        )} and make sure it's in PATH.`
-      );
-      throw err;
-    }
-  }
+  const { command, args } = getCommandAndArgs(job);
+  await spawnAsync(command, args, {
+    stdio: 'inherit',
+  });
 }
 
-async function findWithNodeResolutionAsync(): Promise<string | undefined> {
-  try {
-    const resolvedPath = path.resolve(
-      path.dirname(require.resolve(PLUGIN_PACKAGE_NAME)),
-      '../bin/run'
-    );
-    if (await fs.pathExists(resolvedPath)) {
-      return resolvedPath;
-    }
-  } catch {}
-  return undefined;
+function getCommandAndArgs(job: Job): { command: string; args: string[] } {
+  const jobBase64 = Buffer.from(JSON.stringify({ job })).toString('base64');
+  if (process.env.EAS_LOCAL_BUILD_PLUGIN_PATH) {
+    return {
+      command: process.env.EAS_LOCAL_BUILD_PLUGIN_PATH,
+      args: [jobBase64],
+    };
+  } else {
+    return {
+      command: 'npx',
+      args: ['-y', `${PLUGIN_PACKAGE_NAME}@${PLUGIN_PACKAGE_VERSION}`, jobBase64],
+    };
+  }
 }
