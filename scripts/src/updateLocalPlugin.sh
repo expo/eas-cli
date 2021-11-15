ea0d0bb4dc0c1ed4c3d3b0abddbe02c71e2cd8d7
Dominik Sokal <dominik.sokal@swmansion.com>
2021-11-15 11:34:32 +0100

    pin `eas-cli-local-build-plugin` to specific version  (#753)
    * automate eas-cli-local-build-plugin upgrades
    
    * update PR template, CHANGELOG, CONTRIBUTING guide
    
    * update CHANGELOG.md
    
    * address PR feedback

---
+++ scripts/src/updateLocalPlugin.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
+LOCAL_TS_PATH="$ROOT_DIR/packages/eas-cli/src/build/local.ts"
+
+echo "Checking if eas-cli-local-build-plugin upgrade is available"
+
+plugin_version_latest=$(npm info eas-cli-local-build-plugin@latest version)
+plugin_version_current=$(grep 'const PLUGIN_PACKAGE_VERSION' $LOCAL_TS_PATH | cut -d"'" -f2)
+
+if [[ "$plugin_version_latest" == "$plugin_version_current" ]]; then
+  echo "eas-cli-local-build-plugin is already on the latest version - $plugin_version_latest"
+else
+  echo "Bumping eas-cli-local-build-plugin from $plugin_version_current to $plugin_version_latest"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/$plugin_version_current/$plugin_version_latest/g" $LOCAL_TS_PATH
+  else
+    sed -i "s/$plugin_version_current/$plugin_version_latest/g" $LOCAL_TS_PATH
+  fi
+  git add $LOCAL_TS_PATH
+  git commit -m "upgrade eas-cli-local-build-plugin to $plugin_version_latest"
+fi
