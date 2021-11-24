9e757a0f9ec81c9a1fff3b4c3f1be4f79b403991
Dominik Sokal <dominik.sokal@swmansion.com>
2021-11-24 09:41:56 +0100

    check npm version when building locally

--- packages/eas-cli/src/build/local.ts
+++ packages/eas-cli/src/build/local.ts
@@ -1,17 +1,18 @@
 import { Job } from '@expo/eas-build-job';
 import spawnAsync from '@expo/spawn-async';
+import semver from 'semver';
 
 const PLUGIN_PACKAGE_NAME = 'eas-cli-local-build-plugin';
 const PLUGIN_PACKAGE_VERSION = '0.0.50';
 
 export async function runLocalBuildAsync(job: Job): Promise<void> {
-  const { command, args } = getCommandAndArgs(job);
+  const { command, args } = await getCommandAndArgsAsync(job);
   await spawnAsync(command, args, {
     stdio: 'inherit',
   });
 }
 
-function getCommandAndArgs(job: Job): { command: string; args: string[] } {
+async function getCommandAndArgsAsync(job: Job): Promise<{ command: string; args: string[] }> {
   const jobBase64 = Buffer.from(JSON.stringify({ job })).toString('base64');
   if (process.env.EAS_LOCAL_BUILD_PLUGIN_PATH) {
     return {
@@ -19,9 +20,20 @@ function getCommandAndArgs(job: Job): { command: string; args: string[] } {
       args: [jobBase64],
     };
   } else {
+    const args = [`${PLUGIN_PACKAGE_NAME}@${PLUGIN_PACKAGE_VERSION}`, jobBase64];
+    if (await isAtLeastNpm7Async()) {
+      // npx shipped with npm >= 7.0.0 requires the "-y" flag to run commands without
+      // prompting the user to install a package that is used for the first time
+      args.unshift('-y');
+    }
     return {
       command: 'npx',
-      args: ['-y', `${PLUGIN_PACKAGE_NAME}@${PLUGIN_PACKAGE_VERSION}`, jobBase64],
+      args,
     };
   }
+}
+
+async function isAtLeastNpm7Async(): Promise<boolean> {
+  const version = (await spawnAsync('npm', ['--version'])).stdout.trim();
+  return semver.gte(version, '7.0.0');
 }
