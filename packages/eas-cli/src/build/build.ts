af9ac9fca3199ec5f90dfb9acab273d45277fa9c
Dominik Sokal <dominik.sokal@swmansion.com>
2022-04-05 11:45:31 +0200

    refactor

--- packages/eas-cli/src/build/build.ts
+++ packages/eas-cli/src/build/build.ts
@@ -11,7 +11,7 @@ import { BuildFragment, BuildStatus, UploadSessionType } from '../graphql/genera
 import { BuildResult } from '../graphql/mutations/BuildMutation';
 import { BuildQuery } from '../graphql/queries/BuildQuery';
 import Log, { learnMore } from '../log';
-import { ora } from '../ora';
+import { Ora, ora } from '../ora';
 import { requestedPlatformDisplayNames } from '../platform';
 import { uploadAsync } from '../uploads';
 import { formatBytes } from '../utils/files';
@@ -102,41 +102,41 @@ export async function prepareBuildRequestForPlatformAsync<
       try {
         return await sendBuildRequestAsync(builder, job, metadata);
       } catch (error: any) {
-        if (error?.graphQLErrors?.[0]?.extensions?.errorCode === 'TURTLE_DEPRECATED_JOB_FORMAT') {
-          Log.error('EAS Build API has changed, please upgrade to the latest eas-cli version.');
-          throw new Error('Build request failed.');
-        } else if (
-          error?.graphQLErrors?.[0]?.extensions?.errorCode === 'EAS_BUILD_DOWN_FOR_MAINTENANCE'
-        ) {
-          Log.error(
-            'EAS Build is down for maintenance, please try again later. Check https://status.expo.dev/ for updates.'
-          );
-          throw new Error('Build request failed.');
-        } else if (
-          error?.graphQLErrors?.[0]?.extensions?.errorCode === 'EAS_BUILD_FREE_TIER_DISABLED'
-        ) {
-          Log.error(
-            'EAS Build free tier is temporarily disabled, please try again later. Check https://status.expo.dev/ for updates.'
-          );
-          throw new Error('Build request failed.');
-        } else if (
-          error?.graphQLErrors?.[0]?.extensions?.errorCode === 'EAS_BUILD_TOO_MANY_PENDING_BUILDS'
-        ) {
-          Log.error(
-            `You have already reached the maximum number of pending ${
-              requestedPlatformDisplayNames[job.platform]
-            } builds for your account. Try again later.`
-          );
-          throw new Error('Build request failed.');
-        } else if (error?.graphQLErrors) {
-          Log.error(
-            'Build request failed. Make sure you are using the latest eas-cli version. If the problem persists, please report the issue.'
-          );
-        }
-        throw error;
+        handleBuildRequestError(error, job.platform);
       }
     }
   };
+}
+
+function handleBuildRequestError(error: any, platform: Platform): never {
+  if (error?.graphQLErrors?.[0]?.extensions?.errorCode === 'TURTLE_DEPRECATED_JOB_FORMAT') {
+    Log.error('EAS Build API has changed, please upgrade to the latest eas-cli version.');
+    throw new Error('Build request failed.');
+  } else if (
+    error?.graphQLErrors?.[0]?.extensions?.errorCode === 'EAS_BUILD_DOWN_FOR_MAINTENANCE'
+  ) {
+    Log.error(
+      'EAS Build is down for maintenance, please try again later. Check https://status.expo.dev/ for updates.'
+    );
+    throw new Error('Build request failed.');
+  } else if (error?.graphQLErrors?.[0]?.extensions?.errorCode === 'EAS_BUILD_FREE_TIER_DISABLED') {
+    Log.error(
+      'EAS Build free tier is temporarily disabled, please try again later. Check https://status.expo.dev/ for updates.'
+    );
+    throw new Error('Build request failed.');
+  } else if (
+    error?.graphQLErrors?.[0]?.extensions?.errorCode === 'EAS_BUILD_TOO_MANY_PENDING_BUILDS'
+  ) {
+    Log.error(
+      `You have already reached the maximum number of pending ${requestedPlatformDisplayNames[platform]} builds for your account. Try again later.`
+    );
+    throw new Error('Build request failed.');
+  } else if (error?.graphQLErrors) {
+    Log.error(
+      'Build request failed. Make sure you are using the latest eas-cli version. If the problem persists, please report the issue.'
+    );
+  }
+  throw error;
 }
 
 async function uploadProjectAsync<TPlatform extends Platform>(
@@ -210,147 +210,180 @@ async function sendBuildRequestAsync<TPlatform extends Platform, Credentials, TJ
   );
 }
 
+type MaybeBuildFragment = BuildFragment | null;
+
 export async function waitForBuildEndAsync(
   { buildIds, accountName }: { buildIds: string[]; accountName: string },
-  { timeoutSec = 3600, intervalSec = 30 } = {}
-): Promise<(BuildFragment | null)[]> {
-  Log.log(
-    `Waiting for build${buildIds.length > 1 ? 's' : ''} to complete. You can press Ctrl+C to exit.`
-  );
-  const spinner = ora().start();
-  let time = new Date().getTime();
-  const endTime = time + timeoutSec * 1000;
-  let queueProgressBarStarted = false;
-  const queueProgressBar = new cliProgress.SingleBar(
-    { format: '|{bar}| {estimatedWaitTime}' },
-    cliProgress.Presets.rect
-  );
-  while (time <= endTime) {
-    const builds: (BuildFragment | null)[] = await Promise.all(
-      buildIds.map(async buildId => {
-        try {
-          return await BuildQuery.byIdAsync(buildId, { useCache: false });
-        } catch (err) {
-          Log.debug('Failed to fetch the build status', err);
-          return null;
-        }
-      })
-    );
-    if (builds.length === 1) {
-      const [build] = builds;
-      if (build !== null) {
-        if (queueProgressBarStarted && build?.status && build.status !== BuildStatus.InQueue) {
-          if (build.status === BuildStatus.InProgress) {
-            queueProgressBar.update(queueProgressBar.getTotal());
-          }
-          queueProgressBar.stop();
-          Log.newLine();
-          queueProgressBarStarted = false;
-          spinner.start('Build is about to start');
-        }
-
-        switch (build.status) {
-          case BuildStatus.Finished:
-            spinner.succeed('Build finished');
-            return builds;
-          case BuildStatus.New:
-            spinner.text = `Build is waiting to enter the queue. Check your concurrency limit at ${chalk.underline(
-              formatAccountSubscriptionsUrl(accountName)
-            )}.`;
-            break;
-          case BuildStatus.InQueue: {
-            spinner.text = 'Build queued...';
-            const progressBarPayload =
-              typeof build.estimatedWaitTimeLeftSeconds === 'number'
-                ? { estimatedWaitTime: formatEstimatedWaitTime(build.estimatedWaitTimeLeftSeconds) }
-                : { estimatedWaitTime: '' };
-
-            if (
-              !queueProgressBarStarted &&
-              typeof build.initialQueuePosition === 'number' &&
-              typeof build.queuePosition === 'number'
-            ) {
-              spinner.stopAndPersist();
-              Log.newLine();
-              Log.log('Waiting in queue');
-              queueProgressBar.start(
-                build.initialQueuePosition + 1,
-                build.initialQueuePosition - build.queuePosition + 1,
-                progressBarPayload
-              );
-              queueProgressBarStarted = true;
-            }
-            if (typeof build.queuePosition === 'number') {
-              queueProgressBar.update(build.queuePosition, progressBarPayload);
-            }
-            break;
-          }
-          case BuildStatus.Canceled:
-            spinner.fail('Build canceled');
-            return builds;
-          case BuildStatus.InProgress:
-            spinner.text = 'Build in progress...';
-            break;
-          case BuildStatus.Errored:
-            spinner.fail('Build failed');
-            if (build.error) {
-              return builds;
-            } else {
-              throw new Error(`Standalone build failed!`);
-            }
-          default:
-            spinner.warn('Unknown status');
-            throw new Error(`Unknown build status: ${build.status} - aborting!`);
-        }
-      } else {
-        if (!spinner.text) {
-          spinner.text = 'Could not fetch the build status. Check your network connection.';
-        }
-      }
-    } else {
-      if (builds.filter(build => build?.status === BuildStatus.Finished).length === builds.length) {
-        spinner.succeed('All builds have finished');
-        return builds;
-      } else if (
-        builds.filter(build =>
-          build?.status
-            ? [BuildStatus.Finished, BuildStatus.Errored, BuildStatus.Canceled].includes(
-                build.status
-              )
-            : false
-        ).length === builds.length
-      ) {
-        spinner.fail('Some of the builds were canceled or failed.');
-        return builds;
-      } else {
-        const newBuilds = builds.filter(build => build?.status === BuildStatus.New).length;
-        const inQueue = builds.filter(build => build?.status === BuildStatus.InQueue).length;
-        const inProgress = builds.filter(build => build?.status === BuildStatus.InProgress).length;
-        const errored = builds.filter(build => build?.status === BuildStatus.Errored).length;
-        const finished = builds.filter(build => build?.status === BuildStatus.Finished).length;
-        const canceled = builds.filter(build => build?.status === BuildStatus.Canceled).length;
-        const unknown =
-          builds.length - newBuilds - inQueue - inProgress - errored - finished - canceled;
-        spinner.text = [
-          newBuilds && `Builds created: ${newBuilds}`,
-          inQueue && `Builds in queue: ${inQueue}`,
-          inProgress && `Builds in progress: ${inProgress}`,
-          canceled && `Builds canceled: ${canceled}`,
-          errored && chalk.red(`Builds failed: ${errored}`),
-          finished && chalk.green(`Builds finished: ${finished}`),
-          unknown && chalk.red(`Builds with unknown status: ${unknown}`),
-        ]
-          .filter(i => i)
-          .join('\t');
-      }
+  {
+    // 2 hours (max build time limit) + 10 minutes (possible queue time )
+    timeoutSec = 2 * 60 * 60 + 10 * 60,
+    intervalSec = 10,
+  } = {}
+): Promise<MaybeBuildFragment[]> {
+  const b = `build${buildIds.length > 1 ? 's' : ''}`;
+  Log.log(`Waiting for ${b} to complete. You can press Ctrl+C to exit.`);
+  const spinner = ora(`Waiting for ${b} to complete.`).start();
+  const endTime = new Date().getTime() + timeoutSec * 1000;
+  while (new Date().getTime() <= endTime) {
+    const builds = await getBuildsSafelyAsync(buildIds);
+    const { refetch } =
+      builds.length === 1
+        ? await handleSingleBuildProgressAsync({ build: builds[0], accountName }, { spinner })
+        : await handleMultipleBuildsProgressAsync({ builds }, { spinner });
+    if (!refetch) {
+      return builds;
     }
-    time = new Date().getTime();
     await sleepAsync(intervalSec * 1000);
   }
-  spinner.warn('Timed out');
+  spinner.fail('Timed out');
   throw new Error(
     'Timeout reached! It is taking longer than expected to finish the build, aborting...'
   );
+}
+
+async function getBuildsSafelyAsync(buildIds: string[]): Promise<MaybeBuildFragment[]> {
+  const promises = buildIds.map(async buildId => {
+    try {
+      return await BuildQuery.byIdAsync(buildId, { useCache: false });
+    } catch (err) {
+      Log.debug('Failed to fetch the build status', err);
+      return null;
+    }
+  });
+  return await Promise.all(promises);
+}
+
+interface BuildProgressResult {
+  refetch: boolean;
+}
+
+let queueProgressBarStarted = false;
+const queueProgressBar = new cliProgress.SingleBar(
+  { format: '|{bar}| {estimatedWaitTime}' },
+  cliProgress.Presets.rect
+);
+
+async function handleSingleBuildProgressAsync(
+  { build, accountName }: { build: MaybeBuildFragment; accountName: string },
+  { spinner }: { spinner: Ora }
+): Promise<BuildProgressResult> {
+  if (build === null) {
+    spinner.text = 'Could not fetch the build status. Check your network connection.';
+    return { refetch: true };
+  }
+
+  if (queueProgressBarStarted && build?.status && build.status !== BuildStatus.InQueue) {
+    if (build.status === BuildStatus.InProgress) {
+      queueProgressBar.update(queueProgressBar.getTotal());
+    }
+    queueProgressBar.stop();
+    Log.newLine();
+    queueProgressBarStarted = false;
+    spinner.start('Build is about to start');
+  }
+
+  switch (build.status) {
+    case BuildStatus.Finished:
+      spinner.succeed('Build finished');
+      return { refetch: false };
+    case BuildStatus.New:
+      spinner.text = `Build is waiting to enter the queue. Check your concurrency limit at ${chalk.underline(
+        formatAccountSubscriptionsUrl(accountName)
+      )}.`;
+      break;
+    case BuildStatus.InQueue: {
+      spinner.text = 'Build queued...';
+      const progressBarPayload =
+        typeof build.estimatedWaitTimeLeftSeconds === 'number'
+          ? { estimatedWaitTime: formatEstimatedWaitTime(build.estimatedWaitTimeLeftSeconds) }
+          : { estimatedWaitTime: '' };
+
+      if (
+        !queueProgressBarStarted &&
+        typeof build.initialQueuePosition === 'number' &&
+        typeof build.queuePosition === 'number'
+      ) {
+        spinner.stopAndPersist();
+        Log.newLine();
+        Log.log('Waiting in queue');
+        queueProgressBar.start(
+          build.initialQueuePosition + 1,
+          build.initialQueuePosition - build.queuePosition + 1,
+          progressBarPayload
+        );
+        queueProgressBarStarted = true;
+      }
+      if (typeof build.queuePosition === 'number') {
+        queueProgressBar.update(build.queuePosition, progressBarPayload);
+      }
+      break;
+    }
+    case BuildStatus.Canceled:
+      spinner.fail('Build canceled');
+      return { refetch: false };
+    case BuildStatus.InProgress:
+      spinner.text = 'Build in progress...';
+      break;
+    case BuildStatus.Errored:
+      spinner.fail('Build failed');
+      if (build.error) {
+        return { refetch: false };
+      } else {
+        throw new Error('Standalone build failed!');
+      }
+    default:
+      spinner.warn('Unknown status');
+      throw new Error(`Unknown build status: ${build.status} - aborting!`);
+  }
+  return { refetch: true };
+}
+
+async function handleMultipleBuildsProgressAsync(
+  { builds: maybeBuilds }: { builds: MaybeBuildFragment[] },
+  { spinner }: { spinner: Ora }
+): Promise<BuildProgressResult> {
+  const buildCount = maybeBuilds.length;
+  const builds = maybeBuilds.filter<BuildFragment>(isBuildFragment);
+
+  const allFinished =
+    builds.filter(build => build.status === BuildStatus.Finished).length === buildCount;
+  const allSettled =
+    builds.filter(build =>
+      [BuildStatus.Finished, BuildStatus.Errored, BuildStatus.Canceled].includes(build.status)
+    ).length === buildCount;
+
+  if (allFinished) {
+    spinner.succeed('All builds have finished');
+    return { refetch: false };
+  } else if (allSettled) {
+    spinner.fail('Some of the builds were canceled or failed.');
+    return { refetch: false };
+  } else {
+    const newBuilds = builds.filter(build => build.status === BuildStatus.New).length;
+    const inQueue = builds.filter(build => build.status === BuildStatus.InQueue).length;
+    const inProgress = builds.filter(build => build.status === BuildStatus.InProgress).length;
+    const errored = builds.filter(build => build.status === BuildStatus.Errored).length;
+    const finished = builds.filter(build => build.status === BuildStatus.Finished).length;
+    const canceled = builds.filter(build => build.status === BuildStatus.Canceled).length;
+    const unknown = buildCount - newBuilds - inQueue - inProgress - errored - finished - canceled;
+    const text = [
+      newBuilds && `Builds created: ${newBuilds}`,
+      inQueue && `Builds in queue: ${inQueue}`,
+      inProgress && `Builds in progress: ${inProgress}`,
+      canceled && `Builds canceled: ${canceled}`,
+      errored && chalk.red(`Builds failed: ${errored}`),
+      finished && chalk.green(`Builds finished: ${finished}`),
+      unknown && chalk.red(`Builds in unknown state: ${unknown}`),
+    ]
+      .filter(i => i)
+      .join('\t');
+    spinner.text = text;
+    return { refetch: true };
+  }
+}
+
+function isBuildFragment(maybeBuild: MaybeBuildFragment): maybeBuild is BuildFragment {
+  return maybeBuild !== null;
 }
 
 function formatEstimatedWaitTime(estimatedWaitTimeLeftSeconds: number): string {
