beac65ce530f06dca48a707ed59e26b7bdd0e5f1
Cedric van Putten <me@bycedric.com>
2022-06-02 22:58:51 +0200

    [eas-cli] Add metadata config validation using json schema (#1131)
    * [eas-cli] Add metadata config validation using json schema
    
    * [eas-cli] Fix typo in privacy choices validation
    
    * [eas-cli] Fix typo in fixture test

---
+++ packages/eas-cli/src/metadata/config.ts
@@ -0,0 +1,42 @@
+import Ajv from 'ajv';
+import assert from 'assert';
+
+import { AppleConfigReader } from './apple/config/reader';
+import { AppleConfigWriter } from './apple/config/writer';
+import { AppleMetadata } from './apple/types';
+
+export interface MetadataConfig {
+  /** The store configuration version */
+  configVersion: number;
+  /** All App Store related configuration */
+  apple?: AppleMetadata;
+}
+
+/**
+ * Run the JSON Schema validation to normalize defaults and flag early config errors.
+ * This includes validating the known store limitations for every configurable property.
+ */
+export function validateConfig(config: unknown): {
+  valid: boolean;
+  errors: Ajv.ErrorObject[];
+} {
+  const validator = new Ajv({ allErrors: true, useDefaults: true })
+    .addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'))
+    .compile(require('../../schema/metadata-0.json'));
+
+  const valid = validator(config) as boolean;
+
+  return { valid, errors: validator.errors || [] };
+}
+
+/** Create a versioned deserializer to fetch App Store data from the store configuration. */
+export function createAppleReader(config: MetadataConfig): AppleConfigReader {
+  assert(config.configVersion === 0, 'Unsupported store configuration version');
+  assert(config.apple, 'No apple configuration found');
+  return new AppleConfigReader(config.apple);
+}
+
+/** Create the serializer to write the App Store to the store configuration. */
+export function createAppleWriter(): AppleConfigWriter {
+  return new AppleConfigWriter();
+}
