58be1ceeaa6907910939be8e5c8e1ff30170d305
Bartosz Kaszubowski <gosimek@gmail.com>
2022-08-16 14:34:35 +0200

    address review suggestions

--- packages/eas-cli/src/credentials/manager/SelectPlatform.ts
+++ packages/eas-cli/src/credentials/manager/SelectPlatform.ts
@@ -3,10 +3,8 @@ import { ManageAndroid } from './ManageAndroid';
 import { ManageIos } from './ManageIos';
 
 export class SelectPlatform {
-  private readonly flagPlatform: string;
-
-  constructor(platform: string) {
-    this.flagPlatform = platform;
+  constructor(private readonly flagPlatform?: string) {
+    this.flagPlatform = flagPlatform;
   }
 
   async runAsync(): Promise<void> {
