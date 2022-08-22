007866f278113389b56e0bd6d257b96949e39812
Bartosz Kaszubowski <gosimek@gmail.com>
2022-08-22 10:03:53 +0200

    [cli] Credentials: support platform flag (#1277)
    Co-authored-by: Dominik Sokal <dominik.sokal@swmansion.com>

--- packages/eas-cli/src/commands/credentials.ts
+++ packages/eas-cli/src/commands/credentials.ts
@@ -1,10 +1,17 @@
+import { Flags } from '@oclif/core';
+
 import EasCommand from '../commandUtils/EasCommand';
 import { SelectPlatform } from '../credentials/manager/SelectPlatform';
 
 export default class Credentials extends EasCommand {
   static description = 'manage credentials';
 
+  static flags = {
+    platform: Flags.enum({ char: 'p', options: ['android', 'ios'] }),
+  };
+
   async runAsync(): Promise<void> {
-    await new SelectPlatform().runAsync();
+    const { flags } = await this.parse(Credentials);
+    await new SelectPlatform(flags.platform).runAsync();
   }
 }
