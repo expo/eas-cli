f0c6c316ae458cfb081d3681ddfb6226b19466f2
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-09-26 12:56:02 +0200

    [eas-cli] Increase max versionCode for Android

--- packages/eas-cli/src/project/android/__tests__/versions-test.ts
+++ packages/eas-cli/src/project/android/__tests__/versions-test.ts
@@ -5,13 +5,13 @@ describe(isValidVersionCode, () => {
     expect(isValidVersionCode(123)).toBe(true);
     expect(isValidVersionCode(12.3)).toBe(false);
     expect(isValidVersionCode(-123)).toBe(false);
-    expect(isValidVersionCode(2000000000)).toBe(true);
-    expect(isValidVersionCode(2000000001)).toBe(false);
+    expect(isValidVersionCode(2100000000)).toBe(true);
+    expect(isValidVersionCode(2100000001)).toBe(false);
     expect(isValidVersionCode('123')).toBe(true);
     expect(isValidVersionCode('12.3')).toBe(false);
     expect(isValidVersionCode('-123')).toBe(false);
-    expect(isValidVersionCode('2000000000')).toBe(true);
-    expect(isValidVersionCode('2000000001')).toBe(false);
+    expect(isValidVersionCode('2100000000')).toBe(true);
+    expect(isValidVersionCode('2100000001')).toBe(false);
   });
 });
 
@@ -19,8 +19,8 @@ describe(getNextVersionCode, () => {
   test('getNextVersionCode()', () => {
     expect(getNextVersionCode(123)).toBe(124);
     expect(() => getNextVersionCode(12.3)).toThrowError();
-    expect(getNextVersionCode(1999999999)).toBe(2000000000);
-    expect(() => getNextVersionCode(2000000000)).toThrowError();
+    expect(getNextVersionCode(2099999999)).toBe(2100000000);
+    expect(() => getNextVersionCode(2100000000)).toThrowError();
     expect(getNextVersionCode('123')).toBe(124);
     expect(() => getNextVersionCode('12.3')).toThrowError();
   });
