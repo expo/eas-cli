9f34fd3cc54cde64abd6a775decad5bde2164d00
Cedric van Putten <me@bycedric.com>
2022-09-30 23:19:37 +0200

    feature: implement validation command with json option

--- packages/eas-cli/src/commands/metadata/lint.ts
+++ packages/eas-cli/src/commands/metadata/lint.ts
@@ -1,12 +1,18 @@
+import { getConfig } from '@expo/config';
 import { Flags } from '@oclif/core';
 
 import EasCommand from '../../commandUtils/EasCommand';
-import Log, { learnMore } from '../../log';
+import Log from '../../log';
+import { loadConfigAsync } from '../../metadata/config/resolve';
+import { createMetadataContextAsync } from '../../metadata/context';
+import { MetadataValidationError, logMetadataValidationError } from '../../metadata/errors';
+import { findProjectRootAsync } from '../../project/projectUtils';
+import { enableJsonOutput, printJsonOnlyOutput } from '../../utils/json';
 
 export default class MetadataLint extends EasCommand {
-  static description = 'validate the local store configuration';
+  static override description = 'validate the local store configuration';
 
-  static flags = {
+  static override flags = {
     json: Flags.boolean({
       description: 'Enable JSON output, non-JSON messages will be printed to stderr',
       default: false,
@@ -20,6 +26,43 @@ export default class MetadataLint extends EasCommand {
   async runAsync(): Promise<void> {
     Log.warn('EAS Metadata is in beta and subject to breaking changes.');
 
+    const { flags } = await this.parse(MetadataLint);
+    if (flags.json) {
+      enableJsonOutput();
+    }
 
+    const projectDir = await findProjectRootAsync();
+    const { exp } = getConfig(projectDir, { skipSDKVersionRequirement: true });
+
+    // this command is interactive (all nonInteractive flags passed to utility functions are false)
+    // await getProjectIdAsync(exp, { nonInteractive: false });
+    // await ensureProjectConfiguredAsync({ projectDir, nonInteractive: false });
+
+    const metadataCtx = await createMetadataContextAsync({
+      projectDir,
+      exp,
+      profileName: flags.profile,
+    });
+
+    try {
+      await loadConfigAsync(metadataCtx);
+
+      if (flags.json) {
+        return printJsonOnlyOutput([]);
+      }
+
+      Log.log('✅ Store configuration is valid.');
+    } catch (error) {
+      if (!(error instanceof MetadataValidationError)) {
+        throw error;
+      }
+
+      if (flags.json) {
+        return printJsonOnlyOutput(error.errors);
+      }
+
+      logMetadataValidationError(error);
+      Log.addNewLineIfNone();
+    }
   }
 }
