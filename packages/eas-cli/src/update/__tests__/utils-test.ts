f2564bf2d4276f786c75843fb11e9199a2ca318f
Cedric van Putten <me@bycedric.com>
2022-10-25 20:21:42 +0200

    fix(update): use the old truncated update message limit

--- packages/eas-cli/src/update/__tests__/utils-test.ts
+++ packages/eas-cli/src/update/__tests__/utils-test.ts
@@ -1,28 +1,18 @@
-import Log from '../../log';
 import { getPlatformsForGroup, truncateString } from '../utils';
 
 describe('update utility functions', () => {
   describe(truncateString, () => {
-    const warnSpy = jest.spyOn(Log, 'warn');
-    beforeEach(() => {
-      warnSpy.mockClear();
-    });
-
     it('does not alter messages with less than 1024 characters', () => {
       const message = 'Small message =)';
-      const truncatedMessage = truncateString(message);
+      const truncatedMessage = truncateString(message, 1024);
       expect(truncatedMessage).toEqual(message);
-      expect(warnSpy).not.toBeCalled();
     });
 
     it('truncates messages to a length of 1024, including ellipses', () => {
       const longMessage = Array.from({ length: 2024 }, () => 'a').join('');
-      const truncatedMessage = truncateString(longMessage);
+      const truncatedMessage = truncateString(longMessage, 1024);
       expect(truncatedMessage.length).toEqual(1024);
       expect(truncatedMessage.slice(-3)).toEqual('...');
-      expect(warnSpy).toBeCalledWith(
-        'Update message exceeds the allowed 1024 character limit. Truncating message...'
-      );
     });
   });
 
