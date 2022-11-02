6270bc122bfa133c21f30a38ecfc3b438c0186d0
Dominik Sokal <dominik.sokal@swmansion.com>
2022-11-02 18:05:55 +0100

    [ENG-6619] validate icon PNGs before running build (#1477)
    * validate icon PNGs before running build
    
    * address PR feedback
    
    * address PR feedback
    
    * address more PR feedback

--- packages/eas-cli/src/build/validate.ts
+++ packages/eas-cli/src/build/validate.ts
@@ -1,8 +1,11 @@
 import { Platform, Workflow } from '@expo/eas-build-job';
+import { Errors } from '@oclif/core';
 import fs from 'fs-extra';
 import path from 'path';
+import semver from 'semver';
 
 import Log, { learnMore } from '../log';
+import { isPNGAsync } from '../utils/image';
 import { getVcsClient } from '../vcs';
 import { CommonContext } from './context';
 
@@ -49,4 +52,138 @@ export async function checkGoogleServicesFileAsync<T extends Platform>(
 
 function isInsideDirectory(file: string, directory: string): boolean {
   return file.startsWith(directory);
+}
+
+export async function validatePNGsForManagedProjectAsync<T extends Platform>(
+  ctx: CommonContext<T>
+): Promise<void> {
+  if (ctx.workflow !== Workflow.MANAGED) {
+    return;
+  }
+
+  // don't run PNG checks on SDK 47 and newer
+  // see https://github.com/expo/eas-cli/pull/1477#issuecomment-1293914917
+  if (!ctx.exp.sdkVersion || semver.satisfies(ctx.exp.sdkVersion, '>= 47.0.0')) {
+    return;
+  }
+
+  if (ctx.platform === Platform.ANDROID) {
+    await validateAndroidPNGsAsync(ctx as CommonContext<Platform.ANDROID>);
+  }
+  // Validating iOS PNGs is currently disabled
+  // See https://github.com/expo/eas-cli/pull/1477 for context
+  //
+  // else {
+  //   await validateIosPNGsAsync(ctx as CommonContext<Platform.IOS>);
+  // }
+}
+
+type ConfigPng = { configPath: string; pngPath: string | undefined };
+
+async function validateAndroidPNGsAsync(ctx: CommonContext<Platform.ANDROID>): Promise<void> {
+  const pngs: ConfigPng[] = [
+    {
+      configPath: 'exp.icon',
+      pngPath: ctx.exp.icon,
+    },
+    {
+      configPath: 'exp.android.icon',
+      pngPath: ctx.exp.android?.icon,
+    },
+    {
+      configPath: 'exp.android.adaptiveIcon.foregroundImage',
+      pngPath: ctx.exp.android?.adaptiveIcon?.foregroundImage,
+    },
+    {
+      configPath: 'exp.android.adaptiveIcon.backgroundImage',
+      pngPath: ctx.exp.android?.adaptiveIcon?.backgroundImage,
+    },
+    {
+      configPath: 'exp.splash.image',
+      pngPath: ctx.exp.splash?.image,
+    },
+    {
+      configPath: 'exp.notification.icon',
+      pngPath: ctx.exp.notification?.icon,
+    },
+  ];
+  await validatePNGsAsync(pngs);
+}
+
+// Validating iOS PNGs is currently disabled
+// See https://github.com/expo/eas-cli/pull/1477 for context
+//
+// async function validateIosPNGsAsync(ctx: CommonContext<Platform.IOS>): Promise<void> {
+//   const pngs: ConfigPng[] = [
+//     {
+//       configPath: 'exp.icon',
+//       pngPath: ctx.exp.icon,
+//     },
+//     {
+//       configPath: 'exp.ios.icon',
+//       pngPath: ctx.exp.ios?.icon,
+//     },
+//     {
+//       configPath: 'exp.splash.image',
+//       pngPath: ctx.exp.splash?.image,
+//     },
+//     {
+//       configPath: 'exp.notification.icon',
+//       pngPath: ctx.exp.notification?.icon,
+//     },
+//   ];
+//   await validatePNGsAsync(pngs);
+
+//   const icon = ctx.exp.ios?.icon ?? ctx.exp.icon;
+//   if (!icon) {
+//     return;
+//   }
+//   const iconConfigPath = `expo${ctx.exp.ios?.icon ? '.ios' : ''}.icon`;
+
+//   try {
+//     await ensurePNGIsNotTransparentAsync(icon);
+//   } catch (err: any) {
+//     if (err instanceof ImageTransparencyError) {
+//       Log.error(
+//         `Your iOS app icon (${iconConfigPath}) can't have transparency if you wish to upload your app to the Apple App Store.`
+//       );
+//       Log.error(learnMore('https://expo.fyi/remove-alpha-channel', { dim: false }));
+//       Errors.exit(1);
+//     } else {
+//       throw err;
+//     }
+//   }
+// }
+
+async function validatePNGsAsync(configPngs: ConfigPng[]): Promise<void> {
+  const validationPromises = configPngs.map(configPng => validatePNGAsync(configPng));
+  const validationResults = await Promise.allSettled(validationPromises);
+  const failedValidations = validationResults.filter(
+    (result): result is PromiseRejectedResult => result.status === 'rejected'
+  );
+
+  if (failedValidations.length === 0) {
+    return;
+  }
+
+  Log.error('PNG images validation failed:');
+  for (const { reason } of failedValidations) {
+    const error: Error = reason;
+    Log.error(`- ${error.message}`);
+  }
+  Errors.exit(1);
+}
+
+async function validatePNGAsync({ configPath, pngPath }: ConfigPng): Promise<void> {
+  if (!pngPath) {
+    return;
+  }
+
+  if (!pngPath.endsWith('.png')) {
+    throw new Error(`"${configPath}" is not a PNG file`);
+  }
+
+  if (!(await isPNGAsync(pngPath))) {
+    throw new Error(`"${configPath}" is not valid PNG`);
+  }
 }
