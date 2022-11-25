e00bd3e8a5f28a81e43a1f4851676bc7ab9ed703
Cedric van Putten <me@bycedric.com>
2022-11-25 16:04:01 +0100

    fix(update): correct a merge conflict error related to publish query changes

--- packages/eas-cli/src/graphql/queries/PublishQuery.ts
+++ packages/eas-cli/src/graphql/queries/PublishQuery.ts
@@ -1,16 +1,12 @@
-import { Platform } from '@expo/config';
 import gql from 'graphql-tag';
 
 import { ExpoGraphqlClient } from '../../commandUtils/context/contextUtils/createGraphqlClient';
 import { withErrorHandlingAsync } from '../client';
 import {
   AssetMetadataResult,
-  CodeSigningInfo,
   GetAssetLimitPerUpdateGroupForAppQuery,
   GetAssetLimitPerUpdateGroupForAppQueryVariables,
   GetAssetMetadataQuery,
-  ViewCodeSigningInfoFromUpdateGroupQuery,
-  ViewCodeSigningInfoFromUpdateGroupQueryVariables,
 } from '../generated';
 
 export const PublishQuery = {
@@ -69,39 +65,5 @@ export const PublishQuery = {
         .toPromise()
     );
     return data.app.byId.assetLimitPerUpdateGroup;
-  },
-
-  async getCodeSigningInfoFromUpdateGroupAsync(
-    graphqlClient: ExpoGraphqlClient,
-    updateGroupId: string
-  ): Promise<Partial<Record<Platform, CodeSigningInfo>>> {
-    const data = await withErrorHandlingAsync(
-      graphqlClient
-        .query<
-          ViewCodeSigningInfoFromUpdateGroupQuery,
-          ViewCodeSigningInfoFromUpdateGroupQueryVariables
-        >(
-          gql`
-            query ViewCodeSigningInfoFromUpdateGroup($updateGroupId: ID!) {
-              updatesByGroup(group: $updateGroupId) {
-                id
-                platform
-                codeSigningInfo {
-                  keyid
-                  sig
-                  alg
-                }
-              }
-            }
-          `,
-          { updateGroupId },
-          { additionalTypenames: [] } // required arg
-        )
-        .toPromise()
-    );
-
-    return Object.fromEntries(
-      data.updatesByGroup.map(updateGroup => [updateGroup.platform, updateGroup.codeSigningInfo])
-    );
   },
 };
