24c623fb4b5c0eb4af87a49cea4792d5d54404ae
Szymon Dziedzic <sz.dziedz@gmail.com>
2023-01-11 12:20:51 +0100

    fix error message for large resource classes

--- packages/eas-cli/src/build/runBuildAndSubmit.ts
+++ packages/eas-cli/src/build/runBuildAndSubmit.ts
@@ -144,7 +144,7 @@ function resolveBuildResourceClass(
     [ResourceClass.LARGE, ResourceClass.M1_LARGE].includes(resourceClass)
   ) {
     Log.warn(
-      `Large resource classes are not available for iOS builds yet. Your build will use the same instance type as medium resource class.`
+      `Large resource classes are not available for iOS builds yet. Your build will use the medium resource class..`
     );
   }
 
