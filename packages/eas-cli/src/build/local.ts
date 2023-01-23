a2b73844f084678c040cd7e2a752ff5a04b29ad0
Dominik Sokal <dominik.sokal@swmansion.com>
2023-01-23 12:09:33 +0100

    upgrade eas-cli-local-build-plugin to 0.0.128

--- packages/eas-cli/src/build/local.ts
+++ packages/eas-cli/src/build/local.ts
@@ -6,7 +6,7 @@ import semver from 'semver';
 import { ora } from '../ora';
 
 const PLUGIN_PACKAGE_NAME = 'eas-cli-local-build-plugin';
-const PLUGIN_PACKAGE_VERSION = '0.0.120';
+const PLUGIN_PACKAGE_VERSION = '0.0.128';
 
 export enum LocalBuildMode {
   /**
