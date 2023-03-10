c9d90668190c329c2b6c0be8b0fb544819b60c44
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-03-10 14:04:27 +0100

    Fix
    Fixed testing leftover

--- packages/eas-cli/src/build/queries.ts
+++ packages/eas-cli/src/build/queries.ts
@@ -43,7 +43,7 @@ export async function listAndRenderBuildsOnAppAsync(
       offset: paginatedQueryOptions.offset,
       queryToPerform: (limit, offset) =>
         BuildQuery.viewBuildsOnAppAsync(graphqlClient, {
-          appId: 111 as unknown as string,
+          appId: projectId,
           limit,
           offset,
           filter,
