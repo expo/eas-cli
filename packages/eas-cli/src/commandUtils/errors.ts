ff97442ebb68efa25194d7c79c3f56b80151caa2
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-03-13 11:57:08 +0100

    Suppress ESLint warning
    ESLint erroneously complains about the constructor of `EasCommandError` to be useless, which is not true is the constructor of `Error` parent class allows for optional `message` parameter and the constructor of `EasCommandError` does not
    
    See: https://linear.app/expo/issue/ENG-6945/improve-eas-cli-error-logging

--- packages/eas-cli/src/commandUtils/errors.ts
+++ packages/eas-cli/src/commandUtils/errors.ts
@@ -1,4 +1,6 @@
 export class EasCommandError extends Error {
+  // constructor is not useless, since the constructor for Error allows for optional `message`
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(message: string) {
     super(message);
   }
