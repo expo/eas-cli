af0abdfb6e2bc29063871a18b4a4385418b5162a
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-04-18 13:09:37 +0200

    Add tests
    Added tests for `prepareJobAsync` functions both for Android and iOS to check if they handle both deprecated `customPaths` and new `paths`
    
    See: https://linear.app/expo/issue/ENG-8120/rename-cachecustompaths-to-cachepaths-in-easjson-and-job-schema

---
+++ packages/eas-cli/src/build/ios/__tests__/prepareJob-test.ts
@@ -0,0 +1,66 @@
+import { Platform } from '@expo/eas-build-job';
+import { ArchiveSource, ArchiveSourceType, Workflow } from '@expo/eas-build-job/dist/common';
+import { CredentialsSource } from '@expo/eas-json';
+import { mock } from 'ts-mockito';
+
+import { BuildContext } from '../../context';
+import { prepareJobAsync } from '../prepareJob';
+
+function getBuildContextMock(): BuildContext<Platform.IOS> {
+  const ctxMock = mock<BuildContext<Platform.IOS>>();
+  ctxMock.projectDir = '.';
+  ctxMock.workflow = Workflow.GENERIC;
+  ctxMock.clearCache = false;
+  return ctxMock;
+}
+
+describe('prepareJobAsync called', () => {
+  const jobData = {
+    projectArchive: { type: ArchiveSourceType.NONE } as ArchiveSource,
+    buildScheme: 'test build scheme',
+  };
+  it('uses default cache values with paths when settings not provided', async () => {
+    const ctxMock = getBuildContextMock();
+    const result = await prepareJobAsync(ctxMock, jobData);
+    expect(result?.cache).toBeDefined();
+    expect(result.cache.paths).toBeDefined();
+    expect(result.cache.paths).toEqual([]);
+    expect(result.cache.customPaths).toBeDefined();
+    expect(result.cache.customPaths).toEqual([]);
+  });
+  it('uses paths when paths provided', async () => {
+    const ctxMock = getBuildContextMock();
+    ctxMock.buildProfile = {
+      cache: {
+        disabled: false,
+        paths: ['index.ts'],
+      },
+      credentialsSource: CredentialsSource.LOCAL,
+      distribution: 'internal',
+    };
+    const result = await prepareJobAsync(ctxMock, jobData);
+    expect(result?.cache).toBeDefined();
+    expect(result.cache.paths).toBeDefined();
+    expect(result.cache.paths).toEqual(['index.ts']);
+    expect(result.cache.customPaths).toBeDefined();
+    expect(result.cache.customPaths).toEqual([]);
+  });
+  it('uses deprecated customPaths when customPaths provided', async () => {
+    const ctxMock = getBuildContextMock();
+    ctxMock.buildProfile = {
+      // @ts-ignore
+      cache: {
+        disabled: false,
+        customPaths: ['index.ts'],
+      },
+      credentialsSource: CredentialsSource.LOCAL,
+      distribution: 'internal',
+    };
+    const result = await prepareJobAsync(ctxMock, jobData);
+    expect(result?.cache).toBeDefined();
+    expect(result.cache.paths).toBeDefined();
+    expect(result.cache.paths).toEqual([]);
+    expect(result.cache.customPaths).toBeDefined();
+    expect(result.cache.customPaths).toEqual(['index.ts']);
+  });
+});
