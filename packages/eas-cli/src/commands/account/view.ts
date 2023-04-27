dfc314d5be1dbaa4accb97c0ad8fbf3fbf218bbf
Will Schurman <wschurman@expo.io>
2023-04-27 06:52:24 -0700

    [eas-cli] Add account list to `eas whoami`/`eas account:view` (#1814)

--- packages/eas-cli/src/commands/account/view.ts
+++ packages/eas-cli/src/commands/account/view.ts
@@ -1,8 +1,10 @@
 import chalk from 'chalk';
+import nullthrows from 'nullthrows';
 
 import EasCommand from '../../commandUtils/EasCommand';
+import { Role } from '../../graphql/generated';
 import Log from '../../log';
-import { getActorDisplayName } from '../../user/User';
+import { Actor, getActorDisplayName } from '../../user/User';
 
 export default class AccountView extends EasCommand {
   static override description = 'show the username you are logged in as';
@@ -18,9 +20,48 @@ export default class AccountView extends EasCommand {
     } = await this.getContextAsync(AccountView, { nonInteractive: true });
     if (actor) {
       Log.log(chalk.green(getActorDisplayName(actor)));
+
+      // personal account is included, only show if more accounts that personal account
+      // but do show personal account in list if there are more
+      const accountExcludingPersonalAccount = actor.accounts.filter(
+        account => !('username' in actor) || account.name !== actor.username
+      );
+      if (accountExcludingPersonalAccount.length > 0) {
+        Log.newLine();
+        Log.log('Accounts:');
+        actor.accounts.forEach(account => {
+          const roleOnAccount = AccountView.getRoleOnAccount(actor, account);
+          Log.log(`• ${account.name} (Role: ${AccountView.getLabelForRole(roleOnAccount)})`);
+        });
+      }
     } else {
       Log.warn('Not logged in');
       process.exit(1);
+    }
+  }
+
+  private static getRoleOnAccount(actor: Actor, account: Actor['accounts'][0]): Role {
+    if ('username' in actor && account.name === actor.username) {
+      return Role.Owner;
+    }
+
+    return nullthrows(account.users.find(user => user.actor.id === actor.id)?.role);
+  }
+
+  private static getLabelForRole(role: Role): string {
+    switch (role) {
+      case Role.Owner:
+        return 'Owner';
+      case Role.Admin:
+        return 'Admin';
+      case Role.Developer:
+        return 'Developer';
+      case Role.ViewOnly:
+        return 'Viewer';
+      case Role.Custom:
+      case Role.HasAdmin:
+      case Role.NotAdmin:
+        return 'Custom';
     }
   }
 }
