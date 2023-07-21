f42246c232d52bf51959c3d6310c193f3bdf7b5c
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-07-21 14:09:18 +0200

    [eas-cli] Fix test
    The `currentMachineMethod` tests used to be platform dependent, since the function checks for the architecture of the machine. This check is now mocked to make the tests platform agnostic
    
    See: https://linear.app/expo/issue/ENG-8475/investigate-grabbing-registering-macos-provisioning-udid-from-eas-cli

--- packages/eas-cli/src/devices/actions/create/__tests__/currentMachineMethod-test.ts
+++ packages/eas-cli/src/devices/actions/create/__tests__/currentMachineMethod-test.ts
@@ -1,13 +1,24 @@
+import os from 'os';
 import prompts from 'prompts';
 import { instance, mock } from 'ts-mockito';
 
 import { ExpoGraphqlClient } from '../../../../commandUtils/context/contextUtils/createGraphqlClient';
 import { AppleDeviceMutation } from '../../../../credentials/ios/api/graphql/mutations/AppleDeviceMutation';
 import { AppleDeviceClass, AppleTeam } from '../../../../graphql/generated';
+import { DeviceCreateError } from '../../../utils/errors';
 import { runCurrentMachineMethodAsync } from '../currentMachineMethod';
+
+jest.setTimeout(60000);
 
 jest.mock('../../../../credentials/ios/api/graphql/mutations/AppleDeviceMutation');
 jest.mock('../../../../ora');
+jest.mock('os', () => {
+  return {
+    ...jest.requireActual('os'),
+    cpus: jest.fn(),
+    arch: jest.fn(),
+  };
+});
 
 beforeEach(() => {
   jest.mocked(prompts).mockReset();
@@ -19,6 +30,8 @@ beforeEach(() => {
 
 describe(runCurrentMachineMethodAsync, () => {
   it('proceeds with registration if user approves', async () => {
+    jest.mocked(os.cpus).mockImplementation(() => [{ model: 'Apple M1' } as os.CpuInfo]);
+    jest.mocked(os.arch).mockImplementation(() => 'arm64');
     mockDeviceData('my iPhone', AppleDeviceClass.Iphone);
     jest.mocked(prompts).mockImplementationOnce(async () => ({ value: true }));
     mockDeviceData('my iPad', AppleDeviceClass.Ipad);
@@ -39,6 +52,8 @@ describe(runCurrentMachineMethodAsync, () => {
   });
 
   it('exits registration if user cancels', async () => {
+    jest.mocked(os.cpus).mockImplementation(() => [{ model: 'Apple M1' } as os.CpuInfo]);
+    jest.mocked(os.arch).mockImplementation(() => 'arm64');
     mockDeviceData('my iPhone', AppleDeviceClass.Iphone);
     jest.mocked(prompts).mockImplementationOnce(async () => ({ value: false }));
     mockDeviceData('my iPad', AppleDeviceClass.Ipad);
@@ -54,6 +69,36 @@ describe(runCurrentMachineMethodAsync, () => {
     };
 
     await runCurrentMachineMethodAsync(graphqlClient, accountId, appleTeam);
+
+    expect(AppleDeviceMutation.createAppleDeviceAsync).toHaveBeenCalledTimes(0);
+  });
+
+  it('exits registration if current machine is not of Apple Silicon type', async () => {
+    jest
+      .mocked(os.cpus)
+      .mockImplementation(() => [{ model: 'Intel(R) Core(TM) i5' } as os.CpuInfo]);
+    jest.mocked(os.arch).mockImplementation(() => 'x64');
+    mockDeviceData('my iPhone', AppleDeviceClass.Iphone);
+    jest.mocked(prompts).mockImplementationOnce(async () => ({ value: true }));
+    mockDeviceData('my iPad', AppleDeviceClass.Ipad);
+    jest.mocked(prompts).mockImplementationOnce(async () => ({ value: false }));
+
+    const graphqlClient = instance(mock<ExpoGraphqlClient>());
+    const accountId = 'account-id';
+    // @ts-expect-error appleTeam is missing properties of AppleTeam GraphQL type
+    const appleTeam: AppleTeam = {
+      id: 'apple-team-id',
+      appleTeamIdentifier: 'ABC123XY',
+      appleTeamName: 'John Doe (Individual)',
+    };
+
+    await expect(
+      runCurrentMachineMethodAsync(graphqlClient, accountId, appleTeam)
+    ).rejects.toThrowError(
+      new DeviceCreateError(
+        "Current machine is not of Apple Silicon type - provisioning UDID can't be added automatically."
+      )
+    );
 
     expect(AppleDeviceMutation.createAppleDeviceAsync).toHaveBeenCalledTimes(0);
   });
