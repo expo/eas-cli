dd12d5ef594e88eaebd31f5cba125f7823556ef7
Quinlan Jung <quin@expo.io>
2023-08-25 13:12:45 -0700

    [eas-cli] fix branch with no update printing bug

--- packages/eas-cli/src/channel/print-utils.ts
+++ packages/eas-cli/src/channel/print-utils.ts
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import chalk from 'chalk';
 
 import { UpdateChannelObject } from '../graphql/queries/ChannelQuery';
@@ -11,17 +10,35 @@ import {
 } from '../update/utils';
 import { assertVersion, hasEmptyBranchMap, hasStandardBranchMap } from './branch-mapping';
 
+/**
+ * Log all the branches associated with the channel and their most recent update group
+ */
 export function logChannelDetails(channel: UpdateChannelObject): void {
   assertVersion(channel, 0);
-  assert(
-    hasEmptyBranchMap(channel) || hasStandardBranchMap(channel) || isRollout(channel),
-    'Only standard branch mappings and rollouts are supported.'
-  );
+  const doesSupportMapping =
+    hasEmptyBranchMap(channel) || hasStandardBranchMap(channel) || isRollout(channel);
+  if (!doesSupportMapping) {
+    Log.log(chalk.dim('Custom branch mapping detected.'));
+    return;
+  }
 
-  const branchDescription = channel.updateBranches.flatMap(branch => {
-    const updateGroupWithBranchDescriptions = getUpdateGroupDescriptionsWithBranch(
-      branch.updateGroups
+  const branchDescriptionByBranchId = getDescriptionByBranchId(channel);
+  const entries = Object.entries(branchDescriptionByBranchId);
+  if (entries.length === 0) {
+    Log.log(chalk.dim('No branches are pointed to this channel.'));
+  } else {
+    Log.log(
+      entries
+        .map(([_branchId, description]) => formatBranch(description))
+        .join(`\n\n${chalk.dim('———')}\n\n`)
     );
+  }
+}
+
+export function getDescriptionByBranchId(
+  channel: UpdateChannelObject
+): Record<string, FormattedBranchDescription> {
+  return channel.updateBranches.reduce((acc, branch) => {
     const maybeRollout = isRollout(channel) ? getRollout(channel) : null;
     let maybePercentOnBranch: number | undefined = undefined;
     if (maybeRollout) {
@@ -31,22 +48,21 @@ export function logChannelDetails(channel: UpdateChannelObject): void {
           : 100 - maybeRollout.percentRolledOut;
     }
 
-    return updateGroupWithBranchDescriptions.map(
-      ({ branch, ...updateGroup }): FormattedBranchDescription => ({
-        branch,
-        branchRolloutPercentage: maybePercentOnBranch,
-        update: updateGroup,
-      })
+    if (branch.updateGroups.length === 0) {
+      acc[branch.id] = { branch: branch.name, branchRolloutPercentage: maybePercentOnBranch };
+      return acc;
+    }
+    const updateGroupsWithBranchDescriptions = getUpdateGroupDescriptionsWithBranch(
+      branch.updateGroups
     );
-  });
-
-  if (branchDescription.length === 0) {
-    Log.log(chalk.dim('No branches are pointed to this channel.'));
-  } else {
-    Log.log(
-      branchDescription
-        .map(description => formatBranch(description))
-        .join(`\n\n${chalk.dim('———')}\n\n`)
-    );
-  }
+    // display the most recent update group
+    const updateGroupWithBranchDescription = updateGroupsWithBranchDescriptions[0];
+    const { branch: branchName, ...updateGroup } = updateGroupWithBranchDescription;
+    acc[branch.id] = {
+      branch: branchName,
+      branchRolloutPercentage: maybePercentOnBranch,
+      update: updateGroup,
+    };
+    return acc;
+  }, {} as Record<string, FormattedBranchDescription>);
 }
