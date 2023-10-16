c5d75568ef49562d852407a31ecf0fb7e0f79606
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-10-16 16:03:25 +0200

    Revert "[eas-cli] Fix mutation"
    This reverts commit 606ce9a1fff18681267fc73f17051ae9c890503e.

--- packages/eas-cli/src/credentials/android/api/GraphqlClient.ts
+++ packages/eas-cli/src/credentials/android/api/GraphqlClient.ts
@@ -260,6 +260,7 @@ export async function createKeystoreAsync(
       keystorePassword: keystore.keystorePassword,
       keyAlias: keystore.keyAlias,
       keyPassword: keystore.keyPassword,
+      type: keystore.type,
     },
     account.id
   );
