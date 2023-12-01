3a2bb5bf07eec89b91ad5feba1879e440646b9aa
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-12-01 16:17:01 +0100

    Revert "[eas-cli] Add getters"
    This reverts commit 766d48e1efc6f8e5d128ad4c977493f01bbed3cb.

--- packages/eas-cli/src/build/types.ts
+++ packages/eas-cli/src/build/types.ts
@@ -8,28 +8,8 @@ export enum BuildStatus {
   CANCELED = 'canceled',
 }
 
-export function maybeGetBuildStatus(
-  buildStatusString: string | undefined
-): BuildStatus | undefined {
-  if (!buildStatusString) {
-    return undefined;
-  }
-  return Object.values(BuildStatus).find(buildStatus => buildStatus === buildStatusString);
-}
-
 export enum BuildDistributionType {
   STORE = 'store',
   INTERNAL = 'internal',
   SIMULATOR = 'simulator',
-}
-
-export function maybeGetBuildDistributionType(
-  buildDistributionTypeString: string | undefined
-): BuildDistributionType | undefined {
-  if (!buildDistributionTypeString) {
-    return undefined;
-  }
-  return Object.values(BuildDistributionType).find(
-    buildDistributionType => buildDistributionType === buildDistributionTypeString
-  );
 }
