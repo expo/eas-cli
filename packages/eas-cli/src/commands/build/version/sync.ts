b28620f435de52a613884a5df5d8c77913b31318
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-12-01 16:16:52 +0100

    Revert "[eas-cli] Fix flags"
    This reverts commit 669b21229fa22589fe038a2136f6978886108225.

--- packages/eas-cli/src/commands/build/version/sync.ts
+++ packages/eas-cli/src/commands/build/version/sync.ts
@@ -44,7 +44,7 @@ export default class BuildVersionSyncView extends EasCommand {
     'update a version in native code with a value stored on EAS servers';
 
   public static override flags = {
-    platform: Flags.string({
+    platform: Flags.enum({
       char: 'p',
       options: ['android', 'ios', 'all'],
     }),
