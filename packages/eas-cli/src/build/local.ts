533bde24424f1a93409b17133a7d975c9a47b304
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2023-12-15 11:33:27 +0100

    upgrade eas-cli-local-build-plugin to 1.0.55

--- packages/eas-cli/src/build/local.ts
+++ packages/eas-cli/src/build/local.ts
@@ -6,7 +6,7 @@ import semver from 'semver';
 import { ora } from '../ora';
 
 const PLUGIN_PACKAGE_NAME = 'eas-cli-local-build-plugin';
-const PLUGIN_PACKAGE_VERSION = '1.0.49';
+const PLUGIN_PACKAGE_VERSION = '1.0.55';
 
 export enum LocalBuildMode {
   /**
