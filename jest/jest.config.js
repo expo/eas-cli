3cb2b4cef6a530a0609e26322a79f607164f9984
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-01-12 10:50:25 +0100

    bump dependencies and fix errors/warnings

--- jest/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '/__tests__/.*(test|spec)\\.[jt]sx?$',
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx,js,jsx}'],
-};
