307062b2164a2352b3d5f0edcbaab518125c180a
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-01-16 12:16:18 +0100

    fix imports

--- packages/eas-cli/src/credentials/ios/actions/SetUpAdhocProvisioningProfile.ts
+++ packages/eas-cli/src/credentials/ios/actions/SetUpAdhocProvisioningProfile.ts
@@ -4,10 +4,6 @@ import assert from 'assert';
 import chalk from 'chalk';
 import nullthrows from 'nullthrows';
 
-import { resolveAppleTeamIfAuthenticatedAsync } from './AppleTeamUtils';
-import { assignBuildCredentialsAsync, getBuildCredentialsAsync } from './BuildCredentialsUtils';
-import { chooseDevicesAsync, formatDeviceLabel } from './DeviceUtils';
-import { SetUpDistributionCertificate } from './SetUpDistributionCertificate';
 import DeviceCreateAction, { RegistrationMethod } from '../../../devices/actions/create/action';
 import {
   AppleAppIdentifierFragment,
@@ -34,6 +30,10 @@ import { ProvisioningProfile } from '../appstore/Credentials.types';
 import { ApplePlatform } from '../appstore/constants';
 import { Target } from '../types';
 import { validateProvisioningProfileAsync } from '../validators/validateProvisioningProfile';
+import { resolveAppleTeamIfAuthenticatedAsync } from './AppleTeamUtils';
+import { assignBuildCredentialsAsync, getBuildCredentialsAsync } from './BuildCredentialsUtils';
+import { chooseDevicesAsync, formatDeviceLabel } from './DeviceUtils';
+import { SetUpDistributionCertificate } from './SetUpDistributionCertificate';
 
 enum ReuseAction {
   Yes,
