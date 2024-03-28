26ba0719ce1a44e292b5f56f5ed28660191aa464
Juwan Wheatley <juwan@expo.dev>
2024-03-28 15:55:19 -0400

    [eas-cli] add a `credentials:configure-build` subcommand that prepares a project for EAS Build without executing the build from the CLI (#2282)
    * [eas-cli] create new `init-build` command under the `credentials` topic
    
    * get hacky version of cli command working
    
    * add all option to flag
    
    * extend the Manage<Platform> actions to reduce code duplication
    
    * prompt user for platform if it doesn't exist
    
    * prompt the user to choose a profile
    
    * add a changelog entry
    
    * call SetUpBuildCredentials for android

---
+++ packages/eas-cli/src/commands/credentials/configure-build.ts
@@ -0,0 +1,64 @@
+import { Platform } from '@expo/eas-build-job';
+import { Flags } from '@oclif/core';
+
+import EasCommand from '../../commandUtils/EasCommand';
+import { SelectBuildProfileFromEasJson } from '../../credentials/manager/SelectBuildProfileFromEasJson';
+import { SetUpBuildCredentialsCommandAction } from '../../credentials/manager/SetUpBuildCredentialsCommandAction';
+import { selectPlatformAsync } from '../../platform';
+
+export default class InitializeBuildCredentials extends EasCommand {
+  static override description = 'Set up credentials for building your project.';
+
+  static override flags = {
+    platform: Flags.enum({
+      char: 'p',
+      options: [Platform.ANDROID, Platform.IOS],
+    }),
+    profile: Flags.string({
+      char: 'e',
+      description: 'The name of the build profile in eas.json.',
+      helpValue: 'PROFILE_NAME',
+    }),
+  };
+
+  static override contextDefinition = {
+    ...this.ContextOptions.LoggedIn,
+    ...this.ContextOptions.ProjectConfig,
+    ...this.ContextOptions.DynamicProjectConfig,
+    ...this.ContextOptions.Analytics,
+    ...this.ContextOptions.Vcs,
+  };
+
+  async runAsync(): Promise<void> {
+    const { flags } = await this.parse(InitializeBuildCredentials);
+    const {
+      loggedIn: { actor, graphqlClient },
+      privateProjectConfig,
+      getDynamicPrivateProjectConfigAsync,
+      analytics,
+      vcsClient,
+    } = await this.getContextAsync(InitializeBuildCredentials, {
+      nonInteractive: false,
+    });
+
+    const platform = await selectPlatformAsync(flags.platform);
+
+    const buildProfile =
+      flags.profile ??
+      (await new SelectBuildProfileFromEasJson(
+        privateProjectConfig.projectDir,
+        Platform.IOS
+      ).getProfileNameFromEasConfigAsync());
+
+    await new SetUpBuildCredentialsCommandAction(
+      actor,
+      graphqlClient,
+      vcsClient,
+      analytics,
+      privateProjectConfig ?? null,
+      getDynamicPrivateProjectConfigAsync,
+      platform,
+      buildProfile
+    ).runAsync();
+  }
+}
---
+++ packages/eas-cli/src/credentials/manager/SetUpIosBuildCredentials.ts
@@ -0,0 +1,78 @@
+import { Platform } from '@expo/eas-build-job';
+import assert from 'assert';
+import nullthrows from 'nullthrows';
+
+import { IosActionType } from './Actions';
+import { CheckBuildProfileFlagAgainstEasJson } from './CheckBuildProfileFlagAgainstEasJson';
+import { Action } from './HelperActions';
+import { ManageIos } from './ManageIos';
+import { AccountFragment } from '../../graphql/generated';
+import { getOwnerAccountForProjectIdAsync } from '../../project/projectUtils';
+import { ensureActorHasPrimaryAccount } from '../../user/actions';
+import { CredentialsContext, CredentialsContextProjectInfo } from '../context';
+
+export class SetUpIosBuildCredentials extends ManageIos {
+  constructor(
+    callingAction: Action,
+    projectDir: string,
+    private setUpBuildCredentialsWithProfileNameFromFlag: string
+  ) {
+    super(callingAction, projectDir);
+  }
+
+  override async runAsync(): Promise<void> {
+    const buildProfile = this.callingAction.projectInfo
+      ? await new CheckBuildProfileFlagAgainstEasJson(
+          this.projectDir,
+          Platform.IOS,
+          this.setUpBuildCredentialsWithProfileNameFromFlag
+        ).runAsync()
+      : null;
+
+    let projectInfo: CredentialsContextProjectInfo | null = null;
+    if (this.callingAction.projectInfo) {
+      const { exp, projectId } = await this.callingAction.getDynamicPrivateProjectConfigAsync({
+        env: buildProfile?.env,
+      });
+      projectInfo = { exp, projectId };
+    }
+
+    const ctx = new CredentialsContext({
+      projectDir: process.cwd(),
+      projectInfo,
+      user: this.callingAction.actor,
+      graphqlClient: this.callingAction.graphqlClient,
+      analytics: this.callingAction.analytics,
+      env: buildProfile?.env,
+      nonInteractive: false,
+      vcsClient: this.callingAction.vcsClient,
+    });
+
+    await ctx.bestEffortAppStoreAuthenticateAsync();
+
+    const getAccountForProjectAsync = async (projectId: string): Promise<AccountFragment> => {
+      return await getOwnerAccountForProjectIdAsync(ctx.graphqlClient, projectId);
+    };
+
+    const account = ctx.hasProjectContext
+      ? await getAccountForProjectAsync(ctx.projectId)
+      : ensureActorHasPrimaryAccount(ctx.user);
+
+    let app = null;
+    let targets = null;
+    if (ctx.hasProjectContext) {
+      assert(buildProfile, 'buildProfile must be defined in project context');
+      const projectContext = await this.createProjectContextAsync(ctx, account, buildProfile);
+      app = projectContext.app;
+      targets = projectContext.targets;
+    }
+
+    await this.runProjectSpecificActionAsync(
+      ctx,
+      nullthrows(app, 'app must be defined in project context'),
+      nullthrows(targets, 'targets must be defined in project context'),
+      nullthrows(buildProfile, 'buildProfile must be defined in project context'),
+      IosActionType.SetUpBuildCredentials
+    );
+  }
+}
