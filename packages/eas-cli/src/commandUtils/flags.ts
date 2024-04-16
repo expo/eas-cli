1e7d19f55ce6f4b67fc59d3439c5a788e9d9162d
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2024-04-16 16:33:04 +0200

    [ENG-11913] Implement `eas env` commands

--- packages/eas-cli/src/commandUtils/flags.ts
+++ packages/eas-cli/src/commandUtils/flags.ts
@@ -1,5 +1,6 @@
 import { Flags } from '@oclif/core';
 
+<<<<<<< HEAD
 import { EnvironmentVariableEnvironment, EnvironmentVariableScope } from '../graphql/generated';
 
 async function upperCaseAsync(input: string): Promise<string> {
@@ -10,6 +11,11 @@ function mapToLowercase<T extends string>(options: T[]): string[] {
   return options.map(option => option.toLowerCase());
 }
 
+||||||| parent of 2b270de0 ([ENG-11913] Implement `eas env` commands)
+=======
+import { EnvironmentVariableEnvironment, EnvironmentVariableScope } from '../graphql/generated';
+
+>>>>>>> 2b270de0 ([ENG-11913] Implement `eas env` commands)
 export const EasNonInteractiveAndJsonFlags = {
   json: Flags.boolean({
     description: 'Enable JSON output, non-JSON messages will be printed to stderr.',
@@ -20,6 +26,7 @@ export const EasNonInteractiveAndJsonFlags = {
   }),
 };
 
+<<<<<<< HEAD
 export const EASEnvironmentFlag = {
   environment: Flags.enum({
     description: 'Environment to create the secret in',
@@ -56,6 +63,48 @@ export const EASVariableScopeFlag = {
   }),
 };
 
+||||||| parent of 2b270de0 ([ENG-11913] Implement `eas env` commands)
+=======
+async function upperCaseAsync(input: string): Promise<string> {
+  return input.toUpperCase();
+}
+
+export const EASEnvironmentFlag = {
+  environment: Flags.enum({
+    description: 'Environment to create the secret in',
+    parse: upperCaseAsync,
+    options: [
+      EnvironmentVariableEnvironment.Development,
+      EnvironmentVariableEnvironment.Preview,
+      EnvironmentVariableEnvironment.Production,
+    ].map(env => env.toLowerCase()),
+  }),
+};
+
+export const EASVariableFormatFlag = {
+  format: Flags.string({
+    description: 'Output format',
+    options: ['long', 'short'],
+    default: 'short',
+  }),
+};
+
+export const EASVariableSensitiveFlag = {
+  sensitive: Flags.boolean({
+    description: 'Encrypt variable value at rest',
+    default: false,
+  }),
+};
+
+export const EASVariableScopeFlag = {
+  scope: Flags.enum({
+    description: 'Scope for the variable',
+    options: [EnvironmentVariableScope.Shared, EnvironmentVariableScope.Project],
+    default: EnvironmentVariableScope.Project,
+  }),
+};
+
+>>>>>>> 2b270de0 ([ENG-11913] Implement `eas env` commands)
 export const EASNonInteractiveFlag = {
   'non-interactive': Flags.boolean({
     description: 'Run the command in non-interactive mode.',
