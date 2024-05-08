b90e82bfd7bb8855e9c6391884b0cfcb53ade3fe
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-05-08 14:43:26 +0200

    fix type issues

--- packages/eas-cli/src/credentials/__tests__/fixtures-constants.ts
+++ packages/eas-cli/src/credentials/__tests__/fixtures-constants.ts
@@ -1,3 +1,5 @@
+import assert from 'node:assert';
+
 import { AppFragment, Role } from '../../graphql/generated';
 import { Actor } from '../../user/User';
 
@@ -24,6 +26,7 @@ export const jester: Actor = {
   ],
   isExpoAdmin: false,
   featureGates: {},
+  preferences: {},
 };
 
 export const jester2 = {
@@ -41,6 +44,7 @@ export const jester2 = {
   featureGates: {},
 };
 
+assert(jester.__typename === 'User');
 export const testUsername = jester.username;
 export const testSlug = 'testApp';
 export const testProjectId = '7ef93448-3bc7-4b57-be32-99326dcf24f0';
