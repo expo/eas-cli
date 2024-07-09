c07b94776563084453085c042375140509328da8
Kadi Kraman <kadi@expo.io>
2024-07-09 11:29:15 +0100

    Validate for dev domain name taken

--- packages/eas-cli/src/worker/deployment.ts
+++ packages/eas-cli/src/worker/deployment.ts
@@ -3,6 +3,7 @@ import { CombinedError as GraphqlError } from '@urql/core';
 
 import { DeploymentsMutation } from './mutations';
 import { ExpoGraphqlClient } from '../commandUtils/context/contextUtils/createGraphqlClient';
+import Log from '../log';
 import { promptAsync } from '../prompts';
 
 export async function getSignedDeploymentUrlAsync(
@@ -27,32 +28,62 @@ export async function getSignedDeploymentUrlAsync(
       throw error;
     }
 
-    const validationMessage = 'The project does not have a dev domain name.';
-    const { name } = await promptAsync({
-      type: 'text',
-      name: 'name',
-      message: 'Choose a dev domain name for your project:',
-      validate: value => (value && value.length > 3 ? true : validationMessage),
-      initial: exp.slug,
+    await chooseDevDomainNameAsync({
+      graphqlClient,
+      appId: deploymentVariables.appId,
+      slug: exp.slug,
     });
 
-    if (!name) {
-      throw new Error('Prompt failed');
-    }
+    return await DeploymentsMutation.createSignedDeploymentUrlAsync(
+      graphqlClient,
+      deploymentVariables
+    );
+  }
+}
 
-    // TODO(Kadi): handle the case where the chosen name already exists
+async function chooseDevDomainNameAsync({
+  graphqlClient,
+  appId,
+  slug,
+}: {
+  graphqlClient: ExpoGraphqlClient;
+  appId: string;
+  slug: string;
+}): Promise<void> {
+  const validationMessage = 'The project does not have a dev domain name.';
+  const { name } = await promptAsync({
+    type: 'text',
+    name: 'name',
+    message: 'Choose a dev domain name for your project:',
+    validate: value => (value && value.length > 3 ? true : validationMessage),
+    initial: slug,
+  });
+
+  if (!name) {
+    throw new Error('Prompt failed');
+  }
+
+  try {
     const success = await DeploymentsMutation.assignDevDomainNameAsync(graphqlClient, {
-      appId: deploymentVariables.appId,
+      appId,
       name,
     });
 
     if (!success) {
       throw new Error('Failed to assign dev domain name');
     }
-
-    return await DeploymentsMutation.createSignedDeploymentUrlAsync(
-      graphqlClient,
-      deploymentVariables
+  } catch (error: any) {
+    const isChosenNameTaken = (error as GraphqlError)?.graphQLErrors?.some(e =>
+      ['DEV_DOMAIN_NAME_TAKEN'].includes(e?.extensions?.errorCode as string)
     );
+
+    if (isChosenNameTaken) {
+      Log.error(`The entered dev domain name "${name}" is taken. Choose a different name.`);
+      await chooseDevDomainNameAsync({ graphqlClient, appId, slug });
+    }
+
+    if (!isChosenNameTaken) {
+      throw error;
+    }
   }
 }
