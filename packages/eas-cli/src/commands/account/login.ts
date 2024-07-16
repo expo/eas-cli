e2bf428ef727add59c46772e5c2544e07eab8168
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-07-16 16:56:45 +0200

    eas login and eas whoami improvements

--- packages/eas-cli/src/commands/account/login.ts
+++ packages/eas-cli/src/commands/account/login.ts
@@ -1,7 +1,10 @@
-import { Flags } from '@oclif/core';
+import { Errors, Flags } from '@oclif/core';
+import chalk from 'chalk';
 
 import EasCommand from '../../commandUtils/EasCommand';
 import Log from '../../log';
+import { confirmAsync } from '../../prompts';
+import { getActorDisplayName } from '../../user/User';
 
 export default class AccountLogin extends EasCommand {
   static override description = 'log in with your Expo account';
@@ -17,6 +20,7 @@ export default class AccountLogin extends EasCommand {
   };
 
   static override contextDefinition = {
+    ...this.ContextOptions.MaybeLoggedIn,
     ...this.ContextOptions.SessionManagment,
   };
 
@@ -25,7 +29,46 @@ export default class AccountLogin extends EasCommand {
       flags: { sso },
     } = await this.parse(AccountLogin);
 
-    const { sessionManager } = await this.getContextAsync(AccountLogin, { nonInteractive: false });
+    const {
+      sessionManager,
+      maybeLoggedIn: { actor },
+    } = await this.getContextAsync(AccountLogin, { nonInteractive: false });
+
+    if (sessionManager.getAccessToken()) {
+      Log.warn(
+        'We detected that you have set EXPO_TOKEN in your environment. If EXPO_TOKEN is set, it takes precedence over the current session as an authentication method.'
+      );
+      Log.warn(
+        'Even if you log in with a different account, EXPO_TOKEN will still be used if set.'
+      );
+      Log.warn(
+        `You don't need to use ${chalk.bold(
+          'eas login'
+        )} command if you are planning to use EXPO_TOKEN.`
+      );
+
+      const shouldContinue = await confirmAsync({
+        message: 'Do you want to continue?',
+      });
+      if (!shouldContinue) {
+        Errors.error('Aborted', { exit: 1 });
+      }
+    }
+
+    if (actor) {
+      const loggedInAs = sessionManager.getAccessToken()
+        ? `${getActorDisplayName(actor)} (by using EXPO_TOKEN)`
+        : getActorDisplayName(actor);
+      Log.warn(`You are already logged in as ${chalk.bold(loggedInAs)}.`);
+
+      const shouldContinue = await confirmAsync({
+        message: 'Do you want to continue?',
+      });
+      if (!shouldContinue) {
+        Errors.error('Aborted', { exit: 1 });
+      }
+    }
+
     await sessionManager.showLoginPromptAsync({ sso });
     Log.log('Logged in');
   }
