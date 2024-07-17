f507b9d8832be96bd5b9be11e58785cef92c6ffc
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-07-17 11:55:20 +0200

    apply suggested change

--- packages/eas-cli/src/commands/account/login.ts
+++ packages/eas-cli/src/commands/account/login.ts
@@ -35,31 +35,13 @@ export default class AccountLogin extends EasCommand {
     } = await this.getContextAsync(AccountLogin, { nonInteractive: false });
 
     if (sessionManager.getAccessToken()) {
-      Log.warn(
-        'We detected that you have set EXPO_TOKEN in your environment. If EXPO_TOKEN is set, it takes precedence over the current session as an authentication method.'
+      throw new Error(
+        'EXPO_TOKEN is set in your environment, and is being used for all EAS authentication. To use username/password authentication, unset EXPO_TOKEN in your environment and re-run the command.'
       );
-      Log.warn(
-        'Even if you log in with a different account, EXPO_TOKEN will still be used if set.'
-      );
-      Log.warn(
-        `You don't need to use ${chalk.bold(
-          'eas login'
-        )} command if you are planning to use EXPO_TOKEN.`
-      );
-
-      const shouldContinue = await confirmAsync({
-        message: 'Do you want to continue?',
-      });
-      if (!shouldContinue) {
-        Errors.error('Aborted', { exit: 1 });
-      }
     }
 
     if (actor) {
-      const loggedInAs = sessionManager.getAccessToken()
-        ? `${getActorDisplayName(actor)} (by using EXPO_TOKEN)`
-        : getActorDisplayName(actor);
-      Log.warn(`You are already logged in as ${chalk.bold(loggedInAs)}.`);
+      Log.warn(`You are already logged in as ${chalk.bold(getActorDisplayName(actor))}.`);
 
       const shouldContinue = await confirmAsync({
         message: 'Do you want to continue?',
