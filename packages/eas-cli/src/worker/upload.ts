dfc086517c14fcb9ff35dee3dcbb9aae67c947f9
Phil Pluckthun <phil@kitten.sh>
2024-08-07 17:31:29 +0100

    Add progress indicator and 413 error message

--- packages/eas-cli/src/worker/upload.ts
+++ packages/eas-cli/src/worker/upload.ts
@@ -1,3 +1,6 @@
+import { ora } from '../ora';
+import Log from '../log';
+
 export interface UploadResultName {
   fullName: string;
   name: string;
@@ -8,6 +11,10 @@ export const uploadWorkerAsync = async (
   url: string,
   contents: BodyInit
 ): Promise<UploadResultName> => {
+  Log.addNewLineIfNone();
+
+  const uploadSpinner = ora('Uploading & Deploying').start();
+
   const response = await fetch(url, {
     method: 'POST',
     body: contents,
@@ -18,8 +25,19 @@ export const uploadWorkerAsync = async (
     if (!json.success) {
       throw new Error(json.message ? `${json.message}` : 'Upload failed!');
     }
+
+    uploadSpinner.succeed('Deployed worker');
     return json.result;
   } else {
+    uploadSpinner.fail('Failed to deploy worker');
+
+    if (response.status === 413) {
+      throw new Error(
+        'Upload failed! (Payload too large)\n' +
+          'The combined file-size of your upload was too large.'
+      );
+    }
+
     throw new Error(`Upload failed! (${response.statusText})`);
   }
 };
