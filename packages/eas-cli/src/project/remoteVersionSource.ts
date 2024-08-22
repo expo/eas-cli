d3099559167053d70781177b1132d281b16c61b2
Radosław Krzemień <radoslaw.krzemien@swmansion.com>
2024-08-22 14:43:52 +0200

    Update packages/eas-cli/src/project/remoteVersionSource.ts
    Co-authored-by: Szymon Dziedzic <szymon.dziedzic@swmansion.com>

--- packages/eas-cli/src/project/remoteVersionSource.ts
+++ packages/eas-cli/src/project/remoteVersionSource.ts
@@ -71,13 +71,7 @@ export async function validateBuildProfileVersionSettingsAsync(
     profileInfo.profile.autoIncrement !== 'version'
   ) {
     if (profileInfo.profile.autoIncrement !== true) {
-      Log.log('The field "cli.appVersionSource" is not set, but it will be required in the future');
-      Log.log(
-        learnMore('https://docs.expo.dev/build-reference/app-versions/', {
-          learnMoreMessage: 'See the docs to learn more.',
-          dim: false,
-        })
-      );
+      Log.warn(`The field "cli.appVersionSource" is not set, but it will be required in the future. ${learnMore('https://docs.expo.dev/build-reference/app-versions/')}`);
     } else {
       const easJsonAccessor = EasJsonAccessor.fromProjectPath(projectDir);
       cliConfig = await ensureAppVersionSourceIsSetAsync(
