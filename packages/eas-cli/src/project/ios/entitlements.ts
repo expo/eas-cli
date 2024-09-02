1a70cb60a05929affc2e9152f5a0d82547f1a97f
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-09-02 18:12:42 +0200

    use @expo/prebuild-config and @expo/config-plugins shipped with expo package as well

--- packages/eas-cli/src/project/ios/entitlements.ts
+++ packages/eas-cli/src/project/ios/entitlements.ts
@@ -1,7 +1,9 @@
-import { IOSConfig, compileModsAsync } from '@expo/config-plugins';
+import { IOSConfig, compileModsAsync as _compileModsAsync } from '@expo/config-plugins';
 import { JSONObject } from '@expo/json-file';
-import { getPrebuildConfigAsync } from '@expo/prebuild-config';
+import { getPrebuildConfigAsync as _getPrebuildConfigAsync } from '@expo/prebuild-config';
+import path from 'path';
 
+import Log from '../../log';
 import { readPlistAsync } from '../../utils/plist';
 import { Client } from '../../vcs/vcs';
 import { hasIgnoredIosProjectAsync } from '../workflow';
@@ -22,8 +24,42 @@ export async function getManagedApplicationTargetEntitlementsAsync(
       ...process.env,
       ...env,
     };
+    const projectExpoPrebuildConfigPath = path.join(
+      projectDir,
+      'node_modules',
+      '@expo',
+      'prebuild-config'
+    );
+    let getPrebuildConfigAsync: typeof _getPrebuildConfigAsync;
+    try {
+      const expoPrebuildConfig = require(projectExpoPrebuildConfigPath);
+      getPrebuildConfigAsync = expoPrebuildConfig.getPrebuildConfigAsync;
+    } catch (error: any) {
+      Log.warn(
+        `Failed to load getPrebuildConfigAsync function from ${projectExpoPrebuildConfigPath}: ${error.message}`
+      );
+      Log.warn('Falling back to the version of @expo/prebuild-config shipped with the EAS CLI.');
+      getPrebuildConfigAsync = _getPrebuildConfigAsync;
+    }
     const { exp } = await getPrebuildConfigAsync(projectDir, { platforms: ['ios'] });
 
+    const projectExpoConfigPluginsPath = path.join(
+      projectDir,
+      'node_modules',
+      '@expo',
+      'config-plugins'
+    );
+    let compileModsAsync: typeof _compileModsAsync;
+    try {
+      const expoConfigPlugins = require(projectExpoConfigPluginsPath);
+      compileModsAsync = expoConfigPlugins.compileModsAsync;
+    } catch (error: any) {
+      Log.warn(
+        `Failed to load compileModsAsync function from ${projectExpoConfigPluginsPath}: ${error.message}`
+      );
+      Log.warn('Falling back to the version of @expo/confi-plugins shipped with the EAS CLI.');
+      compileModsAsync = _compileModsAsync;
+    }
     const expWithMods = await compileModsAsync(exp, {
       projectRoot: projectDir,
       platforms: ['ios'],
