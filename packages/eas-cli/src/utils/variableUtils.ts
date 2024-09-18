db829d8fbc5365da5ca9afc0795a5cc26e266463
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2024-09-18 17:21:51 +0200

    Add tests for EnvironmentVariableDelete

--- packages/eas-cli/src/utils/variableUtils.ts
+++ packages/eas-cli/src/utils/variableUtils.ts
@@ -1,7 +1,19 @@
 import dateFormat from 'dateformat';
 
 import formatFields from './formatFields';
-import { EnvironmentVariableEnvironment, EnvironmentVariableFragment } from '../graphql/generated';
+import {
+  EnvironmentVariableEnvironment,
+  EnvironmentVariableFragment,
+  EnvironmentVariableScope,
+} from '../graphql/generated';
+
+export function formatVariableName(variable: EnvironmentVariableFragment): string {
+  const name = variable.name;
+  const scope = variable.scope === EnvironmentVariableScope.Project ? 'project' : 'shared';
+  const environments = variable.environments?.join(', ') ?? '';
+  const updatedAt = variable.updatedAt ? new Date(variable.updatedAt).toLocaleString() : '';
+  return `${name} | ${scope} | ${environments} | Updated at: ${updatedAt}`;
+}
 
 export async function performForEnvironmentsAsync(
   environments: EnvironmentVariableEnvironment[] | null,
