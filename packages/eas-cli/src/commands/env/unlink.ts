f36d44f8958f732ea1111d45fb61585fec0d262b
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2024-09-19 17:02:35 +0200

    Refactor env:unlink, add tests

--- packages/eas-cli/src/commands/env/unlink.ts
+++ packages/eas-cli/src/commands/env/unlink.ts
@@ -2,14 +2,15 @@ import { Flags } from '@oclif/core';
 import chalk from 'chalk';
 
 import EasCommand from '../../commandUtils/EasCommand';
-import { EASEnvironmentFlag, EASNonInteractiveFlag } from '../../commandUtils/flags';
-import { EnvironmentVariableScope } from '../../graphql/generated';
+import { EASMultiEnvironmentFlag, EASNonInteractiveFlag } from '../../commandUtils/flags';
+import { EnvironmentVariableEnvironment } from '../../graphql/generated';
 import { EnvironmentVariableMutation } from '../../graphql/mutations/EnvironmentVariableMutation';
 import { EnvironmentVariablesQuery } from '../../graphql/queries/EnvironmentVariablesQuery';
 import Log from '../../log';
 import { getDisplayNameForProjectIdAsync } from '../../project/projectUtils';
 import { selectAsync } from '../../prompts';
 import { promptVariableEnvironmentAsync } from '../../utils/prompts';
+import { formatVariableName } from '../../utils/variableUtils';
 
 export default class EnvironmentVariableUnlink extends EasCommand {
   static override description = 'unlink a shared environment variable to the current project';
@@ -17,10 +18,10 @@ export default class EnvironmentVariableUnlink extends EasCommand {
   static override hidden = true;
 
   static override flags = {
-    name: Flags.string({
+    'variable-name': Flags.string({
       description: 'Name of the variable',
     }),
-    ...EASEnvironmentFlag,
+    ...EASMultiEnvironmentFlag,
     ...EASNonInteractiveFlag,
   };
 
@@ -31,7 +32,11 @@ export default class EnvironmentVariableUnlink extends EasCommand {
 
   async runAsync(): Promise<void> {
     let {
-      flags: { name, 'non-interactive': nonInteractive, environment },
+      flags: {
+        'variable-name': name,
+        'non-interactive': nonInteractive,
+        environment: unlinkEnvironments,
+      },
     } = await this.parse(EnvironmentVariableUnlink);
     const {
       privateProjectConfig: { projectId },
@@ -40,57 +45,102 @@ export default class EnvironmentVariableUnlink extends EasCommand {
       nonInteractive,
     });
 
-    if (!environment) {
-      environment = await promptVariableEnvironmentAsync({ nonInteractive });
-    }
-
     const projectDisplayName = await getDisplayNameForProjectIdAsync(graphqlClient, projectId);
-
-    const appVariables = await EnvironmentVariablesQuery.byAppIdAsync(graphqlClient, {
+    const variables = await EnvironmentVariablesQuery.sharedAsync(graphqlClient, {
       appId: projectId,
-      environment,
+      filterNames: name ? [name] : undefined,
     });
-    const linkedVariables = appVariables.filter(
-      ({ scope }) => scope === EnvironmentVariableScope.Shared
-    );
 
-    if (linkedVariables.length === 0) {
-      throw new Error(`There are no linked shared env variables for project ${projectDisplayName}`);
-    }
+    let selectedVariable = variables[0];
 
-    if (!name) {
-      name = await selectAsync(
-        'Select shared variable to unlink',
-        linkedVariables.map(variable => ({
-          title: variable.name,
-          value: variable.name,
+    if (variables.length > 1) {
+      if (nonInteractive) {
+        throw new Error("Multiple variables found, please select one using '--variable-name'");
+      }
+      selectedVariable = await selectAsync(
+        'Select shared variable',
+        variables.map(variable => ({
+          title: formatVariableName(variable),
+          value: variable,
         }))
       );
     }
-
-    const selectedVariable = linkedVariables.find(variable => variable.name === name);
 
     if (!selectedVariable) {
       throw new Error(`Shared variable ${name} doesn't exist`);
     }
 
-    const unlinkedVariable = await EnvironmentVariableMutation.unlinkSharedEnvironmentVariableAsync(
-      graphqlClient,
-      selectedVariable.id,
-      projectId,
-      environment
-    );
+    let explicitSelect = false;
 
-    if (!unlinkedVariable) {
-      throw new Error(
-        `Could not unlink variable with name ${selectedVariable.name} from project ${projectDisplayName}`
+    if (!nonInteractive && !unlinkEnvironments) {
+      const selectedEnvironments =
+        (selectedVariable.linkedEnvironments ?? []).length > 0
+          ? selectedVariable.linkedEnvironments
+          : selectedVariable.environments;
+      const environments = await promptVariableEnvironmentAsync({
+        nonInteractive,
+        multiple: true,
+        selectedEnvironments: selectedEnvironments ?? [],
+      });
+      explicitSelect = true;
+      unlinkEnvironments = Object.values(EnvironmentVariableEnvironment).filter(
+        env => !environments.includes(env)
       );
     }
 
-    Log.withTick(
-      `Unlinked variable ${chalk.bold(unlinkedVariable.name)} from the project ${chalk.bold(
-        projectDisplayName
-      )}.`
-    );
+    if (!unlinkEnvironments) {
+      await EnvironmentVariableMutation.unlinkSharedEnvironmentVariableAsync(
+        graphqlClient,
+        selectedVariable.id,
+        projectId
+      );
+      Log.withTick(
+        `Unlinked variable ${chalk.bold(selectedVariable.name)} from project ${chalk.bold(
+          projectDisplayName
+        )} in ${selectedVariable.environments?.join(', ').toLocaleLowerCase()}.`
+      );
+      return;
+    }
+
+    for (const environment of Object.values(EnvironmentVariableEnvironment)) {
+      if (
+        selectedVariable.linkedEnvironments?.includes(environment) !==
+        unlinkEnvironments.includes(environment)
+      ) {
+        if (!explicitSelect && unlinkEnvironments.includes(environment)) {
+          Log.withTick(
+            `Variable ${chalk.bold(
+              selectedVariable.name
+            )} is already unlinked from ${environment.toLocaleLowerCase()}.`
+          );
+        }
+        continue;
+      }
+      if (unlinkEnvironments.includes(environment)) {
+        await EnvironmentVariableMutation.unlinkSharedEnvironmentVariableAsync(
+          graphqlClient,
+          selectedVariable.id,
+          projectId,
+          environment
+        );
+        Log.withTick(
+          `Unlinked variable ${chalk.bold(selectedVariable.name)} from project ${chalk.bold(
+            projectDisplayName
+          )} in ${environment.toLocaleLowerCase()}.`
+        );
+      } else if (explicitSelect) {
+        await EnvironmentVariableMutation.linkSharedEnvironmentVariableAsync(
+          graphqlClient,
+          selectedVariable.id,
+          projectId,
+          environment
+        );
+        Log.withTick(
+          `Linked variable ${chalk.bold(selectedVariable.name)} to project ${chalk.bold(
+            projectDisplayName
+          )} in ${environment.toLocaleLowerCase()}.`
+        );
+      }
+    }
   }
 }
