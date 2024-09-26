3f282eee59386f9acee2fa920ccfaba03a56de36
Phil Pluckthun <phil@kitten.sh>
2024-09-26 12:46:21 +0100

    Fix listWorkerFilesAsync file type

--- packages/eas-cli/src/worker/assets.ts
+++ packages/eas-cli/src/worker/assets.ts
@@ -129,8 +129,8 @@ export async function createManifestAsync(
 
 interface WorkerFileEntry {
   normalizedPath: string;
-  sha512: string;
   path: string;
+  data: Buffer | string;
 }
 
 /** Reads worker files while normalizing sourcemaps and providing normalized paths */
@@ -144,11 +144,17 @@ async function* listWorkerFilesAsync(workerPath: string): AsyncGenerator<WorkerF
   }
 }
 
+interface AssetFileEntry {
+  normalizedPath: string;
+  sha512: string;
+  path: string;
+}
+
 /** Reads files of an asset maps and enumerates normalized paths and data */
 async function* listAssetMapFilesAsync(
   assetPath: string,
   assetMap: AssetMap
-): AsyncGenerator<WorkerFileEntry> {
+): AsyncGenerator<AssetFileEntry> {
   for (const normalizedPath in assetMap) {
     const filePath = path.resolve(assetPath, normalizedPath.split('/').join(path.sep));
     yield {
