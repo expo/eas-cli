6f5371a8a8e3bdcb028a4f3fcc6ed300adaeac9a
Phil Pluckthun <phil@kitten.sh>
2024-12-19 03:02:33 +0000

    Update product name

--- packages/eas-cli/src/commands/worker/deploy.ts
+++ packages/eas-cli/src/commands/worker/deploy.ts
@@ -114,7 +114,7 @@ export default class WorkerDeploy extends EasCommand {
       enableJsonOutput();
     }
 
-    Log.warn('EAS Worker Deployments are in beta and subject to breaking changes.');
+    Log.warn('EAS Hosting is still in beta and subject to changes.');
 
     const {
       getDynamicPrivateProjectConfigAsync,
