7073e407d339b00f628bc6471f0e8e6235e92257
Gabriel Donadel <donadeldev@gmail.com>
2025-03-06 10:45:34 -0500

    Add share command

---
+++ packages/eas-cli/src/graphql/mutations/ShareBuildMutation.ts
@@ -0,0 +1,42 @@
+import { FingerprintSource } from '@expo/eas-build-job';
+import { print } from 'graphql';
+import gql from 'graphql-tag';
+
+import { ExpoGraphqlClient } from '../../commandUtils/context/contextUtils/createGraphqlClient';
+import { withErrorHandlingAsync } from '../client';
+import { CreateFingeprintMutation, FingerprintFragment } from '../generated';
+import { FingerprintFragmentNode } from '../types/Fingerprint';
+
+export const ShareBuildMutation = {
+  async uploadLocalBuildAsync(
+    graphqlClient: ExpoGraphqlClient,
+    appId: string,
+    buildSource: {
+      bucketKey: string;
+    },
+    fingerprintData: { hash: string; source?: FingerprintSource }
+  ): Promise<FingerprintFragment> {
+    const data = await withErrorHandlingAsync(
+      graphqlClient
+        .mutation<CreateFingeprintMutation>(
+          gql`
+            mutation CreateFingeprintMutation(
+              $fingerprintData: CreateFingerprintInput!
+              $appId: ID!
+            ) {
+              fingerprint {
+                createOrGetExistingFingerprint(fingerprintData: $fingerprintData, appId: $appId) {
+                  id
+                  ...FingerprintFragment
+                }
+              }
+            }
+            ${print(FingerprintFragmentNode)}
+          `,
+          { appId, fingerprintData }
+        )
+        .toPromise()
+    );
+    return data.fingerprint.createOrGetExistingFingerprint;
+  },
+};
