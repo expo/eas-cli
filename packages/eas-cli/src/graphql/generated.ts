4f6d751ccbff660a048e6c87d80a09b12dbb1148
Stanisław Chmiela <sjchmiela@gmail.com>
2025-05-12 18:31:08 +0200

    Remove unused method

--- packages/eas-cli/src/graphql/generated.ts
+++ packages/eas-cli/src/graphql/generated.ts
@@ -10001,13 +10001,6 @@ export type WorkflowRunByIdQueryVariables = Exact<{
 
 export type WorkflowRunByIdQuery = { __typename?: 'RootQuery', workflowRuns: { __typename?: 'WorkflowRunQuery', byId: { __typename?: 'WorkflowRun', id: string, status: WorkflowRunStatus } } };
 
-export type WorkflowRunByIdWithJobsQueryVariables = Exact<{
-  workflowRunId: Scalars['ID']['input'];
-}>;
-
-
-export type WorkflowRunByIdWithJobsQuery = { __typename?: 'RootQuery', workflowRuns: { __typename?: 'WorkflowRunQuery', byId: { __typename?: 'WorkflowRun', id: string, name: string, status: WorkflowRunStatus, createdAt: any, workflow: { __typename?: 'Workflow', id: string, name?: string | null, fileName: string }, jobs: Array<{ __typename?: 'WorkflowJob', id: string, key: string, name: string, type: WorkflowJobType, status: WorkflowJobStatus, outputs: any, createdAt: any }> } } };
-
 export type AccountFragment = { __typename?: 'Account', id: string, name: string, ownerUserActor?: { __typename?: 'SSOUser', id: string, username: string } | { __typename?: 'User', id: string, username: string } | null, users: Array<{ __typename?: 'UserPermission', role: Role, actor: { __typename?: 'Robot', id: string } | { __typename?: 'SSOUser', id: string } | { __typename?: 'User', id: string } }> };
 
 export type AppFragment = { __typename?: 'App', id: string, name: string, fullName: string, slug: string, ownerAccount: { __typename?: 'Account', id: string, name: string, ownerUserActor?: { __typename?: 'SSOUser', id: string, username: string } | { __typename?: 'User', id: string, username: string } | null, users: Array<{ __typename?: 'UserPermission', role: Role, actor: { __typename?: 'Robot', id: string } | { __typename?: 'SSOUser', id: string } | { __typename?: 'User', id: string } }> }, githubRepository?: { __typename?: 'GitHubRepository', id: string, metadata: { __typename?: 'GitHubRepositoryMetadata', githubRepoOwnerName: string, githubRepoName: string } } | null };
--- packages/eas-cli/src/graphql/queries/WorkflowRunQuery.ts
+++ packages/eas-cli/src/graphql/queries/WorkflowRunQuery.ts
@@ -2,12 +2,7 @@ import gql from 'graphql-tag';
 
 import { ExpoGraphqlClient } from '../../commandUtils/context/contextUtils/createGraphqlClient';
 import { withErrorHandlingAsync } from '../client';
-import {
-  WorkflowRunByIdQuery,
-  WorkflowRunByIdQueryVariables,
-  WorkflowRunByIdWithJobsQuery,
-  WorkflowRunByIdWithJobsQueryVariables,
-} from '../generated';
+import { WorkflowRunByIdQuery, WorkflowRunByIdQueryVariables } from '../generated';
 
 export const WorkflowRunQuery = {
   async byIdAsync(
@@ -24,52 +19,6 @@ export const WorkflowRunQuery = {
                 byId(workflowRunId: $workflowRunId) {
                   id
                   status
-                }
-              }
-            }
-          `,
-          { workflowRunId },
-          {
-            requestPolicy: useCache ? 'cache-first' : 'network-only',
-            additionalTypenames: ['WorkflowRun'],
-          }
-        )
-        .toPromise()
-    );
-    return data.workflowRuns.byId;
-  },
-  async withJobsByIdAsync(
-    graphqlClient: ExpoGraphqlClient,
-    workflowRunId: string,
-    { useCache = true }: { useCache?: boolean } = {}
-  ): Promise<WorkflowRunByIdWithJobsQuery['workflowRuns']['byId']> {
-    const data = await withErrorHandlingAsync(
-      graphqlClient
-        .query<WorkflowRunByIdWithJobsQuery, WorkflowRunByIdWithJobsQueryVariables>(
-          gql`
-            query WorkflowRunByIdWithJobs($workflowRunId: ID!) {
-              workflowRuns {
-                byId(workflowRunId: $workflowRunId) {
-                  id
-                  name
-                  status
-                  createdAt
-
-                  workflow {
-                    id
-                    name
-                    fileName
-                  }
-
-                  jobs {
-                    id
-                    key
-                    name
-                    type
-                    status
-                    outputs
-                    createdAt
-                  }
                 }
               }
             }
