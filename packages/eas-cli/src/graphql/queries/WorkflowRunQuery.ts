7c929de926d70fcd865f5d42f7fbe9794a3371cb
Stanisław Chmiela <sjchmiela@gmail.com>
2025-05-12 18:33:43 +0200

    Revert "Remove unused method"
    This reverts commit 4f6d751ccbff660a048e6c87d80a09b12dbb1148.

--- packages/eas-cli/src/graphql/queries/WorkflowRunQuery.ts
+++ packages/eas-cli/src/graphql/queries/WorkflowRunQuery.ts
@@ -2,7 +2,12 @@ import gql from 'graphql-tag';
 
 import { ExpoGraphqlClient } from '../../commandUtils/context/contextUtils/createGraphqlClient';
 import { withErrorHandlingAsync } from '../client';
-import { WorkflowRunByIdQuery, WorkflowRunByIdQueryVariables } from '../generated';
+import {
+  WorkflowRunByIdQuery,
+  WorkflowRunByIdQueryVariables,
+  WorkflowRunByIdWithJobsQuery,
+  WorkflowRunByIdWithJobsQueryVariables,
+} from '../generated';
 
 export const WorkflowRunQuery = {
   async byIdAsync(
@@ -19,6 +24,52 @@ export const WorkflowRunQuery = {
                 byId(workflowRunId: $workflowRunId) {
                   id
                   status
+                }
+              }
+            }
+          `,
+          { workflowRunId },
+          {
+            requestPolicy: useCache ? 'cache-first' : 'network-only',
+            additionalTypenames: ['WorkflowRun'],
+          }
+        )
+        .toPromise()
+    );
+    return data.workflowRuns.byId;
+  },
+  async withJobsByIdAsync(
+    graphqlClient: ExpoGraphqlClient,
+    workflowRunId: string,
+    { useCache = true }: { useCache?: boolean } = {}
+  ): Promise<WorkflowRunByIdWithJobsQuery['workflowRuns']['byId']> {
+    const data = await withErrorHandlingAsync(
+      graphqlClient
+        .query<WorkflowRunByIdWithJobsQuery, WorkflowRunByIdWithJobsQueryVariables>(
+          gql`
+            query WorkflowRunByIdWithJobs($workflowRunId: ID!) {
+              workflowRuns {
+                byId(workflowRunId: $workflowRunId) {
+                  id
+                  name
+                  status
+                  createdAt
+
+                  workflow {
+                    id
+                    name
+                    fileName
+                  }
+
+                  jobs {
+                    id
+                    key
+                    name
+                    type
+                    status
+                    outputs
+                    createdAt
+                  }
                 }
               }
             }
