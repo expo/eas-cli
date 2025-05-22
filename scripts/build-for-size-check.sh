fceefef21c77ca845a76748f5a2858b2efb89f1c
Douglas Lowder <douglowder@mac.com>
2025-05-22 01:36:32 -0700

    Adjust yarn.lock inside size check archive

--- scripts/build-for-size-check.sh
+++ scripts/build-for-size-check.sh
@@ -7,3 +7,10 @@ yarn set version 1.22.22
 CLI_SIZE_CHECK=1 .yarn/releases/yarn-1.22.22.cjs workspace eas-cli oclif pack tarballs --no-xz --targets linux-x64 
 cp yarn-releases/*.cjs .yarn/releases
 yarn set version 4.9.1
+rm -rf packages/eas-cli/tmp/eas/.yarnrc.yml packages/eas-cli/tmp/eas/.yarn/releases
+cp -r .yarn/releases packages/eas-cli/tmp/eas/.yarn
+cp -r .yarnrc.yml packages/eas-cli/tmp/eas
+cd packages/eas-cli/tmp/eas
+yarn
+cd ..
+tar zcf ../dist/eas*.tar.gz eas
