fad19a1e0388c4b024d4f18a51b6b49a93459988
Douglas Lowder <douglowder@mac.com>
2025-07-07 12:27:32 -0700

    Workflow to test and verify GraphQL (#3093)
    # Why
    
    Now that #3091 is resolved, we need to add some CI to guard against similar future issues with GraphQL breakage.
    
    # How
    
    Add a new workflow that
    
    - Executes GraphQL generation, and fails if there is an error.
    - Verifies that for a given PR, any changes in GraphQL generated code or schema files have been checked in as part of the PR. If not, the workflow fails.
    
    The new workflow will execute on pushes, and on a daily schedule to guard against breakage due to changes on the server side.
    
    # Test Plan
    
    - CI should pass
    - Push a source change in the GraphQL code, but do not regenerate GraphQL, and verify that the new workflow fails

---
+++ packages/eas-cli/scripts/verify-graphql.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+function cleanup()
+{
+  echo 'Cleaning up...'
+  rm -f graphql.schema.json.bak src/graphql/generated.ts.bak
+}
+
+# Fail if anything errors
+set -eox pipefail
+# If this script exits, trap it first and clean up
+trap cleanup EXIT
+
+cp graphql.schema.json graphql.schema.json.bak
+cp src/graphql/generated.ts src/graphql/generated.ts.bak
+
+yarn generate-graphql-code
+if cmp -s graphql.schema.json graphql.schema.json.bak; then
+    echo "GraphQL schema is up-to-date"
+    if cmp -s src/graphql/generated.ts src/graphql/generated.ts.bak; then
+        echo "GraphQL generated code is up-to-date"
+        exit 0
+    else
+        echo "GraphQL code has changed but has not been regenerated. Run `yarn generate-graphql-code` and commit the changes."
+        exit 1
+    fi
+else
+    echo "GraphQL schema has changed on the server. Running `yarn tsc` to check for breaking changes..."
+    yarn tsc
+    # If there are no errors, then the schema is backwards compatible
+    exit 0
+fi
