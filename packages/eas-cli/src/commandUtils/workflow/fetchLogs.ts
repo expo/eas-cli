10babb9b835403011768434cf069cc2343bb0a4f
Douglas Lowder <douglowder@mac.com>
2025-07-15 15:26:36 -0700

    Update fetchLogs.ts

--- packages/eas-cli/src/commandUtils/workflow/fetchLogs.ts
+++ packages/eas-cli/src/commandUtils/workflow/fetchLogs.ts
@@ -3,10 +3,11 @@ import { WorkflowJobResult } from './types';
 // This function is in a separate module for testing purposes
 
 export async function fetchRawLogsForJobAsync(job: WorkflowJobResult): Promise<string | null> {
-  if (!job.turtleJobRun?.logFileUrls?.length) {
+  const firstLogFileUrl = job.turtleJobRun?.logFileUrls?.[0];
+  if (!firstLogFileUrl) {
     return null;
   }
-  const response = await fetch(job?.turtleJobRun?.logFileUrls[0], {
+  const response = await fetch(firstLogFileUrl, {
     method: 'GET',
   });
   const rawLogs = await response.text();
