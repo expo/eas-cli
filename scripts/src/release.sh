74f5d73a26dbda2785807241d780d95573e6920c
Douglas Lowder <douglowder@mac.com>
2025-07-29 22:55:56 -0700

    Add guard to prevent release script execution by non-bot user

--- scripts/src/release.sh
+++ scripts/src/release.sh
@@ -2,6 +2,14 @@
 
 set -eo pipefail
 
+GITHUB_USER=`git config get --global user.name`
+GITHUB_EMAIL=`git config get --global user.email`
+
+if [[ "$GITHUB_USER" != "Expo CI" || "$GITHUB_EMAIL" != "support+ci@expo.io" ]]; then
+  echo "This script may only be executed by the Expo CI bot in a GitHub workflow."
+  exit 1
+fi
+
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 ROOT_DIR="$( cd "$SCRIPTS_DIR"/.. && pwd )"
 
