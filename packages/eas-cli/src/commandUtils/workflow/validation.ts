0e7f400f69388be9a1c1087e4700bbb771818845
Douglas Lowder <douglowder@mac.com>
2025-08-25 19:13:58 -0700

    Client side workflow validation

--- packages/eas-cli/src/commandUtils/workflow/validation.ts
+++ packages/eas-cli/src/commandUtils/workflow/validation.ts
@@ -1,5 +1,7 @@
 import { EasJsonAccessor, EasJsonUtils } from '@expo/eas-json';
+import { InvalidEasJsonError, MissingEasJsonError } from '@expo/eas-json/build/errors';
 import { CombinedError } from '@urql/core';
+import Ajv, { DefinedError } from 'ajv';
 import * as YAML from 'yaml';
 
 import { AccountFragment } from '../../graphql/generated';
@@ -14,10 +16,18 @@ export async function validateWorkflowFileAsync(
   graphqlClient: ExpoGraphqlClient,
   projectId: string
 ): Promise<void> {
-  const parsedYaml = await parsedYamlFromWorkflowContentsAsync(workflowFileContents);
+  const parsedYaml = parsedYamlFromWorkflowContents(workflowFileContents);
 
   // Check if the parsed result is empty or null
   validateWorkflowIsNotEmpty(parsedYaml);
+
+  const workflowSchema = await fetchWorkflowSchemaAsync();
+
+  // Check that result passes validation against workflow schema
+  validateWorkflowStructure(parsedYaml, workflowSchema);
+
+  // Check that all job types are valid
+  validateWorkflowJobTypes(parsedYaml, workflowSchema);
 
   // Check for build jobs that do not match any EAS build profiles
   await validateWorkflowBuildJobsAsync(parsedYaml, projectDir);
@@ -25,6 +35,38 @@ export async function validateWorkflowFileAsync(
   // Check for other errors using the server-side validation
   await validateWorkflowOnServerAsync(graphqlClient, projectId, workflowFileContents);
 }
+
+export function logWorkflowValidationErrors(
+  error: unknown,
+  account: AccountFragment | undefined,
+  projectName: string | undefined
+): void {
+  if (error instanceof MissingEasJsonError) {
+    throw new Error(
+      'Workflows require a valid eas.json. Please run "eas build:configure" to create it.'
+    );
+  } else if (error instanceof InvalidEasJsonError) {
+    throw new Error(
+      'Workflows require a valid eas.json. Please fix the errors in your eas.json and try again.\n\n' +
+        error.message
+    );
+  } else if (error instanceof YAML.YAMLParseError) {
+    Log.error(`YAML syntax error: ${error.message}`);
+  } else if (error instanceof CombinedError) {
+    WorkflowFile.maybePrintWorkflowFileValidationErrors({
+      error,
+      accountName: account?.name ?? '',
+      projectName: projectName ?? '',
+    });
+
+    throw error;
+  } else if (error instanceof Error) {
+    Log.error(`Error: ${error.message}`);
+  } else {
+    Log.error(`Unexpected error: ${String(error)}`);
+  }
+}
+
 function validateWorkflowIsNotEmpty(parsedYaml: any): void {
   if (
     parsedYaml === null ||
@@ -32,28 +74,6 @@ function validateWorkflowIsNotEmpty(parsedYaml: any): void {
     (typeof parsedYaml === 'object' && Object.keys(parsedYaml).length === 0)
   ) {
     throw new Error('YAML file is empty or contains only comments.');
-  }
-}
-
-export function logWorkflowValidationErrors(
-  error: unknown,
-  account: AccountFragment,
-  projectName: string
-): void {
-  if (error instanceof YAML.YAMLParseError) {
-    Log.error(`YAML syntax error: ${error.message}`);
-  } else if (error instanceof CombinedError) {
-    WorkflowFile.maybePrintWorkflowFileValidationErrors({
-      error,
-      accountName: account.name,
-      projectName,
-    });
-
-    throw error;
-  } else if (error instanceof Error) {
-    Log.error(error.message);
-  } else {
-    Log.error(`Unexpected error: ${String(error)}`);
   }
 }
 
@@ -68,28 +88,17 @@ async function validateWorkflowOnServerAsync(
   });
 }
 
-async function parsedYamlFromWorkflowContentsAsync(workflowFileContents: {
-  yamlConfig: string;
-}): Promise<any> {
-  const workflowSchema = await fetchWorkflowSchemaAsync();
-  const customSchema = new YAML.Schema({ schema: workflowSchema, merge: true });
-  const parsedYaml = YAML.parse(workflowFileContents.yamlConfig, {
-    schema: customSchema,
-    strict: true,
-  });
-  return parsedYaml;
-}
-
 async function validateWorkflowBuildJobsAsync(parsedYaml: any, projectDir: string): Promise<void> {
-  const jobs = Object.entries(parsedYaml?.jobs).flatMap(([key, value]: [string, any]) => {
-    return {
-      key,
-      value,
-    };
-  });
+  const jobs = jobsFromWorkflow(parsedYaml);
   const buildJobs = jobs.filter(job => job.value.type === 'build');
+  if (buildJobs.length === 0) {
+    return;
+  }
   const easJsonAccessor = EasJsonAccessor.fromProjectPath(projectDir);
-  const buildProfileNames = new Set(await EasJsonUtils.getBuildProfileNamesAsync(easJsonAccessor));
+
+  const buildProfileNames = new Set(
+    easJsonAccessor && (await EasJsonUtils.getBuildProfileNamesAsync(easJsonAccessor))
+  );
   const invalidBuildJobs = buildJobs.filter(
     job => !buildProfileNames.has(job.value.params.profile)
   );
@@ -102,11 +111,96 @@ async function validateWorkflowBuildJobsAsync(parsedYaml: any, projectDir: strin
   }
 }
 
-async function fetchWorkflowSchemaAsync(): Promise<YAML.Schema> {
+function validateWorkflowJobTypes(parsedYaml: any, workflowJsonSchema: any): void {
+  const jobs = jobsFromWorkflow(parsedYaml);
+  const jobTypes = jobTypesFromWorkflowSchema(workflowJsonSchema);
+  const invalidJobs = jobs.filter(job => !jobTypes.includes(job.value.type));
+  if (invalidJobs.length > 0) {
+    throw new Error(
+      `The following jobs have invalid types: ${invalidJobs
+        .map(job => job.key)
+        .join(', ')}. Valid types are: ${jobTypes.join(', ')}`
+    );
+  }
+}
+
+function validateWorkflowStructure(parsedYaml: any, workflowJsonSchema: any): void {
+  const ajv = new Ajv({ allowUnionTypes: true });
+  ajv.addKeyword('markdownDescription');
+  ajv.addFormat('uri', {
+    type: 'string',
+    validate: (value: string) => {
+      try {
+        // eslint-disable-next-line no-new
+        new URL(value);
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  });
+  const validate = ajv.compile(workflowJsonSchema);
+  const result = validate(parsedYaml);
+
+  if (!result) {
+    Log.debug(
+      JSON.stringify(
+        {
+          errors: validate.errors,
+        },
+        null,
+        2
+      )
+    );
+    const processedErrors = new Set<string>();
+    for (const err of validate.errors ?? ([] as DefinedError[])) {
+      if (err.message) {
+        const params = new Map(Object.entries(err.params));
+        for (const [key, value] of params) {
+          if (key === 'allowedValue') {
+            processedErrors.add(
+              err?.instancePath +
+                ' ' +
+                err?.message?.replace('must be equal to constant', `must be equal to "${value}"`)
+            );
+          } else {
+            processedErrors.add(err?.instancePath + ' ' + err?.message);
+          }
+        }
+      }
+    }
+    throw new Error([...processedErrors].join('\n'));
+  }
+}
+
+function parsedYamlFromWorkflowContents(workflowFileContents: {
+  yamlConfig: string;
+}): Promise<any> {
+  const parsedYaml = YAML.parse(workflowFileContents.yamlConfig);
+  return parsedYaml;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+async function fetchWorkflowSchemaAsync(): Promise<any> {
   const response = await fetch('https://api.expo.dev/v2/workflows/schema');
   if (!response.ok) {
     throw new Error(`Unable to fetch EAS Workflow schema, received status: ${response.status}`);
   }
+  const jsonResponse: any = (await response.json()) as any;
+  return jsonResponse.data;
+}
 
-  return (await response.json()) as YAML.Schema;
+function jobsFromWorkflow(parsedYaml: any): any[] {
+  return Object.entries(parsedYaml?.jobs).flatMap(([key, value]: [string, any]) => {
+    return {
+      key,
+      value,
+    };
+  });
+}
+
+function jobTypesFromWorkflowSchema(workflowJsonSchema: any): string[] {
+  return workflowJsonSchema?.properties?.jobs?.additionalProperties?.anyOf.map(
+    (props: any) => props.properties.type.const
+  );
 }
