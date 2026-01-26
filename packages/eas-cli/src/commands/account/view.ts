d001580c025a6568e58f3b7a9b3e28200a8ae498
Brent Vatne <brentvatne@gmail.com>
2026-01-25 16:03:51 -0800

    [eas-cli] Update account commands for multi-account support
    - login: Add menu to add account or switch when already logged in
    - logout: Add username arg and --all flag for selective logout
    - view: Show other logged-in accounts when multi-account enabled

--- packages/eas-cli/src/commands/account/view.ts
+++ packages/eas-cli/src/commands/account/view.ts
@@ -4,6 +4,7 @@ import EasCommand from '../../commandUtils/EasCommand';
 import { Role } from '../../graphql/generated';
 import Log from '../../log';
 import { Actor, getActorDisplayName } from '../../user/User';
+import { isMultiAccountEnabled } from '../../utils/easCli';
 
 export default class AccountView extends EasCommand {
   static override description = 'show the username you are logged in as';
@@ -11,38 +12,52 @@ export default class AccountView extends EasCommand {
 
   static override contextDefinition = {
     ...this.ContextOptions.MaybeLoggedIn,
+    ...this.ContextOptions.SessionManagment,
   };
 
   async runAsync(): Promise<void> {
     const {
       maybeLoggedIn: { actor, authenticationInfo },
+      sessionManager,
     } = await this.getContextAsync(AccountView, { nonInteractive: true });
-    if (actor) {
-      const displayName = getActorDisplayName(actor);
-      const loggedInAs = authenticationInfo.accessToken
-        ? `${displayName} (authenticated using EXPO_TOKEN)`
-        : displayName;
-      Log.log(chalk.green(loggedInAs));
-      if ('email' in actor) {
-        Log.log(actor.email);
-      }
 
-      // personal account is included, only show if more accounts that personal account
-      // but do show personal account in list if there are more
-      const accountExcludingPersonalAccount = actor.accounts.filter(
-        account => !('username' in actor) || account.name !== actor.username
-      );
-      if (accountExcludingPersonalAccount.length > 0) {
-        Log.newLine();
-        Log.log('Accounts:');
-        actor.accounts.forEach(account => {
-          const roleOnAccount = AccountView.getRoleOnAccount(actor, account);
-          Log.log(`• ${account.name} (Role: ${AccountView.getLabelForRole(roleOnAccount)})`);
-        });
-      }
-    } else {
+    if (!actor) {
       Log.warn('Not logged in');
       process.exit(1);
+    }
+
+    const displayName = getActorDisplayName(actor);
+    const loggedInAs = authenticationInfo.accessToken
+      ? `${displayName} (authenticated using EXPO_TOKEN)`
+      : displayName;
+    Log.log(chalk.green(loggedInAs));
+    if ('email' in actor) {
+      Log.log(actor.email);
+    }
+
+    // Show other logged-in accounts if multi-account is enabled
+    if (isMultiAccountEnabled() && !authenticationInfo.accessToken) {
+      const accounts = sessionManager.getAllAccounts();
+      const otherAccounts = accounts.filter(a => !a.isActive);
+
+      if (otherAccounts.length > 0) {
+        const otherUsernames = otherAccounts.map(a => a.username).join(', ');
+        Log.log(chalk.dim(`Also logged in: ${otherUsernames}`));
+      }
+    }
+
+    // personal account is included, only show if more accounts that personal account
+    // but do show personal account in list if there are more
+    const accountExcludingPersonalAccount = actor.accounts.filter(
+      account => !('username' in actor) || account.name !== actor.username
+    );
+    if (accountExcludingPersonalAccount.length > 0) {
+      Log.newLine();
+      Log.log('Accounts:');
+      actor.accounts.forEach(account => {
+        const roleOnAccount = AccountView.getRoleOnAccount(actor, account);
+        Log.log(`• ${account.name} (Role: ${AccountView.getLabelForRole(roleOnAccount)})`);
+      });
     }
   }
 
