68b78585a6731e9a3a32524d6472957a2fa880b8
Stanisław Chmiela <sjchmiela@gmail.com>
2026-02-27 21:31:36 +0100

    Update parse tests and options

--- packages/eas-cli/src/commandUtils/workflow/parse.ts
+++ packages/eas-cli/src/commandUtils/workflow/parse.ts
@@ -1,5 +1,9 @@
 import * as YAML from 'yaml';
 
 export function parsedYamlFromWorkflowContents(workflowFileContents: { yamlConfig: string }): any {
-  return YAML.parse(workflowFileContents.yamlConfig, { merge: true });
+  return YAML.parse(workflowFileContents.yamlConfig, {
+    // Keep in sync with backend parser options.
+    merge: true,
+    maxAliasCount: 50,
+  });
 }
