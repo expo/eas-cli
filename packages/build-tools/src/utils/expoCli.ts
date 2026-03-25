004af7a1beb35b7df1b0162e4a1fb7cff2ce4ec7
Phil Pluckthun <phil@kitten.sh>
2026-03-25 08:13:40 +0000

    Align with other CLI invocations

--- packages/build-tools/src/utils/expoCli.ts
+++ packages/build-tools/src/utils/expoCli.ts
@@ -3,18 +3,34 @@
 import resolveFrom from 'resolve-from';
 import spawnAsync from '@expo/spawn-async';
 
+export class ExpoCLIModuleNotFoundError extends Error {}
+
+function resolveExpoCLI(projectRoot: string): string {
+  try {
+    return (
+      resolveFrom.silent(projectRoot, 'expo/bin/cli') ?? resolveFrom(projectRoot, 'expo/bin/cli.js')
+    );
+  } catch (e: any) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      throw new ExpoCLIModuleNotFoundError(`The \`expo\` package was not found.`);
+    }
+    throw e;
+  }
+}
+
 export async function expoCommandAsync(
   projectDir: string,
   args: string[],
   options: Omit<spawnAsync.SpawnOptions, 'cwd'>
 ) {
-  const expoCliPath =
-    resolveFrom.silent(projectDir, 'expo/bin/cli') ?? resolveFrom(projectDir, 'expo/bin/cli.js');
+  const expoCliPath = resolveExpoCLI(projectDir);
   return spawnAsync(expoCliPath, args, {
     cwd: projectDir,
     stdio: 'pipe',
     env: {
       ...options.env,
+      // NOTE: If we're reading user configs, if a user has set this, it might cause excessive output
+      // that can stop the command from being readable
       EXPO_DEBUG: '0',
     },
     ...options,
