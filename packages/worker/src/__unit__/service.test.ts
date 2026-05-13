e20a08b9309b938d3481f78b6b4e693d6eabc9f7
AbbanMustafa <mustafaabban@utexas.edu>
2026-05-13 13:24:22 -0400

    rem tests

--- packages/worker/src/__unit__/service.test.ts
+++ packages/worker/src/__unit__/service.test.ts
@@ -1,75 +1,12 @@
-import {
-  ArchiveSourceType,
-  BuildMode,
-  BuildTrigger,
-  Platform,
-  Workflow,
-  errors,
-} from '@expo/eas-build-job';
+import { errors } from '@expo/eas-build-job';
 import spawn from '@expo/turtle-spawn';
 import { vol } from 'memfs';
 import path from 'path';
 
-import { build } from '../build';
-import { createBuildContext } from '../context';
-import BuildService, { getExpoPackageVersionAsync } from '../service';
-import { turtleFetch } from '../utils/turtleFetch';
+import { getExpoPackageVersionAsync } from '../service';
 
 jest.mock('fs');
 jest.mock('@expo/turtle-spawn', () => jest.fn());
-jest.mock('../build', () => ({
-  build: jest.fn(),
-}));
-jest.mock('../context', () => ({
-  createBuildContext: jest.fn(),
-}));
-jest.mock('../external/analytics', () => ({
-  Analytics: jest.fn().mockImplementation(() => ({
-    flushEventsAsync: jest.fn(),
-    logEvent: jest.fn(),
-  })),
-}));
-jest.mock('../logger', () => {
-  const logger = {
-    child: jest.fn(),
-    debug: jest.fn(),
-    error: jest.fn(),
-    info: jest.fn(),
-    level: jest.fn(),
-    warn: jest.fn(),
-  };
-  logger.child.mockReturnValue(logger);
-
-  return {
-    __esModule: true,
-    default: logger,
-    createBuildLoggerWithSecretsFilter: jest.fn(async () => ({
-      cleanUp: jest.fn(),
-      logBuffer: { getLogs: jest.fn(() => []), getPhaseLogs: jest.fn(() => []) },
-      logger,
-    })),
-  };
-});
-jest.mock('../sentry', () => ({
-  __esModule: true,
-  default: {
-    capture: jest.fn(),
-  },
-}));
-jest.mock('../utils/turtleFetch', () => ({
-  turtleFetch: jest.fn(),
-}));
-jest.mock('../config', () => {
-  const config = jest.requireActual('../config').default;
-  return {
-    __esModule: true,
-    default: {
-      ...config,
-      buildId: 'build-id',
-      wwwApiV2BaseUrl: 'http://api.expo.test/v2/',
-    },
-  };
-});
 
 describe(getExpoPackageVersionAsync, () => {
   const projectRoot = '/test-project';
@@ -122,4 +59,3 @@ describe(getExpoPackageVersionAsync, () => {
     await expect(getExpoPackageVersionAsync(buildContext)).rejects.toBeInstanceOf(errors.UserError);
   });
 });
-
