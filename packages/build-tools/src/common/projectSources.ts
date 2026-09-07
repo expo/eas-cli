8f2879fcf55f411fa81995c3c5fd4b24a88fce54
Stanisław Chmiela <sjchmiela@gmail.com>
2026-09-07 16:48:47 +0200

    [build-tools] Ask users to re-run the job

--- packages/build-tools/src/common/projectSources.ts
+++ packages/build-tools/src/common/projectSources.ts
@@ -32,7 +32,7 @@ Promise<{ handled: boolean }> {
     const projectArchiveResult = await asyncResult(fetchProjectArchiveSourceAsync(ctx));
 
     if (!projectArchiveResult.ok) {
-      throw new SystemError('Failed to refresh project archive. Try the build again.', {
+      throw new SystemError('Failed to refresh project archive. Re-run the job.', {
         cause: projectArchiveResult.reason,
       });
     }
