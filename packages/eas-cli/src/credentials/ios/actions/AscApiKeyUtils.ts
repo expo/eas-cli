140c2de5fc434266a62104c97c219513437e5717
Douglas Lowder <douglowder@mac.com>
2026-09-07 14:14:55 -0700

    Fix errors from nullability change in issuerIdentifier

--- packages/eas-cli/src/credentials/ios/actions/AscApiKeyUtils.ts
+++ packages/eas-cli/src/credentials/ios/actions/AscApiKeyUtils.ts
@@ -304,7 +304,7 @@ export async function resolveAscApiKeyForAppCredentialsAsync({
     ascApiKey: {
       keyP8: fullKey.keyP8,
       keyId: fullKey.keyIdentifier,
-      issuerId: fullKey.issuerIdentifier,
+      issuerId: fullKey.issuerIdentifier ?? '',
     },
     teamId: ascKeyFragment.appleTeam?.appleTeamIdentifier,
     teamName: ascKeyFragment.appleTeam?.appleTeamName ?? undefined,
